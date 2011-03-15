84573ccd35997d54fa8260bb5bae3f7ed112b49d
Jan Berkel <jan.berkel@gmail.com>
2011-03-15 20:35:11 +0100

    Search tests

---
+++ tests/src/com/soundcloud/android/activity/ScSearchTests.java
@@ -0,0 +1,22 @@
+package com.soundcloud.android.activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class ScSearchTests {
+    @Test
+    public void shouldSearch() throws Exception {
+        Robolectric.pauseMainLooper();
+        ScSearch search = new ScSearch();
+        search.onCreate(null);
+        search.doSearch("Testing");
+        // TODO add real tests here
+    }
+}
