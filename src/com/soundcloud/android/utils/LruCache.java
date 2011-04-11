fea8a6d6d4807f1494f53b3526cb56da5b88cc97
Jan Berkel <jan.berkel@gmail.com>
2011-04-11 12:01:20 +0100

    Adding copyright header + provenance

--- src/com/soundcloud/android/utils/LruCache.java
+++ src/com/soundcloud/android/utils/LruCache.java
@@ -6,6 +6,23 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @see <a href="http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android-apps/2.3.3_r1/com/android/camera/gallery/LruCache.java#LruCache">com/android/camera/gallery/LruCache.java</a>
+ */
 public class LruCache<K, V> {
 
     private final HashMap<K, V> mLruMap;
