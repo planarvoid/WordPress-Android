3a978750166fe4f8821c1c402772303e6a10546c
Jan Berkel <jan.berkel@gmail.com>
2011-04-14 14:30:35 +0200

    Cleaned up Connection

--- src/com/soundcloud/android/objects/Connection.java
+++ src/com/soundcloud/android/objects/Connection.java
@@ -13,42 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Connection implements Comparable {
-    public enum Service {
-        Twitter(R.drawable.service_twitter, true, 1, "twitter"),
-        Facebook(R.drawable.service_facebook_profile, true, 0, "facebook_profile", "facebook_page"),
-        Myspace(R.drawable.service_myspace, true, 3, "myspace"),
-        Foursquare(R.drawable.service_foursquare, true, 2, "foursquare"),
-        Unknown(R.drawable.service_myspace, false, 4, "unknown");
-
-        public final int resId;
-        public final String name;
-        public final String[] names;
-        public final boolean enabled;
-        public final int rank;
-
-        Service(int resId, boolean enabled, int rank, String... names) {
-            this.resId = resId;
-            this.rank = rank;
-            this.names = names;
-            this.name  = names[0];
-            this.enabled = enabled;
-        }
-        static Service fromString(String s) {
-            for (Service svc : EnumSet.allOf(Service.class)) {
-                for (String n : svc.names) if (s.equalsIgnoreCase(n)) return svc;
-            }
-            return Unknown;
-        }
-    }
-    public Connection() {}
-    public Connection(Service s) {
-        this._service = s;
-        this.display_name = s.toString();
-        this.active = false;
-    }
-
-    public boolean active() { return active; }
+public class Connection implements Comparable<Connection> {
     private boolean active = true;
     private Service _service;
 
@@ -60,32 +25,50 @@ public class Connection implements Comparable {
     public String service;
     public URI uri;
 
+    public Connection() {}
+    public Connection(Service s) {
+        this._service = s;
+        this.display_name = s.toString();
+        this.active = false;
+    }
+
+    public boolean active() { return active; }
     public Service service() {
         if (_service == null) _service = Service.fromString(service);
         return _service;
     }
 
-    public int getRank(){
-        if (_service == null) _service = Service.fromString(service);
-        return _service.rank;
+    public int compareTo(Connection another) {
+        int rank1 = service().ordinal();
+        int rank2 = another.service().ordinal();
+        return (rank1 > rank2) ? 1 : ((rank1 < rank2) ? -1 : 0);
     }
 
+    public enum Service {
+        Facebook(R.drawable.service_facebook_profile, true, "facebook_profile", "facebook_page"),
+        Twitter(R.drawable.service_twitter, true, "twitter"),
+        Foursquare(R.drawable.service_foursquare, true, "foursquare"),
+        Myspace(R.drawable.service_myspace, true, "myspace"),
+        Unknown(R.drawable.service_myspace, false, "unknown");
 
+        public final int resId;
+        public final String name;
+        public final String[] names;
+        public final boolean enabled;
 
-    public int compareTo(Object another) {
-
-        int rank1 = getRank();
-        int rank2 = ((Connection) another).getRank();
-
-        if (rank1 > rank2){
-            return +1;
-        }else if (rank1 < rank2){
-            return -1;
-        }else{
-            return 0;
+        Service(int resId, boolean enabled, String... names) {
+            this.resId = resId;
+            this.names = names;
+            this.name  = names[0];
+            this.enabled = enabled;
+        }
+        static Service fromString(String s) {
+            for (Service svc : EnumSet.allOf(Service.class)) {
+                for (String n : svc.names) if (s.equalsIgnoreCase(n)) return svc;
+            }
+            return Unknown;
         }
     }
-
 
     @SuppressWarnings("unchecked")
     public static List<Connection> addUnused(List<Connection> connections) {
