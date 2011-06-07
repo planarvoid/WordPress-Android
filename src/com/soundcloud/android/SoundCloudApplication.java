b0e292ae446a26e0e3b1f563378250842a535dbf
Jan Berkel <jan.berkel@gmail.com>
2011-06-07 18:17:04 +0200

    New spreadsheet key

--- src/com/soundcloud/android/SoundCloudApplication.java
+++ src/com/soundcloud/android/SoundCloudApplication.java
@@ -46,7 +46,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@ReportsCrashes(formKey = "dE9lUVFDd1kwRHVObWlzYTJKU0JqQ2c6MQ")
+@ReportsCrashes(formKey = "dFVUNnFjazQ4RVFqOG1wcFl1MTU5QVE6MQ")
 public class SoundCloudApplication extends Application implements AndroidCloudAPI, CloudAPI.TokenListener {
     public static final String TAG = SoundCloudApplication.class.getSimpleName();
     public static final String GA_TRACKING = "UA-2519404-11";
