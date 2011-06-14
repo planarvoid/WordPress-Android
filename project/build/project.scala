ca2e05448ae2f47189870af59274643b9b2b843f
Jan Berkel <jan.berkel@gmail.com>
2011-06-14 16:18:12 +0200

    Upgraded filecache to r153

--- project/build/project.scala
+++ project/build/project.scala
@@ -32,7 +32,7 @@ class Parent(info: ProjectInfo) extends ParentProject(info) {
     val jackson_core = "org.codehaus.jackson" % "jackson-core-asl" % "1.7.1"
     val jackson_mapper = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.1"
     val java_wrapper = "com.soundcloud" % "java-api-wrapper" % "1.0.1-SNAPSHOT"
-    val filecache = "com.google.android" % "filecache" % "0.1"
+    val filecache = "com.google.android" % "filecache" % "r153"
     val analytics = "com.google.android" % "libGoogleAnalytics" % "1.1"
     var wrapper   = "com.commonsware" % "CWAC-AdapterWrapper" % "0.4"
     val vorbis    = "org.xiph" % "libvorbis" % "1.0.0-beta"
