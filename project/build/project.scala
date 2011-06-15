19f141c8f9678e2624a846bd4ec53833c747b6a7
Jan Berkel <jan.berkel@gmail.com>
2011-06-15 17:30:44 +0200

    Bumping version of libGoogleAnalytics to 1.2

--- project/build/project.scala
+++ project/build/project.scala
@@ -33,7 +33,7 @@ class Parent(info: ProjectInfo) extends ParentProject(info) {
     val jackson_mapper = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.1"
     val java_wrapper = "com.soundcloud" % "java-api-wrapper" % "1.0.1-SNAPSHOT"
     val filecache = "com.google.android" % "filecache" % "r153"
-    val analytics = "com.google.android" % "libGoogleAnalytics" % "1.1"
+    val analytics = "com.google.android" % "libGoogleAnalytics" % "1.2"
     var wrapper   = "com.commonsware" % "CWAC-AdapterWrapper" % "0.4"
     val vorbis    = "org.xiph" % "libvorbis" % "1.0.0-beta"
 
