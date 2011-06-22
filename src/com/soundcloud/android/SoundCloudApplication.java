cc9aae29b2feb0d2352f5561141308cf19ba58d7
Jan Berkel <jan.berkel@gmail.com>
2011-06-22 17:15:10 +0200

    Change version + formkey

--- src/com/soundcloud/android/SoundCloudApplication.java
+++ src/com/soundcloud/android/SoundCloudApplication.java
@@ -48,7 +48,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@ReportsCrashes(formKey = "dDNyRjc4eG1SMUcxR1A0X3BJS0JmZUE6MQ")
+@ReportsCrashes(formKey = "dGthVjFzQ0FJanpGNU9fN3BDY3VSYVE6MQ")
 public class SoundCloudApplication extends Application implements AndroidCloudAPI, CloudAPI.TokenListener {
     public static final String TAG = SoundCloudApplication.class.getSimpleName();
     public static final String GA_TRACKING = "UA-2519404-11";
