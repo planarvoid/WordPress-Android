643784237d96267cd8ca66ce9a765d656a7ab9de
Jon Schmidt <jon@soundcloud.com>
2011-07-01 18:38:25 +0200

    fixed events pagination

--- src/com/soundcloud/android/adapter/EventsAdapter.java
+++ src/com/soundcloud/android/adapter/EventsAdapter.java
@@ -1,46 +1,24 @@
 
 package com.soundcloud.android.adapter;
 
-import android.content.ContentResolver;
 import com.soundcloud.android.activity.ScActivity;
 import com.soundcloud.android.model.Event;
 import com.soundcloud.android.model.Track;
-import com.soundcloud.android.provider.DatabaseHelper.Content;
-import com.soundcloud.android.provider.DatabaseHelper.Events;
-import com.soundcloud.android.task.DashboardQueryTask;
-import com.soundcloud.android.task.QueryTask;
-import com.soundcloud.android.utils.CloudUtils;
 import com.soundcloud.android.view.EventsRow;
 import com.soundcloud.android.view.LazyRow;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-
-import android.database.ContentObserver;
-import android.os.Build;
-import android.os.Handler;
 import android.os.Parcelable;
-import android.text.TextUtils;
 
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.List;
 
 public class EventsAdapter extends TracklistAdapter {
 
     public static final String TAG = "EventsAdapter";
-    public String nextCursor;
-
     private boolean mExclusive;
-    private QueryTask mQueryTask;
-
-    private ChangeObserver  mChangeObserver;
 
     public EventsAdapter(ScActivity context, ArrayList<Parcelable> data, boolean isExclusive, Class<?> model) {
         super(context, data, model);
         mExclusive = isExclusive;
-        //mChangeObserver = new ChangeObserver();
-        //addLocalEvents();
     }
 
     public boolean isExclusive(){
@@ -58,83 +36,5 @@ public class EventsAdapter extends TracklistAdapter {
     }
 
 
-    @Override
-    public boolean isQuerying(){
-        return !CloudUtils.isTaskFinished(mQueryTask);
-    }
 
-    @Override
-    public void onPostQueryExecute() {
-        mQueryTask = null;
-        super.onPostQueryExecute();
-        if (mData.size() > 0) {
-            if (TextUtils.isEmpty(nextCursor)){
-                // we must be at the end of the dashboard list, dont allow appending
-                mWrapper.mKeepOnAppending.set(false);
-            }
-        }
-        mActivity.getContentResolver().registerContentObserver(Events.CONTENT_URI, true, mChangeObserver);
-        notifyDataSetChanged();
-        submenuIndex = -1;
-        animateSubmenuIndex = -1;
-
-    }
-
-
-    private void addLocalEvents() {
-
-        // pre SDK 8 doesn't allow auto syncing, so with our current list loading UI
-        // it is easier to just enabling remote pulling only for now
-        if (Build.VERSION.SDK_INT < 8) return;
-
-        mActivity.getContentResolver().unregisterContentObserver(mChangeObserver);
-
-        mData = new ArrayList<Parcelable>();
-        if (CloudUtils.isTaskFinished(mQueryTask)){
-            mQueryTask = new DashboardQueryTask(mActivity.getApp());
-            mQueryTask.setAdapter(this);
-            mQueryTask.setQuery((mExclusive ? Content.EXCLUSIVE_TRACKS : Content.INCOMING_TRACKS),
-                    null, Events.ALIAS_USER_ID + "= ? AND " + Events.ALIAS_EXCLUSIVE + " = ?",
-                    new String[] {
-                            Long.toString(mActivity.getCurrentUserId()), (mExclusive ? "1" : "0")
-                    }, Events.ALIAS_ID + " DESC");
-            mQueryTask.execute();
-        } else
-            mQueryTask.setAdapter(this);
-
-    }
-
-    @Override
-    public void reset() {
-        mData.clear();
-        mPage = 1;
-        submenuIndex = -1;
-        animateSubmenuIndex = -1;
-        nextCursor = "";
-        //addLocalEvents();
-    }
-
-    public void onNextEventsCursor(String mNextCursor) {
-        nextCursor = mNextCursor;
-    }
-
-    protected void onContentChanged() {
-        addLocalEvents();
-    }
-
-    private class ChangeObserver extends ContentObserver {
-        public ChangeObserver() {
-            super(new Handler());
-        }
-
-        @Override
-        public boolean deliverSelfNotifications() {
-            return true;
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            onContentChanged();
-        }
-    }
 }
