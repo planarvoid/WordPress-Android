1c5dd25f879e13c7822c16479e009a638bcdc392
Jan Berkel <jan.berkel@gmail.com>
2011-07-08 18:21:37 +0200

    Added placeholder test

--- tests/src/com/soundcloud/android/service/beta/BetaServiceTest.java
+++ tests/src/com/soundcloud/android/service/beta/BetaServiceTest.java
@@ -1,19 +1,16 @@
 package com.soundcloud.android.service.beta;
 
-import com.soundcloud.android.service.beta.BetaService;
+import com.soundcloud.android.robolectric.DefaultTestRunner;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import com.soundcloud.android.robolectric.DefaultTestRunner;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.util.List;
 
 
 @RunWith(DefaultTestRunner.class)
 public class BetaServiceTest {
 
+    @Test @Ignore
+    public void testMe() throws Exception {
 
+    }
 }
