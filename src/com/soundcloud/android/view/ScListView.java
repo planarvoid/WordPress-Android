50213a17b2e0224b07ca031cc88421798a2aadf2
Jon Schmidt <jon@soundcloud.com>
2011-08-02 15:34:28 +0200

    annoying traces gone

--- src/com/soundcloud/android/view/ScListView.java
+++ src/com/soundcloud/android/view/ScListView.java
@@ -564,7 +564,6 @@ public class ScListView extends ListView implements AbsListView.OnScrollListener
     @Override
     protected void layoutChildren() {
         super.layoutChildren();
-        Log.i("asdf","Layout Children " + mRefreshState + " " + getFirstVisiblePosition());
         if (getFirstVisiblePosition() == 0 && (mRefreshState == TAP_TO_REFRESH || mRefreshState == DONE_REFRESHING)) {
             // not enough views to fill list so pad with an empty view
             final int lastDataPosition = getWrapper().getCount(); // data index + header
@@ -620,7 +619,6 @@ public class ScListView extends ListView implements AbsListView.OnScrollListener
 
 
     private void scrollPastHeader() {
-        Log.i("asdf","SCROLL PAST HEADERRR " + (Build.VERSION.SDK_INT >= 8));
         if (Build.VERSION.SDK_INT >= 8){
             smoothScrollBy(getChildAt(1).getTop() + 1, HEADER_HIDE_DURATION);
             mAutoScrolling = true;
