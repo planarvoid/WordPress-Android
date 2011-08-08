19b1f19c8e55cb8a85d6a61814c4e9520b9d0006
Jon Schmidt <jon@soundcloud.com>
2011-08-08 13:33:35 +0200

    no update for external upload

--- src/com/soundcloud/android/activity/ScUpload.java
+++ src/com/soundcloud/android/activity/ScUpload.java
@@ -137,7 +137,9 @@ public class ScUpload extends ScActivity {
             @Override
             public void onClick(View v) {
                 mapToRecording(mRecording);
-                getContentResolver().update(mRecording.toUri(), mRecording.buildContentValues(), null, null);
+                if (!mRecording.external_upload) {
+                    getContentResolver().update(mRecording.toUri(), mRecording.buildContentValues(), null, null);
+                }
                 if (startUpload()) {
                     mRecording = null;
                     setResult(RESULT_OK);
