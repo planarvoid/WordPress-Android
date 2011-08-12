1f375d32b1aeba2ab5d9f2e489c13face6ef46b2
Jan Berkel <jan.berkel@gmail.com>
2011-08-12 15:25:51 +0200

    Force initialisation

--- src/com/soundcloud/android/activity/NotificationSettings.java
+++ src/com/soundcloud/android/activity/NotificationSettings.java
@@ -1,32 +1,31 @@
 package com.soundcloud.android.activity;
 
-import android.accounts.Account;
-import android.content.ContentResolver;
-import android.nfc.tech.IsoDep;
-import android.preference.*;
-import android.util.Log;
 import com.soundcloud.android.R;
-
-import android.os.Bundle;
 import com.soundcloud.android.SoundCloudApplication;
 import com.soundcloud.android.provider.ScContentProvider;
-import com.soundcloud.android.task.NewConnectionTask;
+
+import android.accounts.Account;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationSettings extends PreferenceActivity {
-
-    ArrayList<CheckBoxPreference> syncPreferences;
+    final List<CheckBoxPreference> syncPreferences = new ArrayList<CheckBoxPreference>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.notifications_settings);
 
-        syncPreferences = new ArrayList<CheckBoxPreference>();
         for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
             if (getPreferenceScreen().getPreference(i) instanceof PreferenceCategory) {
-
                 final PreferenceCategory preferenceCategory = (PreferenceCategory) getPreferenceScreen().getPreference(i);
                 for (int j = 0; j < preferenceCategory.getPreferenceCount(); j++) {
                     if (preferenceCategory.getPreference(j) instanceof CheckBoxPreference) {
@@ -41,7 +40,6 @@ public class NotificationSettings extends PreferenceActivity {
                             }
                         });
                     }
-
                 }
             }
         }
@@ -66,6 +64,5 @@ public class NotificationSettings extends PreferenceActivity {
                 ScContentProvider.disableSyncing(account);
             }
         }
-
     }
 }
