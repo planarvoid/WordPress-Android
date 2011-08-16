832bfdd73e659dbf8d5d31beea2d56ffafdba17f
Jan Berkel <jan.berkel@gmail.com>
2011-08-16 19:59:09 +0200

    Remove debug code

--- src/com/soundcloud/android/view/ScListView.java
+++ src/com/soundcloud/android/view/ScListView.java
@@ -672,17 +672,6 @@ public class ScListView extends ListView implements AbsListView.OnScrollListener
         child.measure(childWidthSpec, childHeightSpec);
     }
 
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        try {
-            super.onTextChanged(s, start, before, count);
-        } catch (IllegalStateException e) {
-            Log.w(TAG, "onTextChanged("+s+","+start+","+before+","+count+"), mActivity="+mActivity);
-            throw e;
-        }
-    }
-
     private void configureLastUpdated() {
         // can trigger a weird layout loop if done in a different state, may need to be revisited
         if (mRefreshState == TAP_TO_REFRESH && mLastUpdated != 0) {
