3da495583999fa30f971b0d6afdc8bc278a59dc2
Jan Berkel <jan.berkel@gmail.com>
2011-08-17 15:42:40 +0200

    WS

--- src/com/soundcloud/android/utils/CloudUtils.java
+++ src/com/soundcloud/android/utils/CloudUtils.java
@@ -282,9 +282,7 @@ public class CloudUtils {
                             relativeLayout.removeViewAt(j);
                         }
                     }
-
                 }
-
             }
         }
     }
