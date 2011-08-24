5431c3f0d3f3ecb75aba25549a89a083cd070db6
Jon Schmidt <jon@soundcloud.com>
2011-08-24 15:07:57 +0200

    copy changes

--- src/com/soundcloud/android/activity/tour/Finish.java
+++ src/com/soundcloud/android/activity/tour/Finish.java
@@ -16,7 +16,7 @@ public class Finish extends Activity {
         super.onCreate(bundle);
         setContentView(R.layout.tour_finish);
 
-
+        ((TextView) findViewById(R.id.txt_title)).setText(getString(R.string.tour_finish_title));
         ((TextView) findViewById(R.id.txt_message)).setText(Html.fromHtml(getString(R.string.tour_finish_message)));
         ((TextView) findViewById(R.id.txt_message)).setMovementMethod(LinkMovementMethod.getInstance());
         findViewById(R.id.btn_done).setOnClickListener(new View.OnClickListener() {
