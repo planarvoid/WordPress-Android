b0c4f5087d90195a3523aab449a29775bbb217f2
Jon Schmidt <jon@soundcloud.com>
2011-09-16 09:22:16 -0400

    resume partial animations

--- src/com/soundcloud/android/view/WaveformControllerLand.java
+++ src/com/soundcloud/android/view/WaveformControllerLand.java
@@ -1,12 +1,16 @@
 package com.soundcloud.android.view;
 
+import android.graphics.Matrix;
 import android.os.Handler;
 import android.os.Message;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
 
@@ -22,7 +26,7 @@ public class WaveformControllerLand extends WaveformController {
     private int mAvatarOffsetY, mCommentBarOffsetY;
     private CommentPanel mCommentPanel;
 
-    private static final int COMMENT_ANIMATE_DURATION = 300;
+    private static final int COMMENT_ANIMATE_DURATION = 500;
 
     private boolean mWaveformHalf;
     private boolean mCommentPanelVisible;
@@ -70,7 +74,7 @@ public class WaveformControllerLand extends WaveformController {
     }
 
      protected void processDownInput(InputObject input) {
-        if (input.view == mPlayerAvatarBar) {
+        if (input.view == mPlayerAvatarBar && mode == TOUCH_MODE_NONE) {
             if (mCurrentComments != null) {
                 mode = TOUCH_MODE_AVATAR_DRAG;
                 //mCommentBubble.comment_mode = CommentBubble.MODE_SHOW_COMMENT;
@@ -92,7 +96,7 @@ public class WaveformControllerLand extends WaveformController {
             } else {
                 if (mCommentPanel != null && !mCommentPanel.closing) {
                     mCommentPanel.interacted = false;
-                    mHandler.postDelayed(mAutoCloseComment, 100);
+                    mHandler.postDelayed(mAutoCloseComment, 500);
                 }
             }
         }
@@ -158,10 +162,11 @@ public class WaveformControllerLand extends WaveformController {
                 lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
                 addView(mCommentPanel, lp);
 
-                toggleWaveformHalf(true);
-                toggleCommentsPanelVisibility(true);
+
             }
 
+            toggleWaveformHalf(true);
+            toggleCommentsPanelVisibility(true);
             mCommentPanel.showComment(mCurrentShowingComment);
         }
     }
@@ -170,15 +175,33 @@ public class WaveformControllerLand extends WaveformController {
 
         if (half && !mWaveformHalf) {
             mWaveformHalf = true;
-            Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
-                    Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
-                    Animation.RELATIVE_TO_SELF, 0.5f);
-            animation.setFillEnabled(true);
-            animation.setFillAfter(true);
-            animation.setDuration(COMMENT_ANIMATE_DURATION);
-            mWaveformHolder.startAnimation(animation);
+            if (mWaveformHolder.getAnimation() != null && !mWaveformHolder.getAnimation().hasEnded()){
+                //calculate new distances and durations based on unfinished animation
+                final float transY =getAnimationTransY(mWaveformHolder.getAnimation());
+                final long duration = (long) Math.max(0,(mWaveformHolder.getHeight()/2-transY)/(mWaveformHolder.getHeight()/2) * COMMENT_ANIMATE_DURATION);
+
+                mWaveformHolder.clearAnimation();
+                Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                        Animation.RELATIVE_TO_SELF, 0.0f, Animation.ABSOLUTE, mWaveformHolder.getTop() + transY,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                animation.setDuration(duration);
+                animation.setFillEnabled(true);
+                animation.setFillAfter(true);
+                mWaveformHolder.startAnimation(animation);
+            } else {
+                 Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                        Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                animation.setFillEnabled(true);
+                animation.setFillAfter(true);
+                animation.setDuration(COMMENT_ANIMATE_DURATION);
+                mWaveformHolder.startAnimation(animation);
+            }
 
         } else if (!half && mWaveformHalf) {
+            if (mWaveformHolder.getAnimation() != null){
+                mWaveformHolder.clearAnimation();
+            }
             mWaveformHalf = false;
             Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f,
@@ -191,16 +214,36 @@ public class WaveformControllerLand extends WaveformController {
     }
 
     private void toggleCommentsPanelVisibility(boolean visible) {
-        if (mCommentPanel == null) return;
 
         if (visible && !mCommentPanelVisible) {
+            mCommentPanelVisible = true;
+            clearDisappearingChildren();
+            cancelAutoCloseComment();
+
             if (mCommentPanel.getParent() != this) addView(mCommentPanel);
-            Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
-                    Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, -1.0f,
-                    Animation.RELATIVE_TO_SELF, 0.0f);
-            animation.setDuration(COMMENT_ANIMATE_DURATION);
-            mCommentPanel.startAnimation(animation);
+
+            if (mCommentPanel.getAnimation() != null && ! mCommentPanel.getAnimation().hasEnded()) {
+                //calculate new distances and durations based on unfinished animation
+                final float transY =getAnimationTransY(mCommentPanel.getAnimation());
+                final long duration = (long) (-transY/(mCommentPanel.getHeight()) * COMMENT_ANIMATE_DURATION);
+
+                mCommentPanel.clearAnimation();
+                Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                        Animation.RELATIVE_TO_SELF, 0.0f, Animation.ABSOLUTE, mCommentPanel.getTop() + transY,
+                        Animation.RELATIVE_TO_SELF, 0.0f);
+                animation.setDuration(duration);
+                mCommentPanel.startAnimation(animation);
+            } else {
+                Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                        Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, -1.0f,
+                        Animation.RELATIVE_TO_SELF, 0.0f);
+                animation.setDuration(COMMENT_ANIMATE_DURATION);
+                mCommentPanel.startAnimation(animation);
+            }
+
         } else if (!visible && mCommentPanelVisible && mCommentPanel.getParent() == this) {
+            if (mCommentPanel.getAnimation() != null) mCommentPanel.getAnimation().cancel();
+            mCommentPanelVisible = false;
             Animation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, -1.0f);
@@ -210,12 +253,24 @@ public class WaveformControllerLand extends WaveformController {
         }
     }
 
+    private float getAnimationTransY(Animation a) {
+        Transformation outTransformation = new Transformation();
+        a.getTransformation(mWaveformHolder.getDrawingTime(), outTransformation);
+
+        Matrix transformationMatrix = outTransformation.getMatrix();
+        float[] matrixValues = new float[9];
+        transformationMatrix.getValues(matrixValues);
+        return matrixValues[Matrix.MTRANS_Y];
+    }
+
     @Override
     public void closeComment() {
-        super.closeComment();
+        mCurrentShowingComment = null;
+                if (mPlayerAvatarBar != null) mPlayerAvatarBar.setCurrentComment(null);
+                if (mCommentLines != null) mCommentLines.setCurrentComment(null);
+
         toggleWaveformHalf(false);
         toggleCommentsPanelVisibility(false);
-        mCommentPanel = null;
     }
 
     @Override
@@ -223,6 +278,7 @@ public class WaveformControllerLand extends WaveformController {
 
         if (commenting){
             toggleWaveformHalf(true);
+            toggleCommentsPanelVisibility(false);
             ((RelativeLayout.LayoutParams) mCurrentTimeDisplay.getLayoutParams()).addRule(RelativeLayout.ALIGN_PARENT_TOP);
             ((RelativeLayout.LayoutParams) mCurrentTimeDisplay.getLayoutParams()).addRule(RelativeLayout.ABOVE,0);
             mCurrentTimeDisplay.setCommentingHeight(mWaveformHolder.getHeight()/2);
