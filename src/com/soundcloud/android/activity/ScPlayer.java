89c85b24d1380de66f88ec086124f173a10602ad
Jan Berkel <jan.berkel@gmail.com>
2011-10-10 11:11:01 +0100

    Added test for player tracking

--- src/com/soundcloud/android/activity/ScPlayer.java
+++ src/com/soundcloud/android/activity/ScPlayer.java
@@ -629,6 +629,8 @@ public class ScPlayer extends ScActivity implements OnTouchListener, LoadTrackIn
     }
 
     private void setPauseButtonImage() {
+        if (mPauseButton == null) return;
+
         try {
             if (mPlaybackService != null && mPlaybackService.isPlaying()) {
                 mPauseButton.setImageDrawable(mPauseState);
@@ -1174,7 +1176,9 @@ public class ScPlayer extends ScActivity implements OnTouchListener, LoadTrackIn
     }
 
     private void registerHeadphoneRemoteControl() {
-        if (mRegisterMediaButtonEventReceiver == null) return;
+        if (mRegisterMediaButtonEventReceiver == null ||
+            mAudioManager == null ||
+            mRemoteControlResponder == null) return;
 
         try {
             mRegisterMediaButtonEventReceiver.invoke(mAudioManager, mRemoteControlResponder);
