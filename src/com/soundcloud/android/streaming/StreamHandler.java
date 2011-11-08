6a695ff5487ed2523b581e70ab20f347715acd0b
Jan Berkel <jan.berkel@gmail.com>
2011-11-08 21:46:16 +0100

    Exp backoff

--- src/com/soundcloud/android/streaming/StreamHandler.java
+++ src/com/soundcloud/android/streaming/StreamHandler.java
@@ -54,12 +54,14 @@ class StreamHandler extends Handler {
             Log.w(StreamLoader.LOG_TAG, "unknown host exception - not retrying");
         } catch (IOException e) {
             Log.w(StreamLoader.LOG_TAG, e);
-            if (task.item.isAvailable() && msg.arg1 < mMaxRetries) {
-                if (Log.isLoggable(StreamLoader.LOG_TAG, Log.DEBUG))
-                    Log.d(StreamLoader.LOG_TAG, "retrying, tries=" + msg.arg1);
 
-                final long backoff = msg.arg1*msg.arg1*150;
-                sendMessageDelayed(obtainMessage(msg.what, msg.arg1+1, 0, msg.obj), backoff);
+            final int numTry = msg.arg1;
+            if (task.item.isAvailable() && numTry < mMaxRetries) {
+                if (Log.isLoggable(StreamLoader.LOG_TAG, Log.DEBUG))
+                    Log.d(StreamLoader.LOG_TAG, "retrying, tries=" + numTry);
+
+                final long backoff = numTry*numTry*1000;
+                sendMessageDelayed(obtainMessage(msg.what, numTry+1, 0, msg.obj), backoff);
             } else {
                 Log.w(StreamLoader.LOG_TAG, "giving up (max tries="+mMaxRetries+")");
             }
