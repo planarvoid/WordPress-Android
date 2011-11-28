e6aa0fa9d08953f7d7598ea5f543c1604f9d987f
Jan Berkel <jan.berkel@gmail.com>
2011-11-28 12:37:16 +0100

    Re-enable betaservice test

--- tests/src/com/soundcloud/android/service/beta/BetaServiceTest.java
+++ tests/src/com/soundcloud/android/service/beta/BetaServiceTest.java
@@ -1,29 +1,22 @@
 package com.soundcloud.android.service.beta;
 
-import static android.content.Context.WIFI_SERVICE;
 import static com.soundcloud.android.Expect.expect;
 import static com.xtremelabs.robolectric.Robolectric.addPendingHttpResponse;
-import static com.xtremelabs.robolectric.Robolectric.application;
 import static com.xtremelabs.robolectric.Robolectric.newInstanceOf;
 
-import com.soundcloud.android.Expect;
 import com.soundcloud.android.robolectric.DefaultTestRunner;
 import com.xtremelabs.robolectric.Robolectric;
-import com.xtremelabs.robolectric.shadows.ShadowConnectivityManager;
 import com.xtremelabs.robolectric.shadows.ShadowEnvironment;
-import com.xtremelabs.robolectric.shadows.ShadowService;
 import org.apache.http.Header;
 import org.apache.http.message.BasicHeader;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 import android.os.Environment;
 
 import java.io.IOException;
@@ -31,16 +24,15 @@ import java.io.InputStream;
 
 
 @RunWith(DefaultTestRunner.class)
-@Ignore
 public class BetaServiceTest {
     BetaService bs;
-/*
     @Before
     public void before() {
         ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
-        ShadowConnectivityManager.setBackgroundDataSettingEnabled(true);
         ConnectivityManager cm = (ConnectivityManager)
                 Robolectric.application.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        Robolectric.shadowOf(cm).setBackgroundDataSetting(true);
         Robolectric.shadowOf(cm).setNetworkInfo(ConnectivityManager.TYPE_WIFI,
                 newInstanceOf(NetworkInfo.class));
 
@@ -64,7 +56,6 @@ public class BetaServiceTest {
                 "x-amz-meta-git-sha1",  "e800b4bedadc6308ebcf72c566bf95d7b9cee30f"));
 
 
-
         bs.onStartCommand(null, 0, 0);
         expect(Robolectric.shadowOf(bs).isStoppedBySelf()).toBeTrue();
     }
@@ -85,5 +76,4 @@ public class BetaServiceTest {
         }
         return headers;
     }
-*/
 }
