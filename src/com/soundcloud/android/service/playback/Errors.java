2e7cedfbdf8b1e8b71581ca69cf404c8042f3c6f
Jan Berkel <jan.berkel@gmail.com>
2011-12-01 00:32:52 +0100

    Interface

--- src/com/soundcloud/android/service/playback/Errors.java
+++ src/com/soundcloud/android/service/playback/Errors.java
@@ -1,18 +1,17 @@
 package com.soundcloud.android.service.playback;
 
-public class Errors {
-
+/** @noinspection UnusedDeclaration*/
+public interface Errors {
     // include/media/stagefright/MediaErrors.h
-    static final int STAGEFRIGHT_ERROR_IO = -1004;
-    static final int STAGEFRIGHT_ERROR_CONNECTION_LOST = -1005;
+    int STAGEFRIGHT_ERROR_IO = -1004;
+    int STAGEFRIGHT_ERROR_CONNECTION_LOST = -1005;
 
     // external/opencore/pvmi/pvmf/include/pvmf_return_codes.h
     // Return code for general failure
-    static final int OPENCORE_PVMFFailure = -1;
+    int OPENCORE_PVMFFailure = -1;
     // Error due to request timing out
-    static final int OPENCORE_PVMFErrTimeout = -11;
+    int OPENCORE_PVMFErrTimeout = -11;
 
     // Custom error for lack of MP error reporting on buffer run out
-    static final int STAGEFRIGHT_ERROR_BUFFER_EMPTY = 99;
-
+    int STAGEFRIGHT_ERROR_BUFFER_EMPTY = 99;
 }
