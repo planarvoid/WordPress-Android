a4462234db7b05f3a1e461c3f48c4160a001fe77
Jon Schmidt <jon@soundcloud.com>
2012-01-19 16:54:46 +0100

    storing activities after appending

--- src/com/soundcloud/android/task/LoadActivitiesTask.java
+++ src/com/soundcloud/android/task/LoadActivitiesTask.java
@@ -1,14 +1,20 @@
 package com.soundcloud.android.task;
 
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.Log;
 import com.soundcloud.android.SoundCloudApplication;
 import com.soundcloud.android.adapter.EventsAdapterWrapper;
 import com.soundcloud.android.adapter.LazyEndlessAdapter;
 import com.soundcloud.android.model.Activities;
 import com.soundcloud.android.model.Activity;
+import com.soundcloud.android.model.CollectionHolder;
+import com.soundcloud.android.model.ScModel;
 import com.soundcloud.android.provider.Content;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.soundcloud.android.SoundCloudApplication.TAG;
@@ -49,5 +55,35 @@ public class LoadActivitiesTask extends RemoteCollectionTask {
         } else {
             throw new IllegalArgumentException("Incorrect paramaters");
         }
+    }
+
+    protected boolean doRemoteLoad() {
+        try {
+            HttpResponse resp = mApp.get(mParams.request);
+            mResponseCode = resp.getStatusLine().getStatusCode();
+            if (mResponseCode != HttpStatus.SC_OK) {
+                throw new IOException("Invalid response: " + resp.getStatusLine());
+            }
+
+            Activities activities = mApp.getMapper().readValue(resp.getEntity().getContent(), Activities.class);
+            mNextHref = activities == null || TextUtils.isEmpty(activities.next_href) ? null : activities.next_href;
+            keepGoing = !TextUtils.isEmpty(mNextHref);
+
+            for (Parcelable p : activities) {
+                ((ScModel) p).resolve(mApp);
+                mNewItems.add(p);
+            }
+
+            // publish what we have, since we already have the items, we don't have to wait on a db commit
+            publishProgress(mNewItems);
+
+            activities.insert(Content.match(mParams.contentUri),mApp.getContentResolver());
+            return true;
+
+        } catch (IOException e) {
+            Log.e(TAG, "error", e);
+            keepGoing = false;
+        }
+        return false;
     }
 }
