78d00967d7a819926c6363771b2fa775d5f47f7f
Jan Berkel <jan.berkel@gmail.com>
2012-01-24 03:37:27 +0100

    Rename

--- tests/src/java/com/soundcloud/android/provider/SoundCloudDBTest.java
+++ tests/src/java/com/soundcloud/android/provider/SoundCloudDBTest.java
@@ -13,7 +13,7 @@ import android.content.ContentResolver;
 import android.net.Uri;
 
 @RunWith(DefaultTestRunner.class)
-public class SoundCloudDBFooTest {
+public class SoundCloudDBTest {
     ContentResolver resolver;
     final static long USER_ID = 1L;
 
