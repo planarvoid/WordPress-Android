a38447391670114a7143365d861a606525d64daf
Jon Schmidt <jon@soundcloud.com>
2012-04-05 13:14:01 +0200

    only use text links if in widget

--- src/com/soundcloud/android/view/PlaybackRemoteViews.java
+++ src/com/soundcloud/android/view/PlaybackRemoteViews.java
@@ -39,38 +39,42 @@ public class PlaybackRemoteViews extends RemoteViews{
         linkButtons(context,trackId,userId,userFavorite,null);
     }
 
-    public void linkButtons(Context context, long trackId, long userId, boolean userFavorite, String extraFlag) {
+    public void linkButtons(Context context, long trackId, long userId, boolean userFavorite, String fromNotificationFlag) {
         // Connect up various buttons and touch events
         final ComponentName name = new ComponentName(context, CloudPlaybackService.class);
         final Intent previous = new Intent(CloudPlaybackService.PREVIOUS_ACTION).setComponent(name);
-        if (!TextUtils.isEmpty(extraFlag)) previous.putExtra(extraFlag,true);
+        if (!TextUtils.isEmpty(fromNotificationFlag)) previous.putExtra(fromNotificationFlag,true);
         setOnClickPendingIntent(R.id.prev, PendingIntent.getService(context,
                 0 /* requestCode */, previous, 0 /* flags */));
 
         final Intent toggle = new Intent(CloudPlaybackService.TOGGLEPAUSE_ACTION).setComponent(name);
-        if (!TextUtils.isEmpty(extraFlag)) toggle.putExtra(extraFlag,true);
+        if (!TextUtils.isEmpty(fromNotificationFlag)) toggle.putExtra(fromNotificationFlag,true);
         setOnClickPendingIntent(R.id.pause, PendingIntent.getService(context,
                 0 /* requestCode */, toggle, 0 /* flags */));
 
         final Intent next = new Intent(CloudPlaybackService.NEXT_ACTION).setComponent(name);
-        if (!TextUtils.isEmpty(extraFlag)) next.putExtra(extraFlag,true);
+        if (!TextUtils.isEmpty(fromNotificationFlag)) next.putExtra(fromNotificationFlag,true);
         setOnClickPendingIntent(R.id.next, PendingIntent.getService(context,
                 0 /* requestCode */, next, 0 /* flags */));
 
-        final Intent player = new Intent(Actions.PLAYER).addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
-        if (!TextUtils.isEmpty(extraFlag)) player.putExtra(extraFlag,true);
-        setOnClickPendingIntent(R.id.title_txt, PendingIntent.getActivity(context, 0, player, 0));
-
         final Intent close = new Intent(CloudPlaybackService.STOP_ACTION).setComponent(name);
-        if (!TextUtils.isEmpty(extraFlag)) close.putExtra(extraFlag,true);
+        if (!TextUtils.isEmpty(fromNotificationFlag)) close.putExtra(fromNotificationFlag,true);
         setOnClickPendingIntent(R.id.close, PendingIntent.getService(context,
                 0 /* requestCode */, close, 0 /* flags */));
 
+        if (fromNotificationFlag == null){
+            final Intent player = new Intent(Actions.PLAYER).addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+            if (!TextUtils.isEmpty(fromNotificationFlag)) player.putExtra(fromNotificationFlag,true);
+            setOnClickPendingIntent(R.id.title_txt, PendingIntent.getActivity(context, 0, player, 0));
+        }
+
         if (trackId != -1) {
-            final Intent browser = new Intent(context, UserBrowser.class).putExtra("userId", userId);
-            if (!TextUtils.isEmpty(extraFlag)) browser.putExtra(extraFlag,true);
-            setOnClickPendingIntent(R.id.user_txt,
-                    PendingIntent.getActivity(context, 0, browser, PendingIntent.FLAG_UPDATE_CURRENT));
+            if (fromNotificationFlag == null){
+                final Intent browser = new Intent(context, UserBrowser.class).putExtra("userId", userId);
+                if (!TextUtils.isEmpty(fromNotificationFlag)) browser.putExtra(fromNotificationFlag,true);
+                setOnClickPendingIntent(R.id.user_txt,
+                        PendingIntent.getActivity(context, 0, browser, PendingIntent.FLAG_UPDATE_CURRENT));
+            }
 
             final Intent toggleLike = new Intent(
                     userFavorite ?
@@ -78,7 +82,7 @@ public class PlaybackRemoteViews extends RemoteViews{
                             CloudPlaybackService.ADD_FAVORITE)
                     .setComponent(name)
                     .putExtra("trackId", trackId);
-            if (!TextUtils.isEmpty(extraFlag)) toggleLike.putExtra(extraFlag,true);
+            if (!TextUtils.isEmpty(fromNotificationFlag)) toggleLike.putExtra(fromNotificationFlag,true);
             setOnClickPendingIntent(R.id.btn_favorite, PendingIntent.getService(context,
                     0 /* requestCode */, toggleLike, PendingIntent.FLAG_UPDATE_CURRENT));
         }
