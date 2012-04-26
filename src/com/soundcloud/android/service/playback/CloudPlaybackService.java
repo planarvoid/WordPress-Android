821ad31b768dd5bcb94d28d8c6b71e727d6ef564
Jan Berkel <jan.berkel@gmail.com>
2012-04-26 12:20:35 +0200

    Use intents in RemoteControlReceiver - more reliable

--- src/com/soundcloud/android/service/playback/CloudPlaybackService.java
+++ src/com/soundcloud/android/service/playback/CloudPlaybackService.java
@@ -51,15 +51,16 @@ public class CloudPlaybackService extends Service implements AudioManagerHelper.
     public static final String TAG = "CloudPlaybackService";
     public static List<Playable> playlistXfer;
 
-    // public service actions TODO also declare in AndroidManifest.xml
-    public static final String PLAY_ACTION          = "com.soundcloud.android.musicservicecommand.play";
-    public static final String TOGGLEPAUSE_ACTION   = "com.soundcloud.android.musicservicecommand.togglepause";
-    public static final String NEXT_ACTION          = "com.soundcloud.android.musicservicecommand.next";
-    public static final String PREVIOUS_ACTION      = "com.soundcloud.android.musicservicecommand.previous";
+    // public service actions
+    public static final String PLAY_ACTION          = "com.soundcloud.android.playback.play";
+    public static final String TOGGLEPAUSE_ACTION   = "com.soundcloud.android.playback.togglepause";
+    public static final String PAUSE_ACTION         = "com.soundcloud.android.playback.pause";
+    public static final String NEXT_ACTION          = "com.soundcloud.android.playback.next";
+    public static final String PREVIOUS_ACTION      = "com.soundcloud.android.playback.previous";
 
-    public static final String RESET_ALL            = "com.soundcloud.android.musicservicecommand.resetall"; // used on logout
-    public static final String CLOSE_ACTION         = "com.soundcloud.android.musicservicecommand.close";    // from the notification
-    public static final String UPDATE_WIDGET_ACTION = "com.soundcloud.android.musicservicecommand.updatewidgetaction";
+    public static final String RESET_ALL            = "com.soundcloud.android.playback.reset"; // used on logout
+    public static final String CLOSE_ACTION         = "com.soundcloud.android.playback.close"; // from the notification
+    public static final String UPDATE_WIDGET_ACTION = "com.soundcloud.android.playback.updatewidgetaction";
 
     public static final String ADD_FAVORITE_ACTION    = "com.soundcloud.android.favorite.add";
     public static final String REMOVE_FAVORITE_ACTION = "com.soundcloud.android.favorite.remove";
@@ -79,8 +80,8 @@ public class CloudPlaybackService extends Service implements AudioManagerHelper.
     public static final String BUFFERING_COMPLETE = "com.soundcloud.android.bufferingcomplete";
 
     // extras
-    public static final String EXTRA_UNMUTE       = "com.soundcloud.android.musicserviceextra.unmute"; // used by alarm clock
-    public static final String EXTRA_TRACK_ID     = "trackId";
+    public static final String EXTRA_UNMUTE       = "com.soundcloud.android.playback.extra.unmute"; // used by alarm clock
+    public static final String EXTRA_TRACK_ID     = "com.soundcloud.android.playback.extra.trackId";
 
     // private stuff
     private static final int TRACK_ENDED      = 1;
@@ -162,6 +163,7 @@ public class CloudPlaybackService extends Service implements AudioManagerHelper.
 
         IntentFilter commandFilter = new IntentFilter();
         commandFilter.addAction(TOGGLEPAUSE_ACTION);
+        commandFilter.addAction(PAUSE_ACTION);
         commandFilter.addAction(NEXT_ACTION);
         commandFilter.addAction(PREVIOUS_ACTION);
         commandFilter.addAction(ADD_FAVORITE_ACTION);
@@ -929,6 +931,8 @@ public class CloudPlaybackService extends Service implements AudioManagerHelper.
                 } else {
                     openCurrent();
                 }
+            } else if (PAUSE_ACTION.equals(action)) {
+                pause();
             } else if (UPDATE_WIDGET_ACTION.equals(action)) {
                 // Someone asked us to executeRefreshTask a set of specific widgets,
                 // probably because they were just added.
--- src/com/soundcloud/android/service/playback/RemoteControlReceiver.java
+++ src/com/soundcloud/android/service/playback/RemoteControlReceiver.java
@@ -1,76 +1,56 @@
 package com.soundcloud.android.service.playback;
 
-import static com.soundcloud.android.SoundCloudApplication.TAG;
-
-import com.soundcloud.android.service.playback.CloudPlaybackService;
-import com.soundcloud.android.service.playback.ICloudPlaybackService;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.RemoteException;
-import android.util.Log;
 import android.view.KeyEvent;
 
-@SuppressWarnings({"UnusedDeclaration"})
 public class RemoteControlReceiver extends BroadcastReceiver {
+    public static final String AUDIO_BECOMING_NOISY = "android.media.AUDIO_BECOMING_NOISY";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        ICloudPlaybackService svc = (ICloudPlaybackService) peekService(context,
-                new Intent(context, CloudPlaybackService.class));
-
         if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
             KeyEvent event = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
             if (event != null && event.getAction() == KeyEvent.ACTION_UP) {
-                if (svc != null) {
-                    try {
-                        switch (event.getKeyCode()) {
-                            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-                            case KeyEvent.KEYCODE_HEADSETHOOK:
-                                handleToggle(context, svc);
-                                break;
-                            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
-                                handlePreviousTrack(context, svc);
-                                break;
-                            case KeyEvent.KEYCODE_MEDIA_NEXT:
-                                handleNextTrack(context, svc);
-                                break;
-                            case KeyEvent.KEYCODE_MEDIA_REWIND:
-                                handleRewind(context, svc);
-                                break;
-                        }
-                    } catch (RemoteException ignored) {
-                        Log.w(TAG, ignored);
-                    }
+                switch (event.getKeyCode()) {
+                    case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+                    case KeyEvent.KEYCODE_HEADSETHOOK:
+                        handleToggle(context);
+                        break;
+                    case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                        handlePreviousTrack(context);
+                        break;
+                    case KeyEvent.KEYCODE_MEDIA_NEXT:
+                        handleNextTrack(context);
+                        break;
+                    case KeyEvent.KEYCODE_MEDIA_REWIND:
+                        handleRewind(context);
+                        break;
                 }
             }
-        } else if ("android.media.AUDIO_BECOMING_NOISY".equals(intent.getAction())) {
-            if (svc != null) {
-                try {
-                    if (svc.isPlaying()) svc.pause();
-                } catch (RemoteException ignored) {
-                }
-            }
+        } else if (AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
+            handlePause(context);
         }
     }
 
-    private void handleRewind(Context context, ICloudPlaybackService svc) throws RemoteException {
-        svc.restart();
+    @SuppressWarnings("UnusedParameters")
+    private void handleRewind(Context context) {
     }
 
-    private void handlePause(Context context, ICloudPlaybackService svc) throws RemoteException {
-        svc.pause();
+    private void handlePause(Context context)  {
+        context.startService(new Intent(CloudPlaybackService.PAUSE_ACTION));
     }
 
-    private void handleToggle(Context context, ICloudPlaybackService svc) throws RemoteException {
-        svc.toggle();
+    private void handleToggle(Context context) {
+        context.startService(new Intent(CloudPlaybackService.TOGGLEPAUSE_ACTION));
     }
 
-    private void handleNextTrack(Context context, ICloudPlaybackService svc) throws RemoteException {
-        svc.next();
+    private void handleNextTrack(Context context) {
+        context.startService(new Intent(CloudPlaybackService.NEXT_ACTION));
     }
 
-    private void handlePreviousTrack(Context context, ICloudPlaybackService svc) throws RemoteException {
-        svc.prev();
+    private void handlePreviousTrack(Context context)  {
+        context.startService(new Intent(CloudPlaybackService.PREVIOUS_ACTION));
     }
 }
