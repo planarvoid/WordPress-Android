f99fd0b77764e52d3cb84116562c09bbcd77376d
Jon Schmidt <jon@soundcloud.com>
2012-07-18 10:55:37 +0200

    fixed client key test

--- tests/src/test/java/com/soundcloud/android/AppTest.java
+++ tests/src/test/java/com/soundcloud/android/AppTest.java
@@ -18,11 +18,11 @@ public class AppTest {
     @Test
     public void shouldDeobfuscateClientSecret() throws Exception {
         // live
-        expect(AndroidCloudAPI.Wrapper.getClientSecret(Env.LIVE))
+        expect(AndroidCloudAPI.Wrapper.getClientSecret(true))
                 .toEqual("26a5240f7ee0ee2d4fa9956ed80616c2");
 
         // sandbox
-        expect(AndroidCloudAPI.Wrapper.getClientSecret(Env.SANDBOX))
+        expect(AndroidCloudAPI.Wrapper.getClientSecret(false))
                 .toEqual("0000000pGDzQNAPHzBH6hBTHphl4Q1e9");
     }
 }
