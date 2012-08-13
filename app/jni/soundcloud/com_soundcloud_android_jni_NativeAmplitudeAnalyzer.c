57cc484920df5e62f58bb2b848d62af6cb5b6e5f
Jan Berkel <jan@soundcloud.com>
2012-08-13 15:55:51 +0200

    Amplitude analyzer: respect audio config

--- app/jni/soundcloud/com_soundcloud_android_jni_NativeAmplitudeAnalyzer.c
+++ app/jni/soundcloud/com_soundcloud_android_jni_NativeAmplitudeAnalyzer.c
@@ -4,8 +4,9 @@
 #define DEBUG_TAG "NativeAmplitudeAnalyzer"
 #include <common.h>
 
-static int getMax(jbyte *buf, jint length);
+static int getMax(jbyte *buf, jint length, const int channels, const int bytesPerSample);
 static jfieldID last_max, current_adjusted_max_amplitude;
+static jfieldID channels, bytes_per_sample;
 
 static float amp_max;
 
@@ -16,7 +17,10 @@ jobject obj, jobject buffer, jint length) {
 
     jbyte *buf = (jbyte*) (*env)->GetDirectBufferAddress(env, buffer);
 
-    int max = getMax(buf, length);
+    const int numChannels = (*env)->GetIntField(env, obj, channels);
+    const int numBytesPerSample = (*env)->GetIntField(env, obj, bytes_per_sample);
+
+    int max = getMax(buf, length, numChannels, numBytesPerSample);
 
     if (max == 0) {
         max = lmax;
@@ -39,16 +43,22 @@ jobject obj, jobject buffer, jint length) {
     return amp < 0.1 ? 0.1 : amp;
 }
 
-static int getMax(jbyte *buf, jint length) {
-    // TODO: get channel config from fields
+static int getMax(jbyte *buf, jint length, const int channels, const int bytesPerSample) {
     int i, j;
-    const int bytesPerSample = 2;
-    const int channels = 1;
-    int max = 0;
-    for (i=0; i < length; i += bytesPerSample) {
-        int value = buf[i+1]<<8 | (0x00ff & (int)buf[i]);
-        if (value > max) {
-            max = value;
+    int value = 0, max = 0;
+    for (i=0; i < length / bytesPerSample; i++) {
+        for(j=0; j < channels; j++) {
+            switch (bytesPerSample) {
+                case 2:
+                    value = buf[i*bytesPerSample+1+(j*2)]<<8 | (0x00ff & (int)buf[i*bytesPerSample+j*2]);
+                    break;
+                case 1:
+                    value = buf[i];
+                    break;
+            }
+            if (value > max) {
+                max = value;
+            }
         }
     }
     return max;
@@ -72,6 +82,8 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 
        last_max = (*env)->GetFieldID(env, cls, "last_max", "I");
        current_adjusted_max_amplitude = (*env)->GetFieldID(env, cls, "current_adjusted_max_amplitude", "I");
+       channels = (*env)->GetFieldID(env, cls, "channels", "I");
+       bytes_per_sample = (*env)->GetFieldID(env, cls, "bytes_per_sample", "I");
 
        if (!last_max || !current_adjusted_max_amplitude) {
           LOG_E("JNI_OnLoad: could not initialize fields");
