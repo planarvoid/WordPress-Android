ef5656f611602a885b06d4778451c173f1ebf91c
Jan Berkel <jan@soundcloud.com>
2012-08-15 14:44:15 +0200

    Add delete all recordings dev setting

--- app/src/main/java/com/soundcloud/android/activity/settings/DevSettings.java
+++ app/src/main/java/com/soundcloud/android/activity/settings/DevSettings.java
@@ -3,16 +3,23 @@ package com.soundcloud.android.activity.settings;
 import com.soundcloud.android.Actions;
 import com.soundcloud.android.R;
 import com.soundcloud.android.SoundCloudApplication;
+import com.soundcloud.android.provider.Content;
+import com.soundcloud.android.record.SoundRecorder;
 import com.soundcloud.android.service.sync.SyncAdapterService;
 import com.soundcloud.android.utils.AndroidUtils;
+import com.soundcloud.android.utils.IOUtils;
 import com.soundcloud.android.utils.SharedPreferencesUtils;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.text.TextUtils;
-import android.widget.Toast;
+ import android.widget.Toast;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,6 +31,7 @@ public final class DevSettings {
     public static final String DEV_REWIND_NOTIFICATIONS = "dev.rewindNotifications";
     public static final String DEV_SYNC_NOW             = "dev.syncNow";
     public static final String DEV_CRASH                = "dev.crash";
+    public static final String DEV_CLEAR_RECORDINGS     = "dev.clearRecordings";
     public static final String DEV_HTTP_PROXY           = "dev.http.proxy";
     public static final String DEV_RECORDING_TYPE       = "dev.defaultRecordingType";
     public static final String DEV_RECORDING_TYPE_RAW   = "raw";
@@ -97,7 +105,48 @@ public final class DevSettings {
                 }
         );
 
+        activity.findPreference(DEV_CLEAR_RECORDINGS).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new AlertDialog.Builder(activity).setMessage(R.string.dev_clear_recordings)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                new Thread(new DeleteRecordings(activity.getApplicationContext())).start();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null)
+                        .create()
+                        .show();
+                return false;
+            }
+        });
+
         SharedPreferencesUtils.listWithLabel((ListPreference) activity.findPreference(DEV_RECORDING_TYPE),
                 R.string.pref_dev_record_type);
+    }
+
+    private static class DeleteRecordings implements Runnable {
+        private final Context mContext;
+        private Handler handler = new Handler();
+
+        private DeleteRecordings(Context context) {
+            this.mContext = context;
+        }
+
+        @Override
+        public void run() {
+            synchronized (DeleteRecordings.class) {
+                IOUtils.deleteDir(SoundRecorder.RECORD_DIR);
+                IOUtils.mkdirs(SoundRecorder.RECORD_DIR);
+                final int count = mContext.getContentResolver().delete(Content.RECORDINGS.uri, null, null);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        AndroidUtils.showToast(mContext, "Deleted " + count + " recordings");
+                    }
+                });
+            }
+        }
     }
 }
