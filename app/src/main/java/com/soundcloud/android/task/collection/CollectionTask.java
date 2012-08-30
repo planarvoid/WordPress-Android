e89b4b793c50ecd16464c926759c956a663f928d
Jon Schmidt <jon@soundcloud.com>
2012-08-30 18:44:54 +0200

    rid of generics

--- app/src/main/java/com/soundcloud/android/task/collection/CollectionTask.java
+++ app/src/main/java/com/soundcloud/android/task/collection/CollectionTask.java
@@ -4,14 +4,13 @@ import static com.soundcloud.android.SoundCloudApplication.TAG;
 
 import com.soundcloud.android.AndroidCloudAPI;
 import com.soundcloud.android.model.Activity;
-import com.soundcloud.android.model.ScModel;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
 
-public class CollectionTask<T extends ScModel> extends AsyncTask<CollectionParams<T>, ReturnData, ReturnData> {
+public class CollectionTask extends AsyncTask<CollectionParams, ReturnData, ReturnData> {
     private AndroidCloudAPI mApi;
     private WeakReference<Callback> mCallback;
 
@@ -33,12 +32,12 @@ public class CollectionTask<T extends ScModel> extends AsyncTask<CollectionParam
     }
 
     @Override
-    protected ReturnData<T> doInBackground(CollectionParams... xparams) {
+    protected ReturnData doInBackground(CollectionParams... xparams) {
         CollectionParams params = xparams[0];
         Log.d(TAG, getClass().getSimpleName() + "Loading collection with params: " + params);
 
         if (Activity.class.isAssignableFrom(params.getContent().resourceType)) {
-            return (ReturnData<T>) new ActivitiesLoader().load(mApi, params);
+            return new ActivitiesLoader().load(mApi, params);
         } else if (params.getContent().isMine()) {
             return new MyCollectionLoader().load(mApi, params);
 
