4b829873169c1c19794bc4086071c9dcc36f764b
Jan Berkel <jan@soundcloud.com>
2012-10-05 16:20:08 +0200

    continue

--- app/src/main/java/com/soundcloud/android/provider/SoundCloudDB.java
+++ app/src/main/java/com/soundcloud/android/provider/SoundCloudDB.java
@@ -156,37 +156,37 @@ public class SoundCloudDB {
 
         for (int i=0; i <items.size(); i++) {
             Parcelable p = items.get(i);
-            if (p != null){
-                long id = ((ScModel) p).id;
-                if (p instanceof Origin) {
-                    Origin origin = (Origin) p;
-                    Track track = origin.getTrack();
-                    if (track != null) {
-                        tracksToInsert.add(track);
-                    }
-                    User user = origin.getUser();
-                    if (user != null) {
-                        usersToInsert.add(user);
-                    }
-                }
+            if (p == null) continue;
 
-                if (uri != null) {
-                    ContentValues cv = new ContentValues();
-                    switch (Content.match(uri)) {
-                        case PLAYLIST:
-                            cv.put(DBHelper.PlaylistItems.USER_ID, ownerId);
-                            cv.put(DBHelper.PlaylistItems.POSITION, i);
-                            cv.put(DBHelper.PlaylistItems.TRACK_ID, id);
-                            break;
-
-                        default:
-                            cv.put(DBHelper.CollectionItems.USER_ID, ownerId);
-                            cv.put(DBHelper.CollectionItems.POSITION, i);
-                            cv.put(DBHelper.CollectionItems.ITEM_ID, id);
-                            break;
-                    }
-                    bulkValues[i] = cv;
+            long id = ((ScModel) p).id;
+            if (p instanceof Origin) {
+                Origin origin = (Origin) p;
+                Track track = origin.getTrack();
+                if (track != null) {
+                    tracksToInsert.add(track);
                 }
+                User user = origin.getUser();
+                if (user != null) {
+                    usersToInsert.add(user);
+                }
+            }
+
+            if (uri != null) {
+                ContentValues cv = new ContentValues();
+                switch (Content.match(uri)) {
+                    case PLAYLIST:
+                        cv.put(DBHelper.PlaylistItems.USER_ID, ownerId);
+                        cv.put(DBHelper.PlaylistItems.POSITION, i);
+                        cv.put(DBHelper.PlaylistItems.TRACK_ID, id);
+                        break;
+
+                    default:
+                        cv.put(DBHelper.CollectionItems.USER_ID, ownerId);
+                        cv.put(DBHelper.CollectionItems.POSITION, i);
+                        cv.put(DBHelper.CollectionItems.ITEM_ID, id);
+                        break;
+                }
+                bulkValues[i] = cv;
             }
         }
 
