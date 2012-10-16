85d80f7a36d66ae4105ff8e62edd193e864ec748
Jon Schmidt <jon@soundcloud.com>
2012-10-16 11:04:27 +0200

    landscape/transport/actionbar layout rework

--- app/src/main/java/com/soundcloud/android/view/play/TransportBar.java
+++ app/src/main/java/com/soundcloud/android/view/play/TransportBar.java
@@ -14,7 +14,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class TransportBar extends LinearLayout{
-    private ImageButton mPauseButton, mFavoriteButton, mCommentButton, mPrevButton, mNextButton;
+    private ImageButton mPauseButton, mPrevButton, mNextButton;
     private Drawable mFavoriteDrawable, mFavoritedDrawable, mPlayState, mPauseState;
     private ViewParent mPrevHolder;
     private ViewParent mNextHolder;
@@ -34,8 +34,6 @@ public class TransportBar extends LinearLayout{
         mPauseButton.requestFocus();
         mPrevButton = (ImageButton) findViewById(R.id.prev);
         mNextButton = (ImageButton) findViewById(R.id.next);
-        mFavoriteButton = (ImageButton) findViewById(R.id.btn_favorite);
-        mCommentButton = (ImageButton) findViewById(R.id.btn_comment);
 
         mPauseState = getResources().getDrawable(R.drawable.ic_pause_states);
         mPlayState = getResources().getDrawable(R.drawable.ic_play_states);
@@ -43,12 +41,6 @@ public class TransportBar extends LinearLayout{
         mPrevHolder = mPrevButton.getParent();
         mNextHolder = mNextButton.getParent();
 
-    }
-
-    public void setCommentMode(boolean isCommenting){
-        if (mCommentButton != null) {
-            mCommentButton.setImageResource(isCommenting ? R.drawable.ic_commenting_states : R.drawable.ic_comment_states);
-        }
     }
 
     public void setOnNextListener(OnClickListener listener) {
@@ -61,13 +53,6 @@ public class TransportBar extends LinearLayout{
 
     public void setOnPrevListener(OnClickListener listener) {
         mPrevButton.setOnClickListener(listener);
-    }
-
-    public void setOnCommentListener(OnClickListener listener) {
-        if (mCommentButton != null) mCommentButton.setOnClickListener(listener);
-    }
-    public void setOnFavoriteListener(OnClickListener listener) {
-        if (mFavoriteButton != null) mFavoriteButton.setOnClickListener(listener);
     }
 
     public void setNavEnabled(boolean b) {
@@ -83,18 +68,6 @@ public class TransportBar extends LinearLayout{
             return true;
         }
         return ret;
-    }
-
-    public void setFavoriteStatus(boolean isFavorite) {
-        if (mFavoriteButton == null) return;
-
-        if (isFavorite) {
-            if (mFavoritedDrawable == null) mFavoritedDrawable = getResources().getDrawable(R.drawable.ic_liked_states);
-            mFavoriteButton.setImageDrawable(mFavoritedDrawable);
-        } else {
-            if (mFavoriteDrawable == null) mFavoriteDrawable = getResources().getDrawable(R.drawable.ic_like_states);
-            mFavoriteButton.setImageDrawable(mFavoriteDrawable);
-        }
     }
 
     public void setPlaybackState(boolean showPlayState) {
