1d45abc21ca86f5bd220359729d6eebb195b856c
Jan Berkel <jan@soundcloud.com>
2012-11-06 16:18:28 +0100

    Fix IllegalArgumentException
    https://www.bugsense.com/dashboard/project/c2486881#error/60140452

--- app/src/main/java/com/soundcloud/android/jni/VorbisDecoder.java
+++ app/src/main/java/com/soundcloud/android/jni/VorbisDecoder.java
@@ -39,7 +39,7 @@ public class VorbisDecoder {
 
     /**
      * Convenience method to decode vorbis stream to file.
-     * @param out
+     * @param out output file
      * @throws DecoderException
      */
     public void decodeToFile(File out) throws DecoderException {
@@ -72,9 +72,9 @@ public class VorbisDecoder {
      * @param length Number of bytes to be read into the buffer. Should be the same size as the buffer. A typical value is 4096.
      * @return
      * <ul>
-     * <li> {@link VorbisConstants.OV_HOLE} indicates there was an interruption in the data. (one of: garbage between pages, loss of sync followed by recapture, or a corrupt page)
-     * <li> {@link VorbisConstants.OV_EBADLINK} indicates that an invalid stream section was supplied to libvorbisfile, or the requested link is corrupt.
-     * <li> {@link VorbisConstants.OV_EINVAL} indicates the initial file headers couldn't be read or are corrupt, or that the initial open call for vf failed.
+     * <li> {@link VorbisConstants#OV_HOLE} indicates there was an interruption in the data. (one of: garbage between pages, loss of sync followed by recapture, or a corrupt page)
+     * <li> {@link VorbisConstants#OV_EBADLINK} indicates that an invalid stream section was supplied to libvorbisfile, or the requested link is corrupt.
+     * <li> {@link VorbisConstants#OV_EINVAL} indicates the initial file headers couldn't be read or are corrupt, or that the initial open call for vf failed.
      * <li> 0 indicates EOF
      * <li> n indicates actual number of bytes read. ov_read() will decode at most one vorbis packet per invocation, so the value returned will generally be less than length.
      * </ul>
@@ -87,11 +87,11 @@ public class VorbisDecoder {
      * @param alignOnPage if yes, find closest sample on page boundary (faster)
      * @return nonzero indicates failure, described by several error codes:
      * <ul>
-     *     <li>{@link VorbisConstants.OV_ENOSEEK} - Bitstream is not seekable.</li>
-     *     <li>{@link VorbisConstants.OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
-     *     <li>{@link VorbisConstants.OV_EREAD} - A read from media returned an error.</li>
-     *     <li>{@link VorbisConstants.OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
-     *     <li>{@link VorbisConstants.OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
+     *     <li>{@link VorbisConstants#OV_ENOSEEK} - Bitstream is not seekable.</li>
+     *     <li>{@link VorbisConstants#OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
+     *     <li>{@link VorbisConstants#OV_EREAD} - A read from media returned an error.</li>
+     *     <li>{@link VorbisConstants#OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
+     *     <li>{@link VorbisConstants#OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
      * </ul>
      * @see <a href="http://xiph.org/vorbis/doc/vorbisfile/ov_pcm_seek.html">ov_pcm_seek</a>
      */
@@ -103,11 +103,11 @@ public class VorbisDecoder {
      * @param alignOnPage if yes, find closest sample on page boundary (faster)
      * @return nonzero indicates failure, described by several error codes:
      * <ul>
-     *     <li>{@link VorbisConstants.OV_ENOSEEK} - Bitstream is not seekable.</li>
-     *     <li>{@link VorbisConstants.OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
-     *     <li>{@link VorbisConstants.OV_EREAD} - A read from media returned an error.</li>
-     *     <li>{@link VorbisConstants.OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
-     *     <li>{@link VorbisConstants.OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
+     *     <li>{@link VorbisConstants#OV_ENOSEEK} - Bitstream is not seekable.</li>
+     *     <li>{@link VorbisConstants#OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
+     *     <li>{@link VorbisConstants#OV_EREAD} - A read from media returned an error.</li>
+     *     <li>{@link VorbisConstants#OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
+     *     <li>{@link VorbisConstants#OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
      * </ul>
      * @see <a href="http://xiph.org/vorbis/doc/vorbisfile/ov_pcm_seek.html">ov_pcm_seek</a>
      */
@@ -122,11 +122,11 @@ public class VorbisDecoder {
      * @param pos time in seconds
      * @return nonzero indicates failure, described by several error codes:
      * <ul>
-     *     <li>{@link VorbisConstants.OV_ENOSEEK} - Bitstream is not seekable.</li>
-     *     <li>{@link VorbisConstants.OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
-     *     <li>{@link VorbisConstants.OV_EREAD} - A read from media returned an error.</li>
-     *     <li>{@link VorbisConstants.OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
-     *     <li>{@link VorbisConstants.OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
+     *     <li>{@link VorbisConstants#OV_ENOSEEK} - Bitstream is not seekable.</li>
+     *     <li>{@link VorbisConstants#OV_EINVAL} - Invalid argument value; possibly called with an OggVorbis_File structure that isn't open.</li>
+     *     <li>{@link VorbisConstants#OV_EREAD} - A read from media returned an error.</li>
+     *     <li>{@link VorbisConstants#OV_EFAULT} - Internal logic fault; indicates a bug or heap/stack corruption.</li>
+     *     <li>{@link VorbisConstants#OV_EBADLINK} - Invalid stream section supplied to libvorbisfile, or the requested link is corrupt.</li>
      * </ul>
      * @see <a href="http://xiph.org/vorbis/doc/vorbisfile/ov_pcm_seek.html">ov_pcm_seek</a>
 
