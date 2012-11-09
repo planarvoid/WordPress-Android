ba38b665e0d760e511413f5fd9e204d56214e54f
Robert Böhnke <robb@soundcloud.com>
2012-11-09 16:53:33 +0100

    Styling tour pages

--- app/src/main/java/com/soundcloud/android/view/tour/TourTextView.java
+++ app/src/main/java/com/soundcloud/android/view/tour/TourTextView.java
@@ -1,6 +1,9 @@
 package com.soundcloud.android.view.tour;
 
 import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -16,4 +19,19 @@ public class TourTextView extends TextView {
     public TourTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
+
+    public void setText(CharSequence text, BufferType type) {
+        SpannableStringBuilder spanBuilder = new SpannableStringBuilder();
+        spanBuilder.append(text);
+
+        spanBuilder.setSpan(
+            new BackgroundColorSpan(0xCC000000),
+            0,
+            text.length(),
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        super.setText(spanBuilder, type);
+    }
+
 }
