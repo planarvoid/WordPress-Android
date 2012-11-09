62add30a99853d9a10a7b3eba0d5a4aeb50e89e4
Robert Böhnke <robb@soundcloud.com>
2012-11-09 14:47:52 +0100

    Addding TourTextView

---
+++ app/src/main/java/com/soundcloud/android/view/tour/TourTextView.java
@@ -0,0 +1,19 @@
+package com.soundcloud.android.view.tour;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class TourTextView extends TextView {
+    public TourTextView(Context context) {
+        super(context);
+    }
+
+    public TourTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TourTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
