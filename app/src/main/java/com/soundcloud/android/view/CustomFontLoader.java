22f737cd6b6b88cc51097db98a4f230c8ba7f6a2
Robert Böhnke <robb@soundcloud.com>
2012-11-15 10:41:33 +0100

    Ignoring fonts that cannot be loaded

--- app/src/main/java/com/soundcloud/android/view/CustomFontLoader.java
+++ app/src/main/java/com/soundcloud/android/view/CustomFontLoader.java
@@ -2,6 +2,9 @@ package com.soundcloud.android.view;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
+import com.soundcloud.android.SoundCloudApplication;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.ref.SoftReference;
 import java.util.Hashtable;
@@ -13,15 +16,19 @@ public class CustomFontLoader {
         fontCache = new Hashtable<String, SoftReference<Typeface>>();
     }
 
+    @Nullable
     public static Typeface getFont(Context context, String path) {
         synchronized (fontCache) {
             SoftReference<Typeface> reference = fontCache.get(path);
             Typeface typeface = reference != null ? reference.get() : null;
 
             if (typeface == null) {
-                typeface = Typeface.createFromAsset(context.getAssets(), path);
-
-                fontCache.put(path, new SoftReference<Typeface>(typeface));
+                try {
+                    typeface = Typeface.createFromAsset(context.getAssets(), path);
+                    fontCache.put(path, new SoftReference<Typeface>(typeface));
+                } catch (RuntimeException e) {
+                    Log.e(SoundCloudApplication.TAG, "Encountered exception loading the font", e);
+                }
             }
 
             return typeface;
