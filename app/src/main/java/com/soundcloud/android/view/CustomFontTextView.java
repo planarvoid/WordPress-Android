55fa88720f96a64de57db96eee47881628e18ac1
Robert Böhnke <robb@soundcloud.com>
2012-11-27 13:10:20 +0100

    Adding custom font capabilities to ClearText

--- app/src/main/java/com/soundcloud/android/view/CustomFontTextView.java
+++ app/src/main/java/com/soundcloud/android/view/CustomFontTextView.java
@@ -10,6 +10,8 @@ import com.soundcloud.android.R;
 import java.lang.ref.SoftReference;
 import java.util.Hashtable;
 
+import static com.soundcloud.android.view.CustomFontLoader.applyCustomFont;
+
 public class CustomFontTextView extends TextView {
 
     public CustomFontTextView(Context context) {
@@ -19,28 +21,14 @@ public class CustomFontTextView extends TextView {
     public CustomFontTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        applyCustomFont(context, attrs);
+        applyCustomFont(context, this, attrs);
     }
 
     public CustomFontTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        applyCustomFont(context, attrs);
+        applyCustomFont(context, this, attrs);
     }
 
-    private void applyCustomFont(Context context, AttributeSet attrs) {
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CustomFontTextView);
-        String path = array.getString(R.styleable.CustomFontTextView_custom_font);
 
-        if (path != null) {
-            setFontFromPath(path);
-        }
-
-        array.recycle();
-    }
-
-    private void setFontFromPath(String path) {
-        Typeface typeface = CustomFontLoader.getFont(getContext(), path);
-        setTypeface(typeface);
-    }
 }
