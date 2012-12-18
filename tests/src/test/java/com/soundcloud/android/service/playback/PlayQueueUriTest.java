50cd14325850c8371f1536dedb9753310c7ba41d
Jon Schmidt <jon@soundcloud.com>
2012-12-18 18:05:19 +0100

    easy on the imports

--- tests/src/test/java/com/soundcloud/android/service/playback/PlayQueueUriTest.java
+++ tests/src/test/java/com/soundcloud/android/service/playback/PlayQueueUriTest.java
@@ -1,28 +1,14 @@
 package com.soundcloud.android.service.playback;
 
 import static com.soundcloud.android.Expect.expect;
-import static com.soundcloud.android.robolectric.TestHelper.readJson;
 
-import com.soundcloud.android.SoundCloudApplication;
-import com.soundcloud.android.model.Playable;
-import com.soundcloud.android.model.ScResource;
 import com.soundcloud.android.model.Track;
-import com.soundcloud.android.model.TrackHolder;
-import com.soundcloud.android.model.User;
 import com.soundcloud.android.provider.Content;
 import com.soundcloud.android.robolectric.DefaultTestRunner;
-import com.xtremelabs.robolectric.Robolectric;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.content.ContentResolver;
-import android.database.Cursor;
 import android.net.Uri;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(DefaultTestRunner.class)
 public class PlayQueueUriTest {
