2acbfdd75beee21555f179376b803889cff48ed5
Jan Berkel <jan@soundcloud.com>
2013-01-07 20:06:34 +0100

    Remove dalvik check, fix test

--- app/src/main/java/com/soundcloud/android/service/sync/SyncAdapterService.java
+++ app/src/main/java/com/soundcloud/android/service/sync/SyncAdapterService.java
@@ -194,13 +194,11 @@ public class SyncAdapterService extends Service {
                     Log.d(TAG, "skipping collection sync, no wifi");
                 }
 
-                if (SoundCloudApplication.DALVIK) {
-                    final List<Uri> dueForSync = SyncCleanups.getCleanupsDueForSync(app, manual);
-                    if (Log.isLoggable(TAG, Log.DEBUG)) {
-                        Log.d(TAG, "cleanups due for sync:" + dueForSync);
-                    }
-                    urisToSync.addAll(dueForSync);
+                final List<Uri> dueForSync = SyncCleanups.getCleanupsDueForSync(app, manual);
+                if (Log.isLoggable(TAG, Log.DEBUG)) {
+                    Log.d(TAG, "cleanups due for sync:" + dueForSync);
                 }
+                urisToSync.addAll(dueForSync);
 
                 if (SyncConfig.shouldSync(app, Consts.PrefKeys.LAST_USER_SYNC, SyncConfig.CLEANUP_DELAY) || manual) {
                     urisToSync.add(Content.ME.uri);
