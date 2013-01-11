f3595280cfd7578aa570b0d4120105cc9962a2d1
Matthias Kaeppler <m.kaeppler@gmail.com>
2013-01-11 14:04:40 +0100

    change output folder to Dropbox/android

--- .jenkins-dropbox.sh
+++ .jenkins-dropbox.sh
@@ -3,6 +3,6 @@ NORMALIZED_GIT_BRANCH=`echo $GIT_BRANCH | sed "s/[^a-zA-Z0-9]/\-/g"`
 
 deploy_to_dropbox() {
   for f in `find app/target/ -name "*.apk" -exec basename {} ';'`; do
-    cp app/target/$f ~/Dropbox/`echo $f | sed "s/\.apk/-$NORMALIZED_GIT_BRANCH\.apk/g"`
+    cp app/target/$f ~/Dropbox/android/`echo $f | sed "s/\.apk/-$NORMALIZED_GIT_BRANCH\.apk/g"`
   done
 }
