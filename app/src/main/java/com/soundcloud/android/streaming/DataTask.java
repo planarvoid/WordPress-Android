cb30e83cf6edec7df53105a188552002bab32a71
jonschmidt <jon@soundcloud.com>
2013-01-29 14:31:19 +0100

    fix playback data task locale formatting

--- app/src/main/java/com/soundcloud/android/streaming/DataTask.java
+++ app/src/main/java/com/soundcloud/android/streaming/DataTask.java
@@ -18,6 +18,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.Locale;
 
 abstract class DataTask extends StreamItemTask {
     static final String LOG_TAG = StreamLoader.LOG_TAG;
@@ -141,7 +142,7 @@ abstract class DataTask extends StreamItemTask {
         protected int getData(URL url, int start, int end, ByteBuffer dst) throws IOException {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestProperty("Range",
-                    String.format("bytes=%d-%d", start, end));
+                    String.format(Locale.ENGLISH, "bytes=%d-%d", start, end));
 
             connection.setReadTimeout(READ_TIMEOUT);
             connection.setConnectTimeout(CONNECTION_TIMEOUT);
