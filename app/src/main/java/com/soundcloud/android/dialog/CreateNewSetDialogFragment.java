84df43b7eec56f7466c1a133f43197cb2a80bd7b
jonschmidt <jon@soundcloud.com>
2013-02-01 12:04:33 +0100

    More addToSet dialog flow and functionality
    still waiting on styling and assets

---
+++ app/src/main/java/com/soundcloud/android/dialog/CreateNewSetDialogFragment.java
@@ -0,0 +1,58 @@
+package com.soundcloud.android.dialog;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.soundcloud.android.R;
+import com.soundcloud.android.model.Track;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.widget.EditText;
+
+public class CreateNewSetDialogFragment extends SherlockDialogFragment {
+
+    public static String KEY_TRACK_ID = "TRACK_ID";
+
+    public static CreateNewSetDialogFragment from(long trackId) {
+
+        Bundle b = new Bundle();
+        b.putLong(KEY_TRACK_ID, trackId);
+
+        CreateNewSetDialogFragment fragment = new CreateNewSetDialogFragment();
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), android.R.style.Theme_Holo_Dialog));
+        builder.setTitle(
+                getString(R.string.create_new_set)
+        );
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(getActivity());
+        builder.setView(input);
+        builder.setNegativeButton(R.string.cancel,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton(R.string.done,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //todo, add set logic
+                dialog.dismiss();
+            }
+        });
+
+        return builder.create();
+
+
+    }
+
+}
--- app/src/main/java/com/soundcloud/android/dialog/MyPlaylistsDialogFragment.java
+++ app/src/main/java/com/soundcloud/android/dialog/MyPlaylistsDialogFragment.java
@@ -1,18 +1,23 @@
 package com.soundcloud.android.dialog;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.soundcloud.android.R;
 import com.soundcloud.android.model.Track;
 import com.soundcloud.android.provider.Content;
 import com.soundcloud.android.provider.DBHelper;
 
-import android.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyPlaylistsDialogFragment extends SherlockDialogFragment {
 
@@ -33,38 +38,64 @@ public class MyPlaylistsDialogFragment extends SherlockDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), android.R.style.Theme_Holo_Dialog));
+
         builder.setTitle(
-                getString(com.soundcloud.android.R.string.add_track_to_set,getArguments().getString(KEY_TRACK_TITLE))
+                getString(R.string.add_track_to_set,getArguments().getString(KEY_TRACK_TITLE))
         );
 
-        Cursor c = getActivity().getContentResolver().query(
+        Cursor cursor = getActivity().getContentResolver().query(
                 Content.ME_PLAYLISTS.uri,
                 new String[]{DBHelper.PlaylistTracksView._ID, DBHelper.PlaylistTracksView.TITLE}, null, null, null);
 
-        final SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(), R.layout.simple_list_item_1,
-                c, new String[]{DBHelper.PlaylistTracksView.TITLE}, new int[]{R.id.text1});
+        MatrixCursor extras = new MatrixCursor(new String[] { DBHelper.PlaylistTracksView._ID, DBHelper.PlaylistTracksView.TITLE });
+        extras.addRow(new Object[] { -1l, getString(R.string.create_new_set) });
+
+        SimpleCursorAdapter.ViewBinder viewBinder = new SimpleCursorAdapter.ViewBinder() {
+               public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                   if (columnIndex == cursor.getColumnIndex(DBHelper.PlaylistTracksView._ID)){
+                       ImageView image = (ImageView) view;
+                       image.setImageResource(cursor.getLong(columnIndex) == -1 ?
+                               R.drawable.ic_add_to_white :
+                               R.drawable.ic_user_tab_sounds);
+
+                       return true;
+
+                   } else {
+                       return false;
+                   }
+               }
+           };
+
+        final SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(),
+                R.layout.search_suggestion,
+                new MergeCursor(new Cursor[]{ extras, cursor }),
+                new String[]{DBHelper.PlaylistTracksView._ID,DBHelper.PlaylistTracksView.TITLE},
+                new int[]{R.id.icon,R.id.title});
+
+        adapter.setViewBinder(viewBinder);
 
         builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 final long playlistId = adapter.getItemId(which);
 
-                // TODO, new playlist
-
-                ContentValues cv = new ContentValues();
-                cv.put(DBHelper.PlaylistTracks.PLAYLIST_ID, playlistId);
-                cv.put(DBHelper.PlaylistTracks.TRACK_ID, getArguments().getLong(KEY_TRACK_ID));
-                cv.put(DBHelper.PlaylistTracks.ADDED_AT, System.currentTimeMillis());
-                // off the UI thread??? ugh
-                getActivity().getContentResolver().insert(Content.PLAYLIST_TRACKS.forId(playlistId), cv);
-
+                if (playlistId == -1){
+                    CreateNewSetDialogFragment.from(getArguments().getLong(KEY_TRACK_ID)).show(getFragmentManager(),"create_new_set_dialog");
+                } else {
+                    ContentValues cv = new ContentValues();
+                    cv.put(DBHelper.PlaylistTracks.PLAYLIST_ID, playlistId);
+                    cv.put(DBHelper.PlaylistTracks.TRACK_ID, getArguments().getLong(KEY_TRACK_ID));
+                    cv.put(DBHelper.PlaylistTracks.ADDED_AT, System.currentTimeMillis());
+                    // off the UI thread??? ugh
+                    getActivity().getContentResolver().insert(Content.PLAYLIST_TRACKS.forId(playlistId), cv);
+                }
                 // we done
                 dialog.dismiss();
             }
         });
 
-        builder.setNegativeButton(getString(com.soundcloud.android.R.string.cancel),
+        builder.setNegativeButton(getString(R.string.cancel),
                 new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
