7858dd31103560ce410a6b401f66cc928f5468d1
Jon Schmidt <jon@soundcloud.com>
2013-02-05 11:21:19 +0100

    do not delete external uploads, fixes #424

--- app/src/main/java/com/soundcloud/android/model/Recording.java
+++ app/src/main/java/com/soundcloud/android/model/Recording.java
@@ -520,8 +520,8 @@ public class Recording extends ScResource implements Comparable<Recording> {
      */
     public void onUploaded(ContentResolver resolver) {
         upload_status = Status.UPLOADED;
+        if (!external_upload) IOUtils.deleteFile(getFile());
         IOUtils.deleteFile(getEncodedFile());
-        IOUtils.deleteFile(getFile());
         IOUtils.deleteFile(resized_artwork_path);
         updateStatus(resolver);
     }
