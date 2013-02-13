d930899937596749b7b9d9df20c161eca5b73346
Matthias Kaeppler <m.kaeppler@gmail.com>
2013-02-13 12:31:21 +0100

    Use e1_likes_mini.json in test when requesting mini resources.

--- tests/src/test/java/com/soundcloud/android/service/sync/ApiSyncerTest.java
+++ tests/src/test/java/com/soundcloud/android/service/sync/ApiSyncerTest.java
@@ -146,14 +146,14 @@ public class ApiSyncerTest {
     @Test
     public void shouldSyncLikes() throws Exception {
         addResourceResponse("/e1/users/" + String.valueOf(USER_ID)
-                + "/likes?limit=200&representation=mini&linked_partitioning=1", "e1_likes.json");
+                + "/likes?limit=200&representation=mini&linked_partitioning=1", "e1_likes_mini.json");
 
         Result result = sync(Content.ME_LIKES.uri);
         expect(result.success).toBeTrue();
         expect(result.synced_at).toBeGreaterThan(0l);
 
-        expect(Content.TRACKS).toHaveCount(2);    // 2 sounds + 1 playlist
-        expect(Content.ME_LIKES).toHaveCount(2);  // ditto
+        expect(Content.TRACKS).toHaveCount(1);
+        expect(Content.ME_LIKES).toHaveCount(1);  // FIXME: when sets go live, change to 2
     }
 
     @Test
@@ -165,7 +165,7 @@ public class ApiSyncerTest {
         expect(result.synced_at).toBeGreaterThan(0l);
 
         addResourceResponse("/e1/users/" + String.valueOf(USER_ID)
-                + "/likes?limit=200&representation=mini&linked_partitioning=1", "e1_likes.json");
+                + "/likes?limit=200&representation=mini&linked_partitioning=1", "e1_likes_mini.json");
 
         result = sync(Content.ME_LIKES.uri);
         expect(result.success).toBeTrue();
@@ -173,7 +173,7 @@ public class ApiSyncerTest {
 
         expect(Content.TRACKS).toHaveCount(49);
         expect(Content.ME_SOUNDS).toHaveCount(48);
-        expect(Content.ME_LIKES).toHaveCount(2);
+        expect(Content.ME_LIKES).toHaveCount(1); // FIXME: when sets go live, change to 2
     }
 
     @Test
