efaf98da4f2dc164c0c405f35f44fe715c8cc878
jonschmidt <jon@soundcloud.com>
2013-02-26 12:08:16 +0100

    use content uri on sound association adapter

--- app/src/main/java/com/soundcloud/android/adapter/SoundAssociationAdapter.java
+++ app/src/main/java/com/soundcloud/android/adapter/SoundAssociationAdapter.java
@@ -29,7 +29,7 @@ public class SoundAssociationAdapter extends ScBaseAdapter<SoundAssociation> imp
 
     @Override
     public Uri getPlayableUri() {
-        return null;
+        return mContentUri;
     }
 
     @Override
