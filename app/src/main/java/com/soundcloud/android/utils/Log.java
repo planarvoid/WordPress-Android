4fb4727cb2c7e22f1349228fd5c0f31d86fc583a
Matthias Kaeppler <mk@soundcloud.com>
2013-03-17 12:28:14 +0100

    Add a Log utility class

---
+++ app/src/main/java/com/soundcloud/android/utils/Log.java
@@ -0,0 +1,64 @@
+package com.soundcloud.android.utils;
+
+import com.soundcloud.android.SoundCloudApplication;
+import org.jetbrains.annotations.NotNull;
+
+public class Log {
+
+    public static void d(@NotNull final String tag, @NotNull final String message) {
+        if (android.util.Log.isLoggable(tag, android.util.Log.DEBUG)) {
+            android.util.Log.d(tag, message);
+        }
+    }
+
+    public static void d(@NotNull final Object obj, @NotNull final String message) {
+        d(obj.getClass().getSimpleName(), message);
+    }
+
+    public static void d(@NotNull final String message) {
+        d(SoundCloudApplication.TAG, message);
+    }
+
+    public static void i(@NotNull final String tag, @NotNull final String message) {
+        if (android.util.Log.isLoggable(tag, android.util.Log.INFO)) {
+            android.util.Log.i(tag, message);
+        }
+    }
+
+    public static void i(@NotNull final Object obj, @NotNull final String message) {
+        i(obj.getClass().getSimpleName(), message);
+    }
+
+    public static void i(@NotNull final String message) {
+        i(SoundCloudApplication.TAG, message);
+    }
+
+    public static void w(@NotNull final String tag, @NotNull final String message) {
+        if (android.util.Log.isLoggable(tag, android.util.Log.WARN)) {
+            android.util.Log.w(tag, message);
+        }
+    }
+
+    public static void w(@NotNull final Object obj, @NotNull final String message) {
+        w(obj.getClass().getSimpleName(), message);
+    }
+
+    public static void w(@NotNull final String message) {
+        w(SoundCloudApplication.TAG, message);
+    }
+
+    public static void e(@NotNull final String tag, @NotNull final String message) {
+        if (android.util.Log.isLoggable(tag, android.util.Log.ERROR)) {
+            android.util.Log.e(tag, message);
+        }
+    }
+
+    public static void e(@NotNull final Object obj, @NotNull final String message) {
+        e(obj.getClass().getSimpleName(), message);
+    }
+
+    public static void e(@NotNull final String message) {
+        e(SoundCloudApplication.TAG, message);
+    }
+
+}
