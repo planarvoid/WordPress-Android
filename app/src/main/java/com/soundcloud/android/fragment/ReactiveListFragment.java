8fa695161a56302c9c1b26b56b62c1a1f440c664
Matthias Kaeppler <mk@soundcloud.com>
2013-03-17 17:13:37 +0100

    Switch the new list fragment to its own layout file.
    Progress status updates now work as follows:
    - if a task is faster than t millis (t=250 currently), no progress spinner will be shown. this is to eliminate flickering due to a very short appearance of the progress spinner for fast tasks such as loading from local storage
    - if a task takes longer than t millis, the progress spinner starts showing

--- app/src/main/java/com/soundcloud/android/fragment/ReactiveListFragment.java
+++ app/src/main/java/com/soundcloud/android/fragment/ReactiveListFragment.java
@@ -6,14 +6,16 @@ import com.soundcloud.android.R;
 import com.soundcloud.android.adapter.ActivityAdapter;
 import com.soundcloud.android.model.act.Activities;
 import com.soundcloud.android.provider.Content;
-import com.soundcloud.android.rx.schedulers.ActivitiesScheduler;
 import com.soundcloud.android.rx.observers.ContextObserver;
+import com.soundcloud.android.rx.schedulers.ActivitiesScheduler;
 import com.soundcloud.android.utils.Log;
+import com.soundcloud.android.view.EmptyListView;
 import com.soundcloud.android.view.ScListView;
 import rx.Observer;
 import rx.Subscription;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,36 +23,23 @@ import android.view.ViewGroup;
 
 public class ReactiveListFragment extends Fragment {
 
-    private static final String TAG = ReactiveListFragment.class.getSimpleName();
+    private static final int PROGRESS_DELAY_MILLIS = 250;
 
     private ScListView mListView;
+    private EmptyListView mEmptyView;
     private ActivityAdapter mAdapter;
 
     private ActivitiesScheduler mScheduler;
     private Subscription mSubscription;
     private ContextObserver<Activities> mActivitiesObserver;
 
-    private class LoadActivitiesObserver implements Observer<Activities> {
-
+    private Handler showProgressHandler = new Handler();
+    private Runnable showProgress = new Runnable() {
         @Override
-        public void onCompleted() {
-            Log.d(this, "onCompleted t=" + Thread.currentThread().getName());
-            Log.d(this, "done=" + mActivitiesObserver.isCompleted());
+        public void run() {
+            mEmptyView.setStatus(EmptyListView.Status.WAITING);
         }
-
-        @Override
-        public void onError(Exception e) {
-            Log.d(this, "onError: " + e + "; t=" + Thread.currentThread().getName());
-            e.printStackTrace();
-        }
-
-        @Override
-        public void onNext(Activities activities) {
-            Log.d(this, "onNext: " + activities.size() + "; t=" + Thread.currentThread().getName());
-            mAdapter.addItems(activities);
-            mAdapter.notifyDataSetChanged();
-        }
-    }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,10 +60,12 @@ public class ReactiveListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.playlist_fragment, container, false);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.basic_list_fragment, container, false);
 
         mListView = (ScListView) layout.findViewById(R.id.list);
         mListView.setAdapter(mAdapter);
+
+        mEmptyView = (EmptyListView) layout.findViewById(android.R.id.empty);
 
         return layout;
     }
@@ -95,6 +86,10 @@ public class ReactiveListFragment extends Fragment {
         mSubscription = mScheduler.scheduleFirstPendingObservable(mActivitiesObserver);
 
         Log.d(this, "onStart: done=" + mActivitiesObserver.isCompleted());
+
+        mEmptyView.setStatus(EmptyListView.Status.OK);
+
+        showProgressHandler.postDelayed(showProgress, PROGRESS_DELAY_MILLIS);
     }
 
     @Override
@@ -104,5 +99,34 @@ public class ReactiveListFragment extends Fragment {
         mSubscription.unsubscribe();
 
     }
+
+    private class LoadActivitiesObserver implements Observer<Activities> {
+
+        @Override
+        public void onCompleted() {
+            Log.d(this, "onCompleted t=" + Thread.currentThread().getName());
+            Log.d(this, "done=" + mActivitiesObserver.isCompleted());
+
+            showProgressHandler.removeCallbacks(showProgress);
+            mEmptyView.setStatus(EmptyListView.Status.OK);
+        }
+
+        @Override
+        public void onError(Exception e) {
+            Log.d(this, "onError: " + e + "; t=" + Thread.currentThread().getName());
+            e.printStackTrace();
+            showProgressHandler.removeCallbacks(showProgress);
+            mEmptyView.setStatus(EmptyListView.Status.ERROR);
+        }
+
+        @Override
+        public void onNext(Activities activities) {
+            Log.d(this, "onNext: " + activities.size() + "; t=" + Thread.currentThread().getName());
+            mAdapter.addItems(activities);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+
 
 }
