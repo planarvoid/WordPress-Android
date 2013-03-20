707922b231e5c587b7f736eba365e50a2dd2dcbe
Matthias Kaeppler <mk@soundcloud.com>
2013-03-20 12:19:27 +0100

    Events.anyOf makes it easier to react to events using the same observer

--- app/src/main/java/com/soundcloud/android/rx/event/Events.java
+++ app/src/main/java/com/soundcloud/android/rx/event/Events.java
@@ -6,6 +6,10 @@ import rx.Subscription;
 import rx.subjects.Subject;
 import rx.util.functions.Action1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Events {
 
     LIKE_CHANGED,
@@ -39,4 +43,40 @@ public enum Events {
         });
     }
 
+    public static SubscriptionBuilder anyOf(Events... events) {
+        return new SubscriptionBuilder(Arrays.asList(events));
+    }
+
+    public static class SubscriptionBuilder {
+        private List<Events> mEvents;
+        private List<Subscription> mSubscriptions;
+
+        private Subscription mCollectiveSubscription = new Subscription() {
+            @Override
+            public void unsubscribe() {
+                for (Subscription s : mSubscriptions) {
+                    s.unsubscribe();
+                }
+            }
+        };
+
+        public SubscriptionBuilder(List<Events> events) {
+            mEvents = events;
+            mSubscriptions = new ArrayList<Subscription>(mEvents.size());
+        }
+
+        public <T> Subscription subscribe(final Observable<T> observable, final Observer<T> observer) {
+            for (Events e : mEvents) {
+                mSubscriptions.add(e.subscribe(observable, observer));
+            }
+            return mCollectiveSubscription;
+        }
+
+        public <T> Subscription subscribe(final Action1<T> action) {
+            for (Events e : mEvents) {
+                mSubscriptions.add(e.subscribe(action));
+            }
+            return mCollectiveSubscription;
+        }
+    }
 }
