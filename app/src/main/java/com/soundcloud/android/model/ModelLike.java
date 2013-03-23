554d6eb0299709d465099ebdeaeedbbf30ee7ff0
Matthias Kaeppler <mk@soundcloud.com>
2013-03-23 21:57:56 +0100

    public method scope is redundant in an interface.

--- app/src/main/java/com/soundcloud/android/model/ModelLike.java
+++ app/src/main/java/com/soundcloud/android/model/ModelLike.java
@@ -3,6 +3,6 @@ package com.soundcloud.android.model;
 import android.net.Uri;
 
 public interface ModelLike {
-    public long getId();
-    public Uri toUri();
+    long getId();
+    Uri toUri();
 }
