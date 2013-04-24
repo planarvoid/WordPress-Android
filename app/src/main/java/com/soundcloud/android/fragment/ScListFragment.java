711a296f6dce870ea7a9e4e8510773e6d744e4ab
jonschmidt <jon@soundcloud.com>
2013-04-24 16:40:10 +0200

    added comment to clarify pull to refresh configuration

--- app/src/main/java/com/soundcloud/android/fragment/ScListFragment.java
+++ app/src/main/java/com/soundcloud/android/fragment/ScListFragment.java
@@ -389,6 +389,10 @@ public class ScListFragment extends SherlockListFragment implements PullToRefres
         }
     }
 
+    /**
+     * Set the pull to refresh state based on having a valid local collection (that has finished any async initialization)
+     * that is also in idle state. If not in that state, then set the loading state to prevent unwanted refreshes/syncs
+     */
     private void configurePullToRefreshState() {
         if (mListView != null){
             if (mLocalCollection.isIdle()) {
