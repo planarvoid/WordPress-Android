1bc7a9db45ef63d602ee8d72b98506596e268733
Jon Schmidt <jon@soundcloud.com>
2013-05-10 11:11:04 +0200

    added visible activity scopes

--- app/src/main/java/com/soundcloud/android/accounts/AuthenticationManager.java
+++ app/src/main/java/com/soundcloud/android/accounts/AuthenticationManager.java
@@ -5,13 +5,17 @@ import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
 
 import android.content.Context;
+import android.os.Bundle;
 
 import java.io.IOException;
 
 public class AuthenticationManager {
 
     public String getGoogleAccountToken(Context context, String accountName, String scope) throws GoogleAuthException, IOException {
-        return GoogleAuthUtil.getToken(context, accountName, scope);
+        Bundle bundle = new Bundle();
+        bundle.putString(GoogleAuthUtil.KEY_REQUEST_VISIBLE_ACTIVITIES,
+                "http://schemas.google.com/AddActivity http://schemas.google.com/CreateActivity http://schemas.google.com/ListenActivity");
+        return GoogleAuthUtil.getToken(context, accountName, scope, bundle);
     }
 
     public void invalidateGoogleAccountToken(Context context, String token){
