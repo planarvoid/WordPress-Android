9404f6efc13bcf5dfc4c8a6158aa526ac32d7623
Jon Schmidt <jon@soundcloud.com>
2013-05-23 18:55:44 -0400

    some batch insert tests

--- tests/src/test/java/com/soundcloud/android/dao/UserAssociationStorageTest.java
+++ tests/src/test/java/com/soundcloud/android/dao/UserAssociationStorageTest.java
@@ -1,20 +1,23 @@
 package com.soundcloud.android.dao;
 
 import static com.soundcloud.android.Expect.expect;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import com.soundcloud.android.model.ScResource;
-import com.soundcloud.android.model.SoundAssociationHolder;
-import com.soundcloud.android.model.SoundAssociationTest;
 import com.soundcloud.android.model.User;
 import com.soundcloud.android.provider.Content;
 import com.soundcloud.android.provider.DBHelper;
 import com.soundcloud.android.robolectric.DefaultTestRunner;
 import com.soundcloud.android.robolectric.TestHelper;
-import com.soundcloud.android.service.sync.ApiSyncerTest;
 import com.soundcloud.android.task.collection.RemoteCollectionLoaderTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -26,6 +29,7 @@ import java.util.List;
 public class UserAssociationStorageTest {
 
     final static long USER_ID = 1L;
+    public static final int BATCH_SIZE = 5;
 
     private ContentResolver resolver;
     private UserAssociationStorage storage;
@@ -76,7 +80,27 @@ public class UserAssociationStorageTest {
         expect(Content.ME_FOLLOWERS).toHaveCount(0);
     }
 
-    public static List<User> createUsers() {
+    @Test
+    public void shouldInsertInSingleBatchIfCollectionIsSmallEnough() throws Exception {
+        List<Long> ids = createIdList(BATCH_SIZE);
+
+        ContentResolver resolver = Mockito.mock(ContentResolver.class);
+        new UserAssociationStorage(resolver).insertInBatches(Content.ME_FOLLOWERS,USER_ID,ids,0, BATCH_SIZE);
+        verify(resolver).bulkInsert(eq(Content.ME_FOLLOWERS.uri), any(ContentValues[].class));
+        verifyNoMoreInteractions(resolver);
+    }
+
+    @Test
+    public void shouldInsertInBatchesIfCollectionIsTooLarge() throws Exception {
+        List<Long> ids = createIdList(BATCH_SIZE * 2);
+
+        ContentResolver resolver = Mockito.mock(ContentResolver.class);
+        new UserAssociationStorage(resolver).insertInBatches(Content.ME_FOLLOWERS, USER_ID, ids, 0, BATCH_SIZE);
+        verify(resolver, times(2)).bulkInsert(eq(Content.ME_FOLLOWERS.uri), any(ContentValues[].class));
+        verifyNoMoreInteractions(resolver);
+    }
+
+    private static List<User> createUsers() {
         List<User> items = new ArrayList<User>();
 
         User u1 = new User();
@@ -90,5 +114,13 @@ public class UserAssociationStorageTest {
         items.add(u1);
         items.add(u2);
         return items;
+    }
+
+    private static List<Long> createIdList(int count) {
+        List<Long> ids = new ArrayList<Long>();
+        for (long i = 0; i < count; i++) {
+            ids.add(i);
+        }
+        return ids;
     }
 }
