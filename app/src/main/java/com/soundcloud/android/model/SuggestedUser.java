58f9f1c7813f70df2f58e3f4902ae5ac5764c4fb
Matthias Kaeppler <mk@soundcloud.com>
2013-06-04 14:25:31 +0200

    Make SuggestedUser parcelable

--- app/src/main/java/com/soundcloud/android/model/SuggestedUser.java
+++ app/src/main/java/com/soundcloud/android/model/SuggestedUser.java
@@ -1,10 +1,23 @@
 package com.soundcloud.android.model;
 
+import android.os.Parcel;
+
 public class SuggestedUser extends ScModel {
 
     private String mUsername, mCity, mCountry;
 
     public SuggestedUser() {
+    }
+
+    public SuggestedUser(String urn) {
+        super(urn);
+    }
+
+    public SuggestedUser(Parcel parcel) {
+        super(parcel);
+        mUsername = parcel.readString();
+        mCity = parcel.readString();
+        mCountry = parcel.readString();
     }
 
     public String getUsername() {
@@ -29,5 +42,13 @@ public class SuggestedUser extends ScModel {
 
     public void setCountry(String country) {
         this.mCountry = country;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(mUsername);
+        dest.writeString(mCity);
+        dest.writeString(mCountry);
     }
 }
--- tests/src/test/java/com/soundcloud/android/model/SuggestedUserTest.java
+++ tests/src/test/java/com/soundcloud/android/model/SuggestedUserTest.java
@@ -1,4 +1,31 @@
 package com.soundcloud.android.model;
 
+import static com.soundcloud.android.Expect.expect;
+
+import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.os.Parcel;
+
+@RunWith(SoundCloudTestRunner.class)
 public class SuggestedUserTest {
+
+    private SuggestedUser user = new SuggestedUser("soundcloud:users:1");
+
+    @Test
+    public void shouldBeParcelable() {
+        user.setUsername("JonSchmidt");
+        user.setCity("Berlin");
+        user.setCountry("Germany");
+
+        Parcel parcel = Parcel.obtain();
+        user.writeToParcel(parcel, 0);
+
+        SuggestedUser unparceledUser = new SuggestedUser(parcel);
+        expect(unparceledUser.getUsername()).toEqual(user.getUsername());
+        expect(unparceledUser.getCity()).toEqual(user.getCity());
+        expect(unparceledUser.getCountry()).toEqual(user.getCountry());
+    }
+
 }
