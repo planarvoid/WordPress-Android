1df480f880ad0b71251c501ffb0fc5bdefe36e7e
Matthias Kaeppler <mk@soundcloud.com>
2013-06-06 11:20:30 +0200

    Bump ModelCitizen to 0.4, user objects now generated with running IDs and unique usernames

--- tests/src/test/java/com/soundcloud/android/blueprints/UserBlueprint.java
+++ tests/src/test/java/com/soundcloud/android/blueprints/UserBlueprint.java
@@ -5,18 +5,27 @@ import com.tobedevoured.modelcitizen.annotation.Blueprint;
 import com.tobedevoured.modelcitizen.annotation.Default;
 import com.tobedevoured.modelcitizen.annotation.NewInstance;
 import com.tobedevoured.modelcitizen.field.ConstructorCallback;
+import com.tobedevoured.modelcitizen.field.FieldCallback;
 
 @Blueprint(User.class)
 public class UserBlueprint {
+
+    private static long runningId = 1L;
 
     @NewInstance
     ConstructorCallback constructor = new ConstructorCallback() {
         @Override
         public Object createInstance() {
-            return new User(1L);
+            return new User(runningId++);
         }
     };
 
     @Default
-    String username = "Skrillex";
+    FieldCallback username = new FieldCallback() {
+        @Override
+        public Object get(Object referenceModel) {
+            return "user" + ((User) referenceModel).getId();
+        }
+    };
+
 }
