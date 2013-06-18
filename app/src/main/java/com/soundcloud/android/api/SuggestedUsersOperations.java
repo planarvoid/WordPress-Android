bc4a23efa76efad31b56a1ccfadb265ca196bc57
Matthias Kaeppler <mk@soundcloud.com>
2013-06-18 18:06:16 +0200

    Make sure we schedule calls on SuggestedUsersOperatios

--- app/src/main/java/com/soundcloud/android/api/SuggestedUsersOperations.java
+++ app/src/main/java/com/soundcloud/android/api/SuggestedUsersOperations.java
@@ -34,7 +34,7 @@ public class SuggestedUsersOperations extends ScheduledOperations {
                 .forPrivateAPI()
                 .forResource(new TypeToken<List<CategoryGroup>>() {})
                 .build();
-        return mRxHttpClient.executeAPIRequest(request);
+        return schedule(mRxHttpClient.<CategoryGroup>executeAPIRequest(request));
     }
 
     public Observable<CategoryGroup> getFacebookSuggestions() {
@@ -43,10 +43,10 @@ public class SuggestedUsersOperations extends ScheduledOperations {
                 .forPrivateAPI()
                 .forResource(new TypeToken<List<CategoryGroup>>() {})
                 .build();
-        return mRxHttpClient.executeAPIRequest(request);
+        return schedule(mRxHttpClient.<CategoryGroup>executeAPIRequest(request));
     }
 
     public Observable<CategoryGroup> getCategoryGroups() {
-        return Observable.merge(getMusicAndSoundsSuggestions(), getFacebookSuggestions());
+        return schedule(Observable.merge(getMusicAndSoundsSuggestions(), getFacebookSuggestions()));
     }
 }
