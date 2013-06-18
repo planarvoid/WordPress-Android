915a48c9e5d90afbf2d0a798d87e0861f5f32729
jonschmidt <jon@soundcloud.com>
2013-06-18 15:13:03 +0200

    set SoundCloudApplication.instance to avoid NPEs for now

--- tests/src/test/java/com/soundcloud/android/robolectric/SoundCloudTestRunner.java
+++ tests/src/test/java/com/soundcloud/android/robolectric/SoundCloudTestRunner.java
@@ -1,15 +1,26 @@
 package com.soundcloud.android.robolectric;
 
 
+import com.soundcloud.android.SoundCloudApplication;
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricConfig;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.runners.model.InitializationError;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public class SoundCloudTestRunner extends RobolectricTestRunner {
 
     public SoundCloudTestRunner(Class testClass) throws InitializationError {
-        super(testClass,new RobolectricConfig(new File("../app")));
+        super(testClass, new RobolectricConfig(new File("../app")));
+    }
+
+    @Override
+    public void beforeTest(Method method) {
+        super.beforeTest(method);
+
+        // until we have a DI framework we have to set this instance to avoid NPEs
+        SoundCloudApplication.instance = Robolectric.application;
     }
 }
