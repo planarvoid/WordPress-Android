2e99979a1c02d68be203fd819a6f14cddcf23b44
jonschmidt <jon@soundcloud.com>
2013-06-28 14:34:31 +0200

    Filter out duplicate users based on a master suggested user set

--- tests/src/test/java/com/soundcloud/android/model/CategoryGroupTest.java
+++ tests/src/test/java/com/soundcloud/android/model/CategoryGroupTest.java
@@ -13,6 +13,8 @@ import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 @RunWith(SoundCloudTestRunner.class)
 public class CategoryGroupTest {
@@ -24,10 +26,16 @@ public class CategoryGroupTest {
     Category category1;
     @Mock
     Category category2;
+    private SuggestedUser suggestedUser1;
+    private SuggestedUser suggestedUser2;
+    private SuggestedUser suggestedUser3;
 
     @Before
     public void before() {
         mCategoryGroup = new CategoryGroup(KEY);
+        suggestedUser1 = new SuggestedUser("soundcloud:users:1");
+        suggestedUser2 = new SuggestedUser("soundcloud:users:2");
+        suggestedUser3 = new SuggestedUser("soundcloud:users:3");
     }
 
     @Test
@@ -39,7 +47,7 @@ public class CategoryGroupTest {
 
     @Test
     public void shouldReturnAllCategories() throws CreateModelException {
-        final ArrayList<SuggestedUser> suggestedUsers = Lists.newArrayList(new SuggestedUser("urn1"), new SuggestedUser("urn2"));
+        final ArrayList<SuggestedUser> suggestedUsers = Lists.newArrayList(suggestedUser1, suggestedUser2);
         when(category1.getUsers()).thenReturn(suggestedUsers);
         when(category2.getUsers()).thenReturn(suggestedUsers);
 
@@ -49,7 +57,7 @@ public class CategoryGroupTest {
 
     @Test
     public void shouldReturnNonEmptyCategories() throws CreateModelException {
-        final ArrayList<SuggestedUser> suggestedUsers = Lists.newArrayList(new SuggestedUser("urn1"), new SuggestedUser("urn2"));
+        final ArrayList<SuggestedUser> suggestedUsers = Lists.newArrayList(suggestedUser1, suggestedUser2);
         when(category1.getUsers()).thenReturn(suggestedUsers);
         when(category2.getUsers()).thenReturn(Collections.<SuggestedUser>emptyList());
 
@@ -78,5 +86,51 @@ public class CategoryGroupTest {
         when(category1.getUsers()).thenReturn(Collections.<SuggestedUser>emptyList());
         mCategoryGroup.setCategories(Lists.newArrayList(category1, Category.error()));
         expect(mCategoryGroup.isEmpty()).toBeFalse();
+    }
+
+    @Test
+    public void shouldFilterDuplicatesFromCategories(){
+
+        List<SuggestedUser> category1Users = Lists.newArrayList(suggestedUser1, suggestedUser2);
+        List<SuggestedUser> category2Users = Lists.newArrayList(suggestedUser2, suggestedUser3);
+
+        when(category1.getUsers()).thenReturn(category1Users);
+        when(category2.getUsers()).thenReturn(category2Users);
+        mCategoryGroup.setCategories(Lists.newArrayList(category1, category2));
+        mCategoryGroup.filterCategories(new HashSet<SuggestedUser>());
+        expect(category2Users).toContainExactly(suggestedUser3);
+    }
+
+    @Test
+    public void shouldNotReturnEmptyCateogoryAfterFiltering(){
+        List<SuggestedUser> category1Users = Lists.newArrayList(suggestedUser1, suggestedUser2, suggestedUser3);
+        List<SuggestedUser> category2Users = Lists.newArrayList(suggestedUser2, suggestedUser3);
+
+        when(category1.getUsers()).thenReturn(category1Users);
+        when(category2.getUsers()).thenReturn(category2Users);
+        mCategoryGroup.setCategories(Lists.newArrayList(category1, category2));
+
+        mCategoryGroup.filterCategories(new HashSet<SuggestedUser>());
+        expect(category2Users).toBeEmpty();
+        expect(mCategoryGroup.getNonEmptyCategories()).not.toContain(category2);
+    }
+
+    @Test
+    public void shouldFilterDuplicatesFromCategoriesOverMultipleGroups(){
+        List<SuggestedUser> category1Users = Lists.newArrayList(suggestedUser1, suggestedUser2);
+        List<SuggestedUser> category2Users = Lists.newArrayList(suggestedUser2, suggestedUser3);
+
+        when(category1.getUsers()).thenReturn(category1Users);
+        when(category2.getUsers()).thenReturn(category2Users);
+        mCategoryGroup.setCategories(Lists.newArrayList(category1));
+
+        CategoryGroup categoryGroup2 = new CategoryGroup("Key2");
+        categoryGroup2.setCategories(Lists.newArrayList(category2));
+
+        final HashSet<SuggestedUser> uniqueSuggestedUsersSet = new HashSet<SuggestedUser>();
+        mCategoryGroup.filterCategories(uniqueSuggestedUsersSet);
+        categoryGroup2.filterCategories(uniqueSuggestedUsersSet);
+        expect(category2Users).toContainExactly(suggestedUser3);
+
     }
 }
