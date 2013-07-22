60dd902e6ea51ee49cdec29880c4fb61067aa1e8
jonschmidt <jon@soundcloud.com>
2013-07-22 18:08:04 +0200

    Missing classes. Thanks gitX

--- tests-integration/src/main/java/com/soundcloud/android/screens/auth/SuggestedUsersCategoryScreen.java
+++ tests-integration/src/main/java/com/soundcloud/android/screens/auth/SuggestedUsersCategoryScreen.java
@@ -1,40 +1,26 @@
 package com.soundcloud.android.screens.auth;
 
 import com.soundcloud.android.R;
-import com.soundcloud.android.activity.landing.SuggestedUsersCategoryActivity;
 import com.soundcloud.android.tests.Han;
-import com.soundcloud.android.tests.Waiter;
+import com.soundcloud.android.view.GridViewCompat;
+
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SuggestedUsersCategoryScreen {
-    private final Waiter waiter;
+
     private Han solo;
 
     public SuggestedUsersCategoryScreen(Han driver) {
         solo = driver;
-        waiter = new Waiter(solo);
     }
 
-    public boolean hasContent(){
-        return solo.getView(R.id.suggested_users_list_header) != null;
-    }
-
-    public boolean hasFacebookSection(){
-        return solo.searchText(solo.getString(R.string.suggested_users_section_facebook), true);
-    }
-
-    public boolean hasMusicSection(){
-        return solo.searchText(solo.getString(R.string.suggested_users_section_music), true);
-    }
-
-    public boolean hasAudioSection(){
-        return solo.searchText(solo.getString(R.string.suggested_users_section_audio), true);
-    }
-
-    public void rockOut(){
-        // TODO : do not click on text, but rather listview items
-        solo.scrollListToTop(0);
-        solo.clickOnText("Rock");
-        solo.waitForActivity(SuggestedUsersCategoryActivity.class);
-        
+    public String followRandomUser(){
+        solo.waitForViewId(R.id.username, 5000);
+        GridViewCompat gridViewCompat = (GridViewCompat) solo.getCurrentGridView();
+        ViewGroup viewGroup = (ViewGroup) gridViewCompat.getChildAt((int) (Math.random() * gridViewCompat.getChildCount()));
+        TextView textView = (TextView) viewGroup.findViewById(R.id.username);
+        solo.clickOnView(textView);
+        return textView.getText().toString();
     }
 }
--- tests-integration/src/main/java/com/soundcloud/android/screens/auth/SuggestedUsersScreen.java
+++ tests-integration/src/main/java/com/soundcloud/android/screens/auth/SuggestedUsersScreen.java
@@ -1,5 +1,9 @@
 package com.soundcloud.android.screens.auth;
 
+import com.soundcloud.android.R;
+import com.soundcloud.android.activity.landing.Home;
+import com.soundcloud.android.activity.landing.SuggestedUsersCategoryActivity;
+import com.soundcloud.android.screens.HomeScreen;
 import com.soundcloud.android.tests.Han;
 import com.soundcloud.android.tests.Waiter;
 
@@ -8,9 +12,37 @@ public class SuggestedUsersScreen {
     private Han solo;
 
     public SuggestedUsersScreen(Han driver) {
-        solo    = driver;
-        waiter  = new Waiter(solo);
+        solo = driver;
+        waiter = new Waiter(solo);
     }
 
+    public boolean hasContent(){
+        return solo.getView(R.id.suggested_users_list_header) != null;
+    }
 
+    public boolean hasFacebookSection(){
+        return solo.searchText(solo.getString(R.string.suggested_users_section_facebook), true);
+    }
+
+    public boolean hasMusicSection(){
+        return solo.searchText(solo.getString(R.string.suggested_users_section_music), true);
+    }
+
+    public boolean hasAudioSection(){
+        return solo.searchText(solo.getString(R.string.suggested_users_section_audio), true);
+    }
+
+    public void rockOut(){
+        // TODO : do not click on text, but rather listview items
+        solo.scrollListToTop(0);
+        solo.clickOnText("Rock");
+        solo.waitForActivity(SuggestedUsersCategoryActivity.class);
+    }
+
+    public HomeScreen finish() {
+        solo.clickOnActionBarItem(R.id.finish);
+        solo.waitForActivity(Home.class);
+        waiter.waitForListContent();
+        return new HomeScreen(solo);
+    }
 }
