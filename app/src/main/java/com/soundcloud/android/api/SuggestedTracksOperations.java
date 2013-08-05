426e01f4e80a0e8111ccf11a1c7a38f2a42c8742
Matthias Kaeppler <mk@soundcloud.com>
2013-08-05 13:00:49 +0200

    Use Skrillex tracks as test data, it doesn't change as much

--- app/src/main/java/com/soundcloud/android/api/SuggestedTracksOperations.java
+++ app/src/main/java/com/soundcloud/android/api/SuggestedTracksOperations.java
@@ -7,7 +7,6 @@ import com.soundcloud.android.api.http.SoundCloudRxHttpClient;
 import com.soundcloud.android.model.CollectionHolder;
 import com.soundcloud.android.model.Track;
 import com.soundcloud.android.rx.ScheduledOperations;
-import com.soundcloud.api.Endpoints;
 import rx.Observable;
 
 public class SuggestedTracksOperations extends ScheduledOperations {
@@ -16,7 +15,7 @@ public class SuggestedTracksOperations extends ScheduledOperations {
     private SoundCloudRxHttpClient rxHttpClient = new SoundCloudRxHttpClient();
 
     public Observable<Observable<Track>> getSuggestedTracks() {
-        APIRequest<CollectionHolder<Track>> request = SoundCloudAPIRequest.RequestBuilder.<CollectionHolder<Track>>get(Endpoints.TRACKS + ".json")
+        APIRequest<CollectionHolder<Track>> request = SoundCloudAPIRequest.RequestBuilder.<CollectionHolder<Track>>get("/users/skrillex/tracks.json")
                 .addQueryParameters("linked_partitioning", "1")
                 .addQueryParameters("limit", "10")
                 .forPublicAPI()
