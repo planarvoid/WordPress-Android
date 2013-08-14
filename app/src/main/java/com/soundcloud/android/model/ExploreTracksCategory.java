791ab7b42d5802d856a1899088adb5ddac150abb
Jon Schmidt <jon@soundcloud.com>
2013-08-14 08:23:21 -0400

    Parcelable functionality for ExploreTrackCat

--- app/src/main/java/com/soundcloud/android/model/ExploreTracksCategory.java
+++ app/src/main/java/com/soundcloud/android/model/ExploreTracksCategory.java
@@ -5,20 +5,33 @@ import com.soundcloud.android.model.behavior.InSection;
 import com.soundcloud.android.model.behavior.Titled;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.Map;
 
-public class ExploreTracksCategory implements InSection {
+public class ExploreTracksCategory implements InSection, Parcelable  {
+
+    public static final String EXTRA = "category";
 
     private String mKey;
     private Map<String, Link> mLinks = Collections.emptyMap();
     private ExploreTracksCategorySection mSection;
 
-    public ExploreTracksCategory(){ }
+    public ExploreTracksCategory(){ /* For Deserialization */ }
 
     public ExploreTracksCategory(String key) {
         this.mKey = key;
+    }
+
+    public ExploreTracksCategory(Parcel in) {
+        Bundle b = in.readBundle(Link.class.getClassLoader());
+        mKey = b.getString("key");
+        mLinks = (Map<String, Link>) b.getSerializable("links");
+        mSection = ExploreTracksCategorySection.values()[b.getInt("section")];
     }
 
     public String getKey() {
@@ -51,4 +64,30 @@ public class ExploreTracksCategory implements InSection {
     public void setSection(ExploreTracksCategorySection section) {
         this.mSection = section;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // use a bundle to avoid typing problems with the map
+        Bundle b = new Bundle();
+        b.setClassLoader(Link.class.getClassLoader());
+        b.putString("key", mKey);
+        b.putSerializable("links", (Serializable) mLinks);
+        b.putInt("section", mSection.ordinal());
+        dest.writeBundle(b);
+    }
+
+    public static final Parcelable.Creator<ExploreTracksCategory> CREATOR = new Parcelable.Creator<ExploreTracksCategory>() {
+        public ExploreTracksCategory createFromParcel(Parcel in) {
+            return new ExploreTracksCategory(in);
+        }
+
+        public ExploreTracksCategory[] newArray(int size) {
+            return new ExploreTracksCategory[size];
+        }
+    };
 }
--- app/src/main/java/com/soundcloud/android/model/Link.java
+++ app/src/main/java/com/soundcloud/android/model/Link.java
@@ -1,7 +1,21 @@
 package com.soundcloud.android.model;
 
-public class Link {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Link implements Parcelable {
+
     private String mHref;
+
+    public Link() { /* deserialization */}
+
+    public Link(String href) {
+        this.mHref = href;
+    }
+
+    public Link(Parcel in) {
+        mHref = in.readString();
+    }
 
     public String getHref() {
         return mHref;
@@ -9,5 +23,42 @@ public class Link {
 
     public void setHref(String href) {
         this.mHref = href;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mHref);
+    }
+
+    public static final Parcelable.Creator<Link> CREATOR = new Parcelable.Creator<Link>() {
+        public Link createFromParcel(Parcel in) {
+            return new Link(in);
+        }
+
+        public Link[] newArray(int size) {
+            return new Link[size];
+        }
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Link)) return false;
+
+        Link link = (Link) o;
+
+        if (mHref != null ? !mHref.equals(link.mHref) : link.mHref != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return mHref != null ? mHref.hashCode() : 0;
     }
 }
