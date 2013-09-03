75f56129679293cba998c3e0ca95c71d4b47a08b
Mustafa Sezgin <mustafa@soundcloud.com>
2013-09-03 14:18:07 +0200

    Hook up the analytics engine to the settings toggle

--- app/src/main/java/com/soundcloud/android/analytics/AnalyticsEngine.java
+++ app/src/main/java/com/soundcloud/android/analytics/AnalyticsEngine.java
@@ -4,9 +4,12 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
+import com.soundcloud.android.activity.settings.Settings;
 import com.soundcloud.android.utils.Log;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import java.util.Collection;
 
@@ -17,26 +20,30 @@ import java.util.Collection;
  *
  * The analytics engine should be used in aspects located in the aspect folder under src/main/java
  */
-public class AnalyticsEngine {
+public class AnalyticsEngine implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     private static final String TAG = AnalyticsEngine.class.getSimpleName();
 
     private final Collection<AnalyticsProvider> mAnalyticsProviders;
     private final AnalyticsProperties mAnalyticsProperties;
+    private boolean mAnalyticsPreferenceEnabled;
 
     public AnalyticsEngine(Context context){
-        this(new AnalyticsProperties(context.getResources()), new LocalyticsAnalyticsProvider(context.getApplicationContext()));
+        this(new AnalyticsProperties(context.getResources()), PreferenceManager.getDefaultSharedPreferences(context),
+                new LocalyticsAnalyticsProvider(context.getApplicationContext()));
     }
 
     @VisibleForTesting
-    protected AnalyticsEngine(AnalyticsProperties analyticsProperties, AnalyticsProvider... analyticsProviders) {
+    protected AnalyticsEngine(AnalyticsProperties analyticsProperties, SharedPreferences sharedPreferences, AnalyticsProvider... analyticsProviders) {
         checkArgument(analyticsProviders.length > 0, "Need to provide at least one analytics provider");
         mAnalyticsProviders = Lists.newArrayList(analyticsProviders);
         mAnalyticsProperties = analyticsProperties;
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        mAnalyticsPreferenceEnabled = sharedPreferences.getBoolean(Settings.ANALYTICS, true);
     }
 
     public void openSession(){
-        if(mAnalyticsProperties.isAnalyticsDisabled()){
+        if(analyticsIsDisabled()){
             Log.d(TAG, "Analytics disabled, not opening session");
             return;
         }
@@ -47,7 +54,7 @@ public class AnalyticsEngine {
     }
 
     public void closeSession(){
-        if(mAnalyticsProperties.isAnalyticsDisabled()){
+        if(analyticsIsDisabled()){
             Log.d(TAG, "Analytics disabled, not closing session");
             return;
         }
@@ -57,4 +64,19 @@ public class AnalyticsEngine {
         }
     }
 
+    private boolean analyticsIsDisabled() {
+        return mAnalyticsProperties.isAnalyticsDisabled() || !mAnalyticsPreferenceEnabled;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(Settings.ANALYTICS.equalsIgnoreCase(key)){
+            mAnalyticsPreferenceEnabled = sharedPreferences.getBoolean(Settings.ANALYTICS, true);
+        }
+    }
+
+    @VisibleForTesting
+    protected boolean isAnalyticsPreferenceEnabled(){
+        return mAnalyticsPreferenceEnabled;
+    }
 }
--- tests/src/test/java/com/soundcloud/android/analytics/AnalyticsEngineTest.java
+++ tests/src/test/java/com/soundcloud/android/analytics/AnalyticsEngineTest.java
@@ -1,43 +1,68 @@
 package com.soundcloud.android.analytics;
 
 
-import com.soundcloud.android.robolectric.SoundCloudTestRunner;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
+import static com.soundcloud.android.Expect.expect;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
+import com.soundcloud.android.activity.settings.Settings;
+import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import android.content.SharedPreferences;
+
 @RunWith(SoundCloudTestRunner.class)
 public class AnalyticsEngineTest {
-    private AnalyticsEngine analytics;
+    private AnalyticsEngine analyticsEngine;
     @Mock
     private AnalyticsProperties analyticsProperties;
     @Mock
     private AnalyticsProvider analyticsProviderOne;
     @Mock
     private AnalyticsProvider analyticsProviderTwo;
-
-    @Before
-    public void setUp(){
-        analytics = new AnalyticsEngine(analyticsProperties, analyticsProviderOne, analyticsProviderTwo);
-    }
+    @Mock
+    private SharedPreferences sharedPreferences;
 
     @Test
     public void shouldCallOpenSessionOnAllProvidersIfAnalyticsEnabled(){
         when(analyticsProperties.isAnalyticsDisabled()).thenReturn(false);
-        analytics.openSession();
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(true);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.openSession();
         verify(analyticsProviderOne).openSession();
         verify(analyticsProviderTwo).openSession();
     }
 
     @Test
-    public void shouldNotCallOpenSessionOnAnyProvidersIfAnalyticsDisabled(){
+    public void shouldNotCallOpenSessionOnAnyProvidersIfAnalyticsPreferenceDisabled(){
+        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(false);
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(false);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.openSession();
+        verifyZeroInteractions(analyticsProviderOne);
+        verifyZeroInteractions(analyticsProviderTwo);
+    }
+
+    @Test
+    public void shouldNotCallOpenSessionOnAnyProvidersIfAnalyticsPropertyDisabled(){
         when(analyticsProperties.isAnalyticsDisabled()).thenReturn(true);
-        analytics.openSession();
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(true);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.openSession();
+        verifyZeroInteractions(analyticsProviderOne);
+        verifyZeroInteractions(analyticsProviderTwo);
+    }
+
+    @Test
+    public void shouldNotOpenCloseSessionOnAnyProvidersIfAnalyticsDisabled(){
+        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(true);
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(false);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.openSession();
         verifyZeroInteractions(analyticsProviderOne);
         verifyZeroInteractions(analyticsProviderTwo);
     }
@@ -45,17 +70,65 @@ public class AnalyticsEngineTest {
     @Test
     public void shouldCallCloseSessionOnAllProvidersIfAnalyticsEnabled(){
         when(analyticsProperties.isAnalyticsDisabled()).thenReturn(false);
-        analytics.closeSession();
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(true);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.closeSession();
         verify(analyticsProviderOne).closeSession();
         verify(analyticsProviderTwo).closeSession();
     }
 
     @Test
-    public void shouldNotCallCloseSessionOnAnyProvidersIfAnalyticsDisabled(){
-        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(true);
-        analytics.closeSession();
+    public void shouldNotCallCloseSessionOnAnyProvidersIfAnalyticsPreferenceDisabled(){
+        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(false);
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(false);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.closeSession();
         verifyZeroInteractions(analyticsProviderOne);
         verifyZeroInteractions(analyticsProviderTwo);
+    }
+
+    @Test
+    public void shouldNotCallCloseSessionOnAnyProvidersIfAnalyticsPropertyDisabled(){
+        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(true);
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(true);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.closeSession();
+        verifyZeroInteractions(analyticsProviderOne);
+        verifyZeroInteractions(analyticsProviderTwo);
+    }
+
+    @Test
+    public void shouldNotCallCloseSessionOnAnyProvidersIfAnalyticsDisabled(){
+        when(analyticsProperties.isAnalyticsDisabled()).thenReturn(true);
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(false);
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        analyticsEngine.closeSession();
+        verifyZeroInteractions(analyticsProviderOne);
+        verifyZeroInteractions(analyticsProviderTwo);
+    }
+
+    @Test
+    public void shouldRegisterItselfAsListenerForPreferenceChanges(){
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        verify(sharedPreferences).registerOnSharedPreferenceChangeListener(analyticsEngine);
+    }
+
+    @Test
+    public void shouldUpdateAnalyticsPreferenceWhenChangeOccurs(){
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        expect(analyticsEngine.isAnalyticsPreferenceEnabled()).toBeFalse();
+        when(sharedPreferences.getBoolean(Settings.ANALYTICS, true)).thenReturn(true);
+        analyticsEngine.onSharedPreferenceChanged(sharedPreferences,Settings.ANALYTICS);
+        expect(analyticsEngine.isAnalyticsPreferenceEnabled()).toBeTrue();
+    }
+
+    @Test
+    public void shouldNotUpdateAnalyticsPreferenceIfKeyIsNotAnalytics(){
+        analyticsEngine = new AnalyticsEngine(analyticsProperties, sharedPreferences, analyticsProviderOne, analyticsProviderTwo);
+        expect(analyticsEngine.isAnalyticsPreferenceEnabled()).toBeFalse();
+        SharedPreferences secondSharedPreferences = mock(SharedPreferences.class);
+        analyticsEngine.onSharedPreferenceChanged(secondSharedPreferences, Settings.ACCOUNT_SYNC_SETTINGS);
+        verifyZeroInteractions(secondSharedPreferences);
     }
 
 }
