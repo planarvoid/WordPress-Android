356d74f49cd78c5892f9a96690a987697e05f4b4
Matthias Kaeppler <mk@soundcloud.com>
2013-09-20 09:58:31 +0200

    Use m prefix for member fields in Stats

--- app/src/main/java/com/soundcloud/android/model/Stats.java
+++ app/src/main/java/com/soundcloud/android/model/Stats.java
@@ -4,44 +4,44 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Stats {
 
-    private long playbackCount;
-    private long repostsCount;
-    private long likesCount;
-    private long commentsCount;
+    private long mPlaybackCount;
+    private long mRepostsCount;
+    private long mLikesCount;
+    private long mCommentsCount;
 
     public long getPlaybackCount() {
-        return playbackCount;
+        return mPlaybackCount;
     }
 
     @JsonProperty("playback_count")
     public void setPlaybackCount(long playbackCount) {
-        this.playbackCount = playbackCount;
+        this.mPlaybackCount = playbackCount;
     }
 
     public long getRepostsCount() {
-        return repostsCount;
+        return mRepostsCount;
     }
 
     @JsonProperty("reposts_count")
     public void setRepostsCount(long repostsCount) {
-        this.repostsCount = repostsCount;
+        this.mRepostsCount = repostsCount;
     }
 
     public long getLikesCount() {
-        return likesCount;
+        return mLikesCount;
     }
 
     @JsonProperty("likes_count")
     public void setLikesCount(long likesCount) {
-        this.likesCount = likesCount;
+        this.mLikesCount = likesCount;
     }
 
     public long getCommentsCount() {
-        return commentsCount;
+        return mCommentsCount;
     }
 
     @JsonProperty("comments_count")
     public void setCommentsCount(long commentsCount) {
-        this.commentsCount = commentsCount;
+        this.mCommentsCount = commentsCount;
     }
 }
