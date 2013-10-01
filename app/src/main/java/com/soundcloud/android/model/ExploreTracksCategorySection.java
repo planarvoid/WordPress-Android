08b537034a5b296a42fc6929b8176057cacd4abb
jonschmidt <jon@soundcloud.com>
2013-10-01 15:48:09 +0200

    Revert "Explore > Discovery Renaming in resources"
    This reverts commit ff3041ddba5d6430ec268673be49bfe10a2f3132.
    
    Conflicts:
    	app/src/main/java/com/soundcloud/android/activity/landing/ExploreActivity.java

--- app/src/main/java/com/soundcloud/android/model/ExploreTracksCategorySection.java
+++ app/src/main/java/com/soundcloud/android/model/ExploreTracksCategorySection.java
@@ -4,8 +4,8 @@ package com.soundcloud.android.model;
 import com.soundcloud.android.R;
 
 public enum ExploreTracksCategorySection {
-    MUSIC(R.string.discover_category_header_music),
-    AUDIO(R.string.discover_category_header_audio);
+    MUSIC(R.string.explore_category_header_music),
+    AUDIO(R.string.explore_category_header_audio);
 
     private final int mSectionTitleId;
 
