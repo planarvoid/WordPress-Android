0a35c01116b1c6df6969b58bda4c381729e7f15d
Matthias Kaeppler <mk@soundcloud.com>
2013-10-09 18:13:11 +0200

    Remove unused method

--- app/src/main/java/com/soundcloud/android/rx/ScActions.java
+++ app/src/main/java/com/soundcloud/android/rx/ScActions.java
@@ -1,19 +1,8 @@
 package com.soundcloud.android.rx;
 
-import rx.Observable;
-import rx.Observer;
 import rx.util.functions.Action1;
 
 public class ScActions {
-
-    public static <T> Action1<Observable<T>> pendingAction(final Observer<T> observer) {
-        return new Action1<Observable<T>>() {
-            @Override
-            public void call(Observable<T> observable) {
-                observable.subscribe(observer);
-            }
-        };
-    }
 
     public static final Action1 NO_OP = new Action1() {
         @Override
