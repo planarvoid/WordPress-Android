114a2b4ed34fe10c8eea3d019c8588467235d92e
jonschmidt <jon@soundcloud.com>
2013-10-29 11:32:21 +0100

    Fixed MenuScreen Page object model

--- tests-integration/src/main/java/com/soundcloud/android/screens/MenuScreen.java
+++ tests-integration/src/main/java/com/soundcloud/android/screens/MenuScreen.java
@@ -2,11 +2,12 @@ package com.soundcloud.android.screens;
 
 import com.soundcloud.android.R;
 import com.soundcloud.android.activity.auth.Onboard;
+import com.soundcloud.android.fragment.NavigationDrawerFragment;
 import com.soundcloud.android.tests.Han;
 import com.soundcloud.android.tests.Waiter;
-import com.soundcloud.android.view.MainMenu;
 
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MenuScreen {
@@ -19,7 +20,7 @@ public class MenuScreen {
     }
 
     public void logout() {
-        solo.clickOnView(R.id.custom_home);
+        solo.clickOnView(R.id.home);
         solo.clickOnMenuItem(R.string.side_menu_settings);
         solo.clickOnText(R.string.pref_revoke_access);
         solo.assertText(R.string.menu_clear_user_title);
@@ -28,22 +29,22 @@ public class MenuScreen {
         solo.waitForViewId(R.id.tour_bottom_bar, 5000);
     }
 
-    public MainMenu rootMenu() {
-        return (MainMenu) solo.waitForViewId(R.id.nav_drawer_listview, 20000);
+    public ListView rootMenu() {
+        return (ListView) solo.waitForViewId(R.id.nav_drawer_listview, 20000);
     }
 
     public View youMenu() {
-        return rootMenu().findViewById(R.id.nav_you);
+        return rootMenu().getChildAt(NavigationDrawerFragment.NavItem.PROFILE.ordinal());
     }
 
     public String getUserName() {
-        TextView you = (TextView) youMenu().findViewById(R.id.main_menu_item_text);
+        TextView you = (TextView) youMenu().findViewById(R.id.username);
         return you.getText().toString();
     }
 
     public void openExplore() {
-        View item = rootMenu().findViewById(R.id.nav_explore);
-        solo.clickOnView(R.id.custom_home);
+        View item = rootMenu().getChildAt(NavigationDrawerFragment.NavItem.EXPLORE.ordinal());
+        solo.clickOnView(R.id.home);
         solo.clickOnView(item);
         waiter.waitForListContent();
     }
