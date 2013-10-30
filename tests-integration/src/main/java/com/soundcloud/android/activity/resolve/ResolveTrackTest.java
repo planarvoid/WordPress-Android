fef336ab6b17926ea4e209cda3cd613b1ddcc17e
Jon Schmidt <jon@soundcloud.com>
2013-10-30 14:05:48 +0100

    Brought resolve track tests back

--- tests-integration/src/main/java/com/soundcloud/android/activity/resolve/ResolveTrackTest.java
+++ tests-integration/src/main/java/com/soundcloud/android/activity/resolve/ResolveTrackTest.java
@@ -5,7 +5,7 @@ import com.soundcloud.android.activity.PlayerActivity;
 
 public abstract class ResolveTrackTest extends ResolveBaseTest {
 
-    public void ignore_testResolveUrl() throws Exception {
+    public void testResolveUrl() throws Exception {
         solo.assertActivity(PlayerActivity.class, DEFAULT_WAIT);
 
         solo.assertText("CHE FLUTE");
