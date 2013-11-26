765eb6ca62a925e4bc96c24b84c740e784043a13
jonschmidt <jon@soundcloud.com>
2013-11-26 16:53:38 +0100

    Removed legacy play queue test

---
+++ app/src/main/java/com/soundcloud/android/playback/PlaybackModule.java
@@ -0,0 +1,38 @@
+package com.soundcloud.android.playback;
+
+import com.soundcloud.android.model.ScModelManager;
+import com.soundcloud.android.playback.service.PlayQueueManager;
+import com.soundcloud.android.playback.service.PlayQueueStorage;
+import com.soundcloud.android.playback.service.PlaybackOperations;
+import dagger.Module;
+import dagger.Provides;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import javax.inject.Singleton;
+
+@Module(complete = false, library = true)
+public class PlaybackModule {
+
+    @Provides
+    @Singleton
+    PlaybackOperations providePlaybackOperations() {
+        return new PlaybackOperations();
+    }
+
+    @Provides
+    @Singleton
+    PlayQueueManager providePlayQueueManager(Context context, PlayQueueStorage playQueueStorage, PlaybackOperations playbackOperations,
+                                             SharedPreferences sharedPreferences, ScModelManager modelManager) {
+        return new PlayQueueManager(context, playQueueStorage, playbackOperations, sharedPreferences, modelManager);
+    }
+
+    @Provides
+    PlayQueueStorage providePlayQueueStorage(ContentResolver contentResolver){
+        return new PlayQueueStorage(contentResolver);
+    }
+
+
+}
