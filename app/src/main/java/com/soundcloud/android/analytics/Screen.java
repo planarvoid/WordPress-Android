b0320b96e2f4133a16d245a107258a59b9873df4
Matthias Kaeppler <mk@soundcloud.com>
2013-12-03 16:57:27 +0100

    Track user profile screens
    refs #1065
    
    This simply replaces the ATI tracking on ProfileActivity

--- app/src/main/java/com/soundcloud/android/analytics/Screen.java
+++ app/src/main/java/com/soundcloud/android/analytics/Screen.java
@@ -9,6 +9,22 @@ public enum Screen {
     SIDE_MENU_LIKES("collection:likes"),
     SIDE_MENU_PLAYLISTS("collection:playlists"),
 
+    // your own profile
+    YOUR_POSTS("you:posts"),
+    YOUR_INFO("you:info"),
+    YOUR_PLAYLISTS("you:playlists"),
+    YOUR_LIKES("you:likes"),
+    YOUR_FOLLOWINGS("you:followings"),
+    YOUR_FOLLOWERS("you:followers"),
+
+    // other user's profiles
+    USER_POSTS("users:posts"),
+    USER_INFO("users:info"),
+    USER_PLAYLISTS("users:playlists"),
+    USER_LIKES("users:likes"),
+    USER_FOLLOWINGS("users:followings"),
+    USER_FOLLOWERS("users:followers"),
+
     // explore screens
     EXPLORE_GENRES("explore:genres"),
     EXPLORE_TRENDING_MUSIC("explore:trending_music"),
