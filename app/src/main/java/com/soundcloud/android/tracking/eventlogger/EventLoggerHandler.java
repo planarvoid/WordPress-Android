648984cd536ad076713d65df75693224fd84594d
jonschmidt <jon@soundcloud.com>
2013-12-04 12:18:52 +0100

    Refactored EventLoggerHandler and added tests
    - Moved DB logic to DB helper

--- app/src/main/java/com/soundcloud/android/tracking/eventlogger/EventLoggerHandler.java
+++ app/src/main/java/com/soundcloud/android/tracking/eventlogger/EventLoggerHandler.java
@@ -1,25 +1,17 @@
 package com.soundcloud.android.tracking.eventlogger;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.soundcloud.android.SoundCloudApplication;
 import com.soundcloud.android.events.PlaybackEventData;
-import com.soundcloud.android.model.ClientUri;
 import com.soundcloud.android.utils.IOUtils;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.util.Pair;
 
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class EventLoggerHandler extends Handler {
 
@@ -48,18 +40,10 @@ public class EventLoggerHandler extends Handler {
         switch (msg.what) {
             case EventLogger.INSERT_TOKEN:
                 final PlaybackEventData params = (PlaybackEventData) msg.obj;
-
-                mTrackingDbHelper.execute(new EventLoggerDbHelper.ExecuteBlock() {
-                    @Override
-                    public void call(SQLiteDatabase database) {
-                        long id = database.insertOrThrow(EventLoggerDbHelper.EVENTS_TABLE, null,
-                                createValuesFromPlaybackEvent(params));
-                        if (id < 0) {
-                            Log.w(EventLogger.TAG, "error inserting tracking event");
-                        }
-                    }
-                });
-
+                long id = mTrackingDbHelper.insertEvent(params);
+                if (id < 0) {
+                    Log.w(EventLogger.TAG, "error inserting tracking event");
+                }
                 removeMessages(EventLogger.FLUSH_TOKEN);
                 sendMessageDelayed(obtainMessage(EventLogger.FLUSH_TOKEN), EventLogger.FLUSH_DELAY);
                 break;
@@ -78,7 +62,7 @@ public class EventLoggerHandler extends Handler {
         }
     }
 
-    /* package */ boolean flushPlaybackTrackingEvents() {
+    private boolean flushPlaybackTrackingEvents() {
         if (Log.isLoggable(EventLogger.TAG, Log.DEBUG)) Log.d(EventLogger.TAG, "flushPlaybackTrackingEvents");
 
         if (!IOUtils.isConnected(mContext)) {
@@ -86,81 +70,20 @@ public class EventLoggerHandler extends Handler {
             return true;
         }
 
-        final List<Pair<Long, String>> urls = new ArrayList<Pair<Long, String>>();
-        mTrackingDbHelper.execute(new EventLoggerDbHelper.ExecuteBlock() {
-            @Override
-            public void call(SQLiteDatabase database) {
-                Cursor cursor = database.query(EventLoggerDbHelper.EVENTS_TABLE, null, null, null, null, null,
-                        EventLoggerDbHelper.TrackingEvents.TIMESTAMP + " DESC",
-                        String.valueOf(EventLogger.BATCH_SIZE));
+        List<Pair<Long, String>> events = mTrackingDbHelper.getUnpushedEvents(mTrackingApi);
 
-                if (cursor != null) {
-                    while (cursor.moveToNext()) {
-                        final long eventId = cursor.getLong(cursor.getColumnIndex(EventLoggerDbHelper.TrackingEvents._ID));
-                        try {
-                            urls.add(Pair.create(eventId, mTrackingApi.buildUrl(cursor)));
-                        } catch (UnsupportedEncodingException e) {
-                            Log.w(EventLogger.TAG, "Failed to encode play event ", e);
-                        }
-                    }
-                    cursor.close();
+        if (!events.isEmpty()) {
+            final String[] submitted = mTrackingApi.pushToRemote(events);
+            if (submitted.length > 0) {
+                int deleted = mTrackingDbHelper.deleteEventsById(submitted);
+                if (deleted != submitted.length) {
+                    Log.w(EventLogger.TAG, "error deleting events (deleted=" + deleted + ")");
+                } else {
+                    if (Log.isLoggable(EventLogger.TAG, Log.DEBUG)) Log.d(EventLogger.TAG, "submitted " + deleted + " events");
                 }
             }
-        });
-
-        if (!urls.isEmpty()) {
-            final String[] submitted = mTrackingApi.pushToRemote(urls);
-            if (submitted.length > 0) {
-
-                mTrackingDbHelper.execute(new EventLoggerDbHelper.ExecuteBlock() {
-                    @Override
-                    public void call(SQLiteDatabase database) {
-                        StringBuilder query = new StringBuilder(submitted.length * 22 - 1);
-                        query.append(EventLoggerDbHelper.TrackingEvents._ID).append(" IN (?");
-                        for (int i = 1; i < submitted.length; i++) query.append(",?");
-                        query.append(")");
-
-                        final int deleted = database.delete(EventLoggerDbHelper.EVENTS_TABLE, query.toString(), submitted);
-                        if (deleted != submitted.length) {
-                            Log.w(EventLogger.TAG, "error deleting events (deleted=" + deleted + ")");
-                        } else {
-                            if (Log.isLoggable(EventLogger.TAG, Log.DEBUG)) Log.d(EventLogger.TAG, "submitted " + deleted + " events");
-                        }
-                    }
-                });
-
-            }
         }
 
-        return urls.size() < EventLogger.BATCH_SIZE;
-    }
-
-    private ContentValues createValuesFromPlaybackEvent(PlaybackEventData params) {
-        ContentValues values = new ContentValues();
-        values.put(EventLoggerDbHelper.TrackingEvents.TIMESTAMP, params.getTimeStamp());
-        values.put(EventLoggerDbHelper.TrackingEvents.ACTION, params.getAction().toApiName());
-        values.put(EventLoggerDbHelper.TrackingEvents.SOUND_URN, ClientUri.forTrack(params.getTrack().getId()).toString());
-        values.put(EventLoggerDbHelper.TrackingEvents.SOUND_DURATION, params.getTrack().duration);
-        values.put(EventLoggerDbHelper.TrackingEvents.USER_URN, buildUserUrn(params.getUserId()));
-        values.put(EventLoggerDbHelper.TrackingEvents.SOURCE_INFO, params.getEventLoggerParams());
-        return values;
-    }
-
-    private static String buildUserUrn(final long userId) {
-        if (userId < 0) {
-            return "anonymous:" + UUID.randomUUID();
-        } else {
-            return ClientUri.forUser(userId).toString();
-        }
-    }
-
-    @VisibleForTesting
-    Cursor eventsCursor() {
-        return mTrackingDbHelper.getWritableDatabase().query(EventLoggerDbHelper.EVENTS_TABLE, null, null, null, null, null, null);
-    }
-
-    @VisibleForTesting
-    EventLoggerDbHelper getTrackingDbHelper() {
-        return mTrackingDbHelper;
+        return events.size() < EventLogger.BATCH_SIZE;
     }
 }
--- tests/src/test/java/com/soundcloud/android/tracking/eventlogger/EventLoggerHandlerTest.java
+++ tests/src/test/java/com/soundcloud/android/tracking/eventlogger/EventLoggerHandlerTest.java
@@ -1,12 +1,115 @@
 package com.soundcloud.android.tracking.eventlogger;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import com.soundcloud.android.events.PlaybackEventData;
+import com.soundcloud.android.model.Track;
 import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.soundcloud.android.robolectric.TestHelper;
+import com.xtremelabs.robolectric.Robolectric;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import android.content.Context;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 @RunWith(SoundCloudTestRunner.class)
 public class EventLoggerHandlerTest {
 
     EventLoggerHandler eventLoggerHandler;
 
+    @Mock
+    Context context;
+    @Mock
+    EventLoggerApi api;
+    @Mock
+    EventLoggerDbHelper dbHelper;
 
+    final String trackingParams1 = "tracking=params";
+    final String trackingParams2 = "tracking=params2";
+
+    @Before
+    public void before() {
+        eventLoggerHandler = new EventLoggerHandler(Robolectric.application.getMainLooper(), context, dbHelper, api);
+        when(context.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(Robolectric.application.getSystemService(Context.CONNECTIVITY_SERVICE));
+    }
+
+    @Test
+    public void shouldInsertTrackingEventsIntoDatabase() throws Exception {
+        Track track = TestHelper.getModelFactory().createModel(Track.class);
+
+        final PlaybackEventData playbackEventData1 = new PlaybackEventData(track, Action.PLAY, 1l, trackingParams1);
+        final PlaybackEventData playbackEventData2 = new PlaybackEventData(track, Action.STOP, 2l, trackingParams2);
+
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.INSERT_TOKEN, playbackEventData1));
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.INSERT_TOKEN, playbackEventData2));
+
+        verify(dbHelper).insertEvent(playbackEventData1);
+        verify(dbHelper).insertEvent(playbackEventData2);
+    }
+
+    @Test
+    public void shouldNotFlushTrackingEventsWithNoConnection() throws Exception {
+        TestHelper.simulateOffline();
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FLUSH_TOKEN));
+        verifyZeroInteractions(dbHelper);
+    }
+
+    @Test
+    public void shouldNotFlushTrackingEventsWithNoLocalEvents() throws Exception {
+        when(dbHelper.getUnpushedEvents(api)).thenReturn(Collections.<Pair<Long, String>>emptyList());
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FLUSH_TOKEN));
+        verifyZeroInteractions(api);
+    }
+
+    @Test
+    public void shouldPushLocalEventsToApi(){
+        final ArrayList<Pair<Long, String>> pairs = setupUnpushedEvents();
+
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FLUSH_TOKEN));
+        verify(api).pushToRemote(pairs);
+    }
+
+    @Test
+    public void shouldNotTryToDeleteEventsIfNoEventsPushed(){
+        final ArrayList<Pair<Long, String>> pairs = setupUnpushedEvents();
+        when(api.pushToRemote(pairs)).thenReturn(new String[]{});
+
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FLUSH_TOKEN));
+        verify(dbHelper, never()).deleteEventsById(any(String[].class));
+    }
+
+    @Test
+    public void shouldDeletePushedEvents() throws Exception {
+        final ArrayList<Pair<Long, String>> pairs = setupUnpushedEvents();
+        final String[] strings = {"1L", "2L"};
+        when(api.pushToRemote(pairs)).thenReturn(strings);
+
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FLUSH_TOKEN));
+        verify(dbHelper).deleteEventsById(strings);
+    }
+
+    @Test
+    public void shouldFlushOnFinishToken() {
+        eventLoggerHandler.sendMessage(eventLoggerHandler.obtainMessage(EventLogger.FINISH_TOKEN));
+        verify(dbHelper).getUnpushedEvents(api);
+    }
+
+    private ArrayList<Pair<Long, String>> setupUnpushedEvents() {
+        final Pair<Long, String> pair1 = new Pair<Long, String>(1L, "url1");
+        final Pair<Long, String> pair2 = new Pair<Long, String>(2L, "url2");
+        final ArrayList<Pair<Long,String>> pairs = Lists.newArrayList(pair1, pair2);
+        when(dbHelper.getUnpushedEvents(api)).thenReturn(pairs);
+        return pairs;
+    }
 }
