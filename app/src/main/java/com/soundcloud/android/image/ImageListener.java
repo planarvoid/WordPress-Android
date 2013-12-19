1c8e1062241d8cef805cb093c501aaaa2eade0a3
Pedro Lopes <lopes.m.pedro@gmail.com>
2013-12-19 14:55:07 +0100

    Remove unnecessary public access modifier on methods

--- app/src/main/java/com/soundcloud/android/image/ImageListener.java
+++ app/src/main/java/com/soundcloud/android/image/ImageListener.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 import android.view.View;
 
 public interface ImageListener {
-    public void onLoadingStarted(String imageUri, View view);
-    public void onLoadingFailed(String imageUri, View view, String failedReason);
-    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage);
+    void onLoadingStarted(String imageUri, View view);
+    void onLoadingFailed(String imageUri, View view, String failedReason);
+    void onLoadingComplete(String imageUri, View view, Bitmap loadedImage);
 }
