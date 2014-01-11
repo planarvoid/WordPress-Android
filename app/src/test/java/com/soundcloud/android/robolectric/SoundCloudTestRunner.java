95dba8818f33e6ad2a688a6c042ac92368193aa0
Matthias Kaeppler <mk@soundcloud.com>
2014-01-11 11:48:51 +0100

    Create afterTest hook in SoundCloudTestRunner that resets event queue
    This is hacky but strengthens test isolation by someone forgetting to unsubscribe from a queue in a unit test. It will also fail the test in case the reset itself fails.

--- app/src/test/java/com/soundcloud/android/robolectric/SoundCloudTestRunner.java
+++ app/src/test/java/com/soundcloud/android/robolectric/SoundCloudTestRunner.java
@@ -1,6 +1,7 @@
 package com.soundcloud.android.robolectric;
 
 import com.soundcloud.android.SoundCloudApplication;
+import com.soundcloud.android.events.EventBus;
 import com.soundcloud.android.robolectric.shadows.ScShadowParcel;
 import com.soundcloud.android.robolectric.shadows.ShadowV4Fragment;
 import com.xtremelabs.robolectric.Robolectric;
@@ -8,9 +9,14 @@ import com.xtremelabs.robolectric.RobolectricConfig;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.runners.model.InitializationError;
 import org.mockito.MockitoAnnotations;
+import rx.Observer;
+import rx.Subscription;
+import rx.subjects.PublishSubject;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 public class SoundCloudTestRunner extends RobolectricTestRunner {
 
@@ -30,6 +36,40 @@ public class SoundCloudTestRunner extends RobolectricTestRunner {
 
         // until we have a DI framework we have to set this instance to avoid NPEs
         SoundCloudApplication.instance = (SoundCloudApplication) Robolectric.application;
+    }
+
+
+    @Override
+    public void afterTest(Method method) {
+        super.afterTest(method);
+
+        resetEventBusSubscriptions();
+    }
+
+    // hacky, but we need to ensure that forgetting to unsubscribe from an event queue does not interfere with other
+    // tests due to stale observers still receiving messages and having knock-on effects.
+    private void resetEventBusSubscriptions() {
+        for (EventBus bus : EventBus.values()) {
+            PublishSubject queue = bus.QUEUE;
+            Class<? extends PublishSubject> clazz = queue.getClass();
+            try {
+                Field stateField = clazz.getDeclaredField("state");
+                stateField.setAccessible(true);
+                Object subjectState = stateField.get(queue);
+                Class<?> subjectStateClass = subjectState.getClass();
+                Field observersField = subjectStateClass.getDeclaredField("observers");
+                observersField.setAccessible(true);
+                Map<Subscription, Observer> observers = (Map<Subscription, Observer>) observersField.get(subjectState);
+
+                for (Subscription s : observers.keySet()) {
+                    System.err.println("Force unsubscribing from event queue " + bus.name() + "; forgot to call unsubscribe?");
+                    s.unsubscribe();
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException("Failed resetting event queue " + bus.name() + ": " + e);
+            }
+        }
     }
 
     @Override
