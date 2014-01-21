93835496e13358bba2c2b14e6ba81d26bd0a8524
Jamie McDonald <jdamcd@gmail.com>
2014-01-21 13:33:54 +0100

    Posting UIEvent for drawer open and close
    Refs #1203

--- app/src/main/java/com/soundcloud/android/main/NavigationDrawerFragment.java
+++ app/src/main/java/com/soundcloud/android/main/NavigationDrawerFragment.java
@@ -1,6 +1,8 @@
 package com.soundcloud.android.main;
 
 import com.soundcloud.android.R;
+import com.soundcloud.android.events.EventBus;
+import com.soundcloud.android.events.UIEvent;
 import org.jetbrains.annotations.Nullable;
 
 import android.content.res.Configuration;
@@ -116,6 +118,7 @@ public class NavigationDrawerFragment extends NavigationFragment {
                     return;
                 }
                 getActivity().supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+                EventBus.UI.publish(UIEvent.fromDrawerClose());
             }
 
             @Override
@@ -125,6 +128,7 @@ public class NavigationDrawerFragment extends NavigationFragment {
                     return;
                 }
                 getActivity().supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+                EventBus.UI.publish(UIEvent.fromDrawerOpen());
             }
         };
 
