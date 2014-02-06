43620f68bebf3a91c09b454a9bd1b3d395949780
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-02-06 17:31:59 +0100

    Acceptance tests for the first two playlist discovery stories
    Missing the implementation of testTypingSearchFieldPresentsSearchSuggestions and testClearingSearchFieldReturnsToDisplayingTags
    
    Refs #1247

---
+++ tests-integration/src/main/java/com/soundcloud/android/search/Search.java
@@ -0,0 +1,68 @@
+package com.soundcloud.android.search;
+
+
+import static com.soundcloud.android.tests.TestUser.testUser;
+
+import com.soundcloud.android.main.MainActivity;
+import com.soundcloud.android.screens.MainScreen;
+import com.soundcloud.android.screens.search.SearchPlaylistTagsScreen;
+import com.soundcloud.android.screens.search.SearchResultsScreen;
+import com.soundcloud.android.tests.AccountAssistant;
+import com.soundcloud.android.tests.ActivityTestCase;
+import com.soundcloud.android.tests.Waiter;
+
+public class Search extends ActivityTestCase<MainActivity> {
+
+    private MainScreen mainScreen;
+
+    public Search() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        AccountAssistant.loginAs(getInstrumentation(), testUser.getUsername(), testUser.getPassword());
+        super.setUp();
+
+        waiter = new Waiter(solo);
+        mainScreen = new MainScreen(solo);
+    }
+
+    public void testTappingSearchIconOpensFullPageSearch() {
+        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+        assertEquals("Playlist tags screen should be visible", true, playlistTagsScreen.isVisible());
+    }
+
+    public void testClickingPhysicalSearchButtonOpensFullPageSearch() {
+        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.clickPhysicalSearchButton();
+        assertEquals("Playlist tags screen should be visible", true, playlistTagsScreen.isVisible());
+    }
+
+    public void testSubmittingSearchQueryOpensSearchResults() {
+        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        assertEquals("Search results screen should be visible", true, resultsScreen.isVisible());
+    }
+
+    public void testSubmittingSearchQueryRendersSearchResultsList() {
+        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        assertTrue("Search results should be populated", resultsScreen.getResultItemCount() > 0);
+    }
+
+    public void testGoingBackFromSearchResultsReturnsToPreviousPage() {
+        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        MainScreen mainScreen = resultsScreen.pressBack();
+        assertEquals("Main screen should be visible", true, mainScreen.isVisible());
+    }
+
+    public void testTypingSearchFieldPresentsSearchSuggestions() {
+        // To be implemented
+    }
+
+    public void testClearingSearchFieldReturnsToDisplayingTags() {
+        // To be implemented
+    }
+
+}
