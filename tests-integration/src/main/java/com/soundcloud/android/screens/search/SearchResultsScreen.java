cf3fed4f9535bed77b715cb421a515953eb24c02
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-02-19 15:33:02 +0100

    Add acceptance tests for new tabbed search results
    Refs #1286

--- tests-integration/src/main/java/com/soundcloud/android/screens/search/SearchResultsScreen.java
+++ tests-integration/src/main/java/com/soundcloud/android/screens/search/SearchResultsScreen.java
@@ -9,35 +9,49 @@ import com.soundcloud.android.screens.PlayerScreen;
 import com.soundcloud.android.screens.PlaylistDetailsScreen;
 import com.soundcloud.android.screens.ProfileScreen;
 import com.soundcloud.android.screens.Screen;
+import com.soundcloud.android.screens.elements.ViewPagerElement;
 import com.soundcloud.android.search.CombinedSearchActivity;
 import com.soundcloud.android.tests.Han;
+import com.soundcloud.android.view.SlidingTabLayout;
 
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class SearchResultsScreen extends Screen {
     private static final Class ACTIVITY = CombinedSearchActivity.class;
 
+    private static final String ALL_TAB_TEXT = "ALL";
+    private static final String TRACKS_TAB_TEXT = "TRACKS";
+    private static final String PLAYLISTS_TAB_TEXT = "PLAYLISTS";
+    private static final String PEOPLE_TAB_TEXT = "PEOPLE";
+
+    private ViewPagerElement viewPager;
+
     public SearchResultsScreen(Han solo) {
         super(solo);
+        viewPager = new ViewPagerElement(solo);
         waiter.waitForActivity(getActivity());
     }
 
     public PlayerScreen clickFirstTrackItem() {
-        View itemView = getFirstItemByClass(Track.class);
+        View itemView = getFirstResultsItemByClass(Track.class);
         solo.clickOnView(itemView);
         return new PlayerScreen(solo);
     }
 
     public PlaylistDetailsScreen clickFirstPlaylistItem() {
-        View itemView = getFirstItemByClass(Playlist.class);
+        View itemView = getFirstResultsItemByClass(Playlist.class);
         solo.clickOnView(itemView);
         return new PlaylistDetailsScreen(solo);
     }
 
     public ProfileScreen clickFirstUserItem() {
-        View itemView = getFirstItemByClass(User.class);
+        View itemView = getFirstResultsItemByClass(User.class);
         solo.clickOnView(itemView);
         return new ProfileScreen(solo);
     }
@@ -48,20 +62,68 @@ public class SearchResultsScreen extends Screen {
         return new MainScreen(solo);
     }
 
+    public void touchAllTab() {
+        touchTab(ALL_TAB_TEXT);
+        waiter.waitForListContentAndRetryIfLoadingFailed();
+    }
+
+    public void touchTracksTab() {
+        touchTab(TRACKS_TAB_TEXT);
+        waiter.waitForListContentAndRetryIfLoadingFailed();
+    }
+
+    public void touchPlaylistsTab() {
+        touchTab(PLAYLISTS_TAB_TEXT);
+        waiter.waitForListContentAndRetryIfLoadingFailed();
+    }
+
+    public void touchPeopleTab() {
+        touchTab(PEOPLE_TAB_TEXT);
+        waiter.waitForListContentAndRetryIfLoadingFailed();
+    }
+
+    private boolean touchTab(String tabText) {
+        List<View> indicatorItems = getViewPagerIndicator().getChildAt(0).getTouchables();
+        for(View view : indicatorItems){
+            if(((TextView)view).getText().equals(tabText)){
+                solo.performClick(view);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String currentTabTitle(){
+        waiter.waitForItemCountToIncrease(resultsList().getAdapter(), 0);
+        List<View> indicatorItems = getViewPagerIndicator().getChildAt(0).getTouchables();
+        TextView selectedItem = (TextView) indicatorItems.get(getViewPager().getCurrentItem());
+        return selectedItem.getText().toString();
+    }
+
+    private ViewPager getViewPager() {
+        return (ViewPager)solo.getView(R.id.pager);
+    }
+
+    private SlidingTabLayout getViewPagerIndicator() {
+        return (SlidingTabLayout)solo.getView(R.id.sliding_tabs);
+    }
+
     public int getResultItemCount() {
+        waiter.waitForItemCountToIncrease(resultsList().getAdapter(), 0);
         return resultsList().getAdapter().getCount();
     }
 
     private ListView resultsList() {
-        return solo.getCurrentListView();
+        return (ListView)viewPager.getCurrentPage(ListView.class);
     }
 
-    private View getFirstItemByClass(Class itemClass) {
-        ListAdapter adapter = solo.getCurrentListView().getAdapter();
+    private View getFirstResultsItemByClass(Class itemClass) {
+        ListAdapter adapter = resultsList().getAdapter();
+        waiter.waitForItemCountToIncrease(adapter, 0);
         int numberOfItems = adapter.getCount();
         for (int i = 0; i < numberOfItems; i++) {
             if(itemClass.isInstance(adapter.getItem(i))) {
-                return solo.getCurrentListView().getChildAt(i);
+                return resultsList().getChildAt(i);
             }
         }
         return null;
--- tests-integration/src/main/java/com/soundcloud/android/search/Search.java
+++ tests-integration/src/main/java/com/soundcloud/android/search/Search.java
@@ -1,8 +1,6 @@
 package com.soundcloud.android.search;
 
 
-import static com.soundcloud.android.tests.TestUser.testUser;
-
 import com.soundcloud.android.main.MainActivity;
 import com.soundcloud.android.screens.MainScreen;
 import com.soundcloud.android.screens.PlayerScreen;
@@ -17,6 +15,7 @@ import com.soundcloud.android.tests.Waiter;
 public class Search extends ActivityTestCase<MainActivity> {
 
     private MainScreen mainScreen;
+    private SearchPlaylistTagsScreen playlistTagsScreen;
 
     public Search() {
         super(MainActivity.class);
@@ -24,37 +23,33 @@ public class Search extends ActivityTestCase<MainActivity> {
 
     @Override
     public void setUp() throws Exception {
-        AccountAssistant.loginAs(getInstrumentation(), testUser.getUsername(), testUser.getPassword());
+        AccountAssistant.loginAsDefault(getInstrumentation());
         super.setUp();
 
         waiter = new Waiter(solo);
         mainScreen = new MainScreen(solo);
+        playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
     }
 
     public void testTappingSearchIconOpensFullPageSearch() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
         assertEquals("Playlist tags screen should be visible", true, playlistTagsScreen.isVisible());
     }
 
     public void testClickingPhysicalSearchButtonOpensFullPageSearch() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.clickPhysicalSearchButton();
         assertEquals("Playlist tags screen should be visible", true, playlistTagsScreen.isVisible());
     }
 
     public void testSubmittingSearchQueryOpensSearchResults() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
         SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
         assertEquals("Search results screen should be visible", true, resultsScreen.isVisible());
     }
 
     public void testSubmittingSearchQueryRendersSearchResultsList() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
         SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
         assertTrue("Search results should be populated", resultsScreen.getResultItemCount() > 0);
     }
 
     public void testGoingBackFromSearchResultsReturnsToPreviousPage() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
         SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
         MainScreen mainScreen = resultsScreen.pressBack();
         assertEquals("Main screen should be visible", true, mainScreen.isVisible());
@@ -68,25 +63,54 @@ public class Search extends ActivityTestCase<MainActivity> {
         // To be implemented
     }
 
-    public void testTappingTrackOpensPlayer() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+    public void testTappingTrackOnAllTabOpensPlayer() {
         SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("track");
         PlayerScreen playerScreen = resultsScreen.clickFirstTrackItem();
         assertEquals("Player screen should be visible", true, playerScreen.isVisible());
     }
 
-    public void testTappingPlaylistOpensPlaylistDetails() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
+    public void testTappingPlaylistOnAllTabOpensPlaylistDetails() {
         SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("track playlist");
         PlaylistDetailsScreen playlistScreen = resultsScreen.clickFirstPlaylistItem();
         assertEquals("Playlist screen should be visible", true, playlistScreen.isVisible());
     }
 
-    public void testTappingUserOpensProfile() {
-        SearchPlaylistTagsScreen playlistTagsScreen = mainScreen.actionBar().clickSearchButton();
-        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("skrillex");
+    public void testTappingUserOnAllTabOpensProfile() {
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
         ProfileScreen profileScreen = resultsScreen.clickFirstUserItem();
         assertEquals("Profile screen should be visible", true, profileScreen.isVisible());
+    }
+
+    public void testTappingTrackOnTracksTabOpensPlayer() {
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        resultsScreen.touchTracksTab();
+        PlayerScreen playerScreen = resultsScreen.clickFirstTrackItem();
+        assertEquals("Player screen should be visible", true, playerScreen.isVisible());
+    }
+
+    public void testTappingPlaylistOnPlaylistsTabOpensPlaylistDetails() {
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        resultsScreen.touchPlaylistsTab();
+        PlaylistDetailsScreen playlistScreen = resultsScreen.clickFirstPlaylistItem();
+        assertEquals("Playlist screen should be visible", true, playlistScreen.isVisible());
+    }
+
+    public void testTappingUserOnPeopleTabOpensProfile() {
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        resultsScreen.touchPeopleTab();
+        ProfileScreen profileScreen = resultsScreen.clickFirstUserItem();
+        assertEquals("Profile screen should be visible", true, profileScreen.isVisible());
+    }
+
+    public void testOrderOfDisplayededTabs() {
+        SearchResultsScreen resultsScreen = playlistTagsScreen.actionBar().doSearch("dub");
+        assertEquals("Current tab should be ALL", "ALL", resultsScreen.currentTabTitle());
+        resultsScreen.swipeLeft();
+        assertEquals("Current tab should be TRACKS", "TRACKS", resultsScreen.currentTabTitle());
+        resultsScreen.swipeLeft();
+        assertEquals("Current tab should be PLAYLISTS", "PLAYLISTS", resultsScreen.currentTabTitle());
+        resultsScreen.swipeLeft();
+        assertEquals("Current tab should be PEOPLE", "PEOPLE", resultsScreen.currentTabTitle());
     }
 
 }
