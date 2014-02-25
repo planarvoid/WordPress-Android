56bab5698a1941fc1e4d9f48f43f2a9791aadb83
Jon Schmidt <jon@soundcloud.com>
2014-02-25 09:49:34 +0100

    Reset StreamLoader state in preparation for new stream
    Fixes #1314
    When changing tracks, we need to ensure that tasks related to the new stream will be processed as quickly as possible, so this will clear any pending queues/tasks relating to the old stream (except for play count tasks which should still happen).

--- app/src/main/java/com/soundcloud/android/playback/streaming/ItemQueue.java
+++ app/src/main/java/com/soundcloud/android/playback/streaming/ItemQueue.java
@@ -68,6 +68,10 @@ class ItemQueue implements Iterable<StreamItem> {
         return mItems.size();
     }
 
+    public void clear(){
+        mItems.clear();
+    }
+
     @Override
     public Iterator<StreamItem> iterator() {
         // provide a copy of the data so we can manipulate the queue while iterating over it
