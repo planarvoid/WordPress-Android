9ccf49eb1ea0d1563de9eeeb0cad1e672d0f1d7f
Jamie McDonald <jdamcd@gmail.com>
2014-02-27 18:04:03 +0100

    Fix for Dagger issue with tablet main layout

--- app/src/main/java/com/soundcloud/android/main/MainModule.java
+++ app/src/main/java/com/soundcloud/android/main/MainModule.java
@@ -3,6 +3,6 @@ package com.soundcloud.android.main;
 import dagger.Module;
 
 @Module(complete = false,
-        injects = {MainActivity.class, NavigationDrawerFragment.class})
+        injects = {MainActivity.class, NavigationFragment.class, NavigationDrawerFragment.class})
 public class MainModule {
 }
