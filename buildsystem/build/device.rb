5c06112b946f2c5fd6e890fc0b86c9b327df6432
Slawomir Smiechura <slawomir@soundcloud.com>
2014-03-11 15:14:19 +0100

    Fetch device screenshots

--- buildsystem/build/device.rb
+++ buildsystem/build/device.rb
@@ -2,8 +2,8 @@ module Build
   class Device
     def self.get_screenshots
       if !$?.success?
-        Build.run_command_silent "adb pull /sdcard/Robotium-Screenshots"
-        Build.run_command_silent "adb shell rm -R /sdcard/Robotium-Screenshots"
+        Build.run_command_silent "adb -d pull /sdcard/Robotium-Screenshots"
+        Build.run_command_silent "adb -d shell rm -R /sdcard/Robotium-Screenshots"
       end
     end
   end
