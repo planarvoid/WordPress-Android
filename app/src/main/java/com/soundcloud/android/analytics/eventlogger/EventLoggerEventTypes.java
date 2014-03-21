dd7d4851e03dc38c7749a79901f85002b9868f55
jonschmidt <jon@soundcloud.com>
2014-03-21 10:58:31 +0100

    Fixed audio performance path

--- app/src/main/java/com/soundcloud/android/analytics/eventlogger/EventLoggerEventTypes.java
+++ app/src/main/java/com/soundcloud/android/analytics/eventlogger/EventLoggerEventTypes.java
@@ -1,7 +1,7 @@
 package com.soundcloud.android.analytics.eventlogger;
 
 enum EventLoggerEventTypes {
-    PLAYBACK("audio"), PLAYBACK_PERFORMANCE("audio-performance");
+    PLAYBACK("audio"), PLAYBACK_PERFORMANCE("audio_performance");
 
     private String mPath;
     private EventLoggerEventTypes(String path){
