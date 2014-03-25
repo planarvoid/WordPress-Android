34814245ab430c24f7f1c6e4344eb3c7db0717eb
Matthias Kaeppler <mk@soundcloud.com>
2014-03-25 18:28:38 +0100

    Temporary my ass! :-P

--- app/src/main/java/com/soundcloud/android/api/TempEndpoints.java
+++ app/src/main/java/com/soundcloud/android/api/TempEndpoints.java
@@ -10,25 +10,17 @@ public interface TempEndpoints {
     String USER_PLAYLISTS       = "/users/%d/playlists";
     String SEARCH               = "/search";
     String TRACK_SEARCH         = "/search/sounds";
-    String PLAYLIST_SEARCH      = "/search/sets";
-    String USER_SEARCH          = "/search/people";
 
     public interface e1 {
         String MY_STREAM           = "/e1/me/stream";
         String MY_ACTIVITIES       = "/e1/me/activities";
-        String MY_TRACK_LIKES      = "/e1/me/track_likes";
         String MY_TRACK_LIKE       = "/e1/me/track_likes/%d";
-        String MY_TRACK_REPOSTS    = "/e1/me/track_reposts";
         String MY_TRACK_REPOST     = "/e1/me/track_reposts/%d";
-        String MY_PLAYLIST_LIKES   = "/e1/me/playlist_likes";
         String MY_PLAYLIST_LIKE    = "/e1/me/playlist_likes/%d";
-        String MY_PLAYLIST_REPOSTS = "/e1/me/playlist_reposts";
         String MY_PLAYLIST_REPOST  = "/e1/me/playlist_reposts/%d";
         String USER_REPOSTS        = "/e1/users/%d/reposts";
         String USER_LIKES          = "/e1/users/%d/likes";
-        String MY_SOUNDS           = "/e1/me/sounds";
         String MY_SOUNDS_MINI      = "/e1/me/sounds/mini";
-        String MY_LIKES            = "/e1/me/likes";
         String USER_SOUNDS         = "/e1/users/%d/sounds";
         String TRACK_REPOSTERS     = "/e1/tracks/%d/reposters";
         String PLAYLIST_LIKERS     = "/e1/playlists/%d/likers";
