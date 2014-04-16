94ffcdddf7a358667f2f87bd1bc8b868a4276828
Matthias Kaeppler <mk@soundcloud.com>
2014-04-16 14:38:42 +0200

    Make CLEAR_TASK actually work for LogoutActivity

--- app/src/main/java/com/soundcloud/android/accounts/LogoutActivity.java
+++ app/src/main/java/com/soundcloud/android/accounts/LogoutActivity.java
@@ -2,25 +2,21 @@ package com.soundcloud.android.accounts;
 
 import com.soundcloud.android.R;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.IntentCompat;
 
 public class LogoutActivity extends FragmentActivity {
 
-    @TargetApi(11)
     public static void start(Activity activity) {
         Intent intent = new Intent(activity, LogoutActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // so that at least on newer devices, backing out of this actually exits the app
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
         activity.startActivity(intent);
-        activity.finish();
     }
 
     @Override
