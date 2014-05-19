6929d4f33264980b7cf08780e17e3d8ba5ae8a59
Jamie McDonald <jdamcd@gmail.com>
2014-05-19 14:48:25 +0200

    Some clearer naming in PlayerArtworkImageView

--- app/src/main/java/com/soundcloud/android/playback/ui/PlayerArtworkImageView.java
+++ app/src/main/java/com/soundcloud/android/playback/ui/PlayerArtworkImageView.java
@@ -7,7 +7,6 @@ import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
-
 /**
  * Offsets the imageview source by 20% from the top
  * Used by {@link com.soundcloud.android.explore.ExploreTracksAdapter}
@@ -22,46 +21,46 @@ public class PlayerArtworkImageView extends AspectRatioImageView {
         setScaleType(ScaleType.MATRIX);
     }
 
-
     public void setProgressProportion(float progress) {
         this.progress = progress;
         setImageMatrix(getWidth(), getHeight());
     }
 
     @Override
-    protected boolean setFrame(int l, int t, int r, int b) {
-        setImageMatrix(r - l, b - t);
-        return super.setFrame(l, t, r, b);
+    protected boolean setFrame(int left, int top, int right, int bottom) {
+        setImageMatrix(right - left, bottom - top);
+        return super.setFrame(left, top, right, bottom);
     }
 
-    private void setImageMatrix(int vwidth, int vheight) {
+    private void setImageMatrix(int vWidth, int vHeight) {
         final Drawable drawable = getDrawable();
         if (drawable != null) {
 
-            final int dwidth = drawable.getIntrinsicWidth();
-            final int dheight = drawable.getIntrinsicHeight();
+            final int dWidth = drawable.getIntrinsicWidth();
+            final int dHeight = drawable.getIntrinsicHeight();
 
             float scale;
-            float dx = 0, dy = 0;
+            float dX = 0, dY = 0;
             final Matrix matrix = getImageMatrix();
 
-            if (dwidth * vheight > vwidth * dheight) {
-                scale = (float) vheight / (float) dheight;
-                final int excessWidth = (int) (dwidth * scale - vwidth);
+            if (dWidth * vHeight > vWidth * dHeight) {
+                scale = (float) vHeight / (float) dHeight;
+                final int excessWidth = (int) (dWidth * scale - vWidth);
                 final float offset = -excessWidth / 2 + excessWidth * progress;
-                dx = (vwidth - dwidth * scale) * 0.5f - offset;
-                // optimisation. Do not redraw unnecessarily
-                if (Math.abs(dx - lastDx) > .0001) {
-                    lastDx = dx;
+                dX = (vWidth - dWidth * scale) * 0.5f - offset;
+
+                // Optimisation prevents unnecessary redraw in the same position
+                if (Math.abs(dX - lastDx) > .0001) {
+                    lastDx = dX;
                     invalidate();
                 }
             } else {
-                scale = (float) vwidth / (float) dwidth;
-                dy = (vheight - dheight * scale) * 0.5f;
+                scale = (float) vWidth / (float) dWidth;
+                dY = (vHeight - dHeight * scale) * 0.5f;
             }
 
             matrix.setScale(scale, scale);
-            matrix.postTranslate(dx, dy);
+            matrix.postTranslate(dX, dY);
             setImageMatrix(matrix);
         }
     }
