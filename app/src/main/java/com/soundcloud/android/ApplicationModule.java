543412761de67e1ece14f520784201a98271daff
jonschmidt <jon@soundcloud.com>
2014-05-20 12:19:02 +0200

    Moved IRemoteAudioManager provider to appModule
    Will fix dependency issue in build

--- app/src/main/java/com/soundcloud/android/ApplicationModule.java
+++ app/src/main/java/com/soundcloud/android/ApplicationModule.java
@@ -4,6 +4,9 @@ import com.soundcloud.android.api.ApiModule;
 import com.soundcloud.android.creators.record.SoundRecorder;
 import com.soundcloud.android.events.EventBus;
 import com.soundcloud.android.model.ScModelManager;
+import com.soundcloud.android.playback.service.managers.FroyoRemoteAudioManager;
+import com.soundcloud.android.playback.service.managers.ICSRemoteAudioManager;
+import com.soundcloud.android.playback.service.managers.IRemoteAudioManager;
 import com.soundcloud.android.storage.StorageModule;
 import dagger.Module;
 import dagger.Provides;
@@ -13,6 +16,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
@@ -91,5 +95,19 @@ public class ApplicationModule {
     @Provides
     public SoundRecorder provideSoundRecorder() {
         return SoundRecorder.getInstance(application);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Provides
+    @Singleton
+    public IRemoteAudioManager provideRemoteAudioManager(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            try {
+                return new ICSRemoteAudioManager(context);
+            } catch (Exception e) {
+                SoundCloudApplication.handleSilentException("Could not create remote audio manager", e);
+            }
+        }
+        return new FroyoRemoteAudioManager(context);
     }
 }
--- app/src/main/java/com/soundcloud/android/playback/PlaybackModule.java
+++ app/src/main/java/com/soundcloud/android/playback/PlaybackModule.java
@@ -1,37 +1,14 @@
 package com.soundcloud.android.playback;
 
 import com.soundcloud.android.ApplicationModule;
-import com.soundcloud.android.SoundCloudApplication;
 import com.soundcloud.android.playback.service.PlaybackService;
 import com.soundcloud.android.playback.service.PlayerWidgetController;
-import com.soundcloud.android.playback.service.managers.FroyoRemoteAudioManager;
-import com.soundcloud.android.playback.service.managers.ICSRemoteAudioManager;
-import com.soundcloud.android.playback.service.managers.IRemoteAudioManager;
 import com.soundcloud.android.playback.ui.PlayerFragment;
 import dagger.Module;
-import dagger.Provides;
-
-import android.content.Context;
-import android.os.Build;
-
-import javax.inject.Singleton;
 
 @Module(addsTo = ApplicationModule.class, injects = {
         PlaybackService.class, PlayerActivity.class, PlayerFragment.class, PlayerWidgetController.class
 })
 public class PlaybackModule {
 
-    @SuppressWarnings("unchecked")
-    @Provides
-    @Singleton
-    public IRemoteAudioManager provideRemoteAudioManager(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            try {
-                return new ICSRemoteAudioManager(context);
-            } catch (Exception e) {
-                SoundCloudApplication.handleSilentException("Could not create remote audio manager", e);
-            }
-        }
-        return new FroyoRemoteAudioManager(context);
-    }
 }
