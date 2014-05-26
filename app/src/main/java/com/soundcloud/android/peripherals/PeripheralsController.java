ae00d5cb74456a502b5ffe858488485ab50c62d5
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-05-26 16:39:39 +0200

    Send track information to peripherals on play queue change and logout events
    Refs #1703

--- app/src/main/java/com/soundcloud/android/peripherals/PeripheralsController.java
+++ app/src/main/java/com/soundcloud/android/peripherals/PeripheralsController.java
@@ -2,11 +2,17 @@ package com.soundcloud.android.peripherals;
 
 import static com.soundcloud.android.playback.service.Playa.StateTransition;
 
+import com.soundcloud.android.events.CurrentUserChangedEvent;
 import com.soundcloud.android.events.EventBus;
 import com.soundcloud.android.events.EventQueue;
+import com.soundcloud.android.events.PlayQueueEvent;
 import com.soundcloud.android.model.Track;
+import com.soundcloud.android.playback.service.PlayQueueManager;
 import com.soundcloud.android.rx.observers.DefaultSubscriber;
+import com.soundcloud.android.track.TrackOperations;
 import com.soundcloud.android.utils.ScTextUtils;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 
 import android.content.Context;
 import android.content.Intent;
@@ -20,15 +26,21 @@ public class PeripheralsController {
 
     private final Context context;
     private final EventBus eventBus;
+    private final PlayQueueManager playQueueManager;
+    private final TrackOperations trackOperations;
 
     @Inject
-    public PeripheralsController(Context context, EventBus eventBus) {
+    public PeripheralsController(Context context, EventBus eventBus, PlayQueueManager playQueueManager, TrackOperations trackOperations) {
         this.context = context;
         this.eventBus = eventBus;
+        this.playQueueManager = playQueueManager;
+        this.trackOperations = trackOperations;
     }
 
     public void subscribe() {
+        eventBus.subscribe(EventQueue.CURRENT_USER_CHANGED, new CurrentUserChangedSubscriber());
         eventBus.subscribe(EventQueue.PLAYBACK_STATE_CHANGED, new PlayStateChangedSubscriber());
+        eventBus.subscribe(EventQueue.PLAY_QUEUE, new PlayQueueChangedSubscriber());
     }
 
     public void notifyMetaChanged(Context context, Track track, boolean isPlaying) {
@@ -40,14 +52,14 @@ public class PeripheralsController {
     }
 
     private void notifyChangeViaGoogleMusic(Context context, Track track, boolean isPlaying, String action) {
-        if(track != null) {
+        if (track != null) {
             Intent intent = new Intent(action);
             intent.putExtra("id", track.getId());
             intent.putExtra("track", ScTextUtils.getClippedString(track.getTitle(), 40));
             intent.putExtra("playing", isPlaying);
             intent.putExtra("duration", track.duration);
 
-            if(track.getUserName() != null) {
+            if (track.getUserName() != null) {
                 intent.putExtra("artist", ScTextUtils.getClippedString(track.getUserName(), 30));
             }
 
@@ -61,10 +73,58 @@ public class PeripheralsController {
         context.sendBroadcast(intent);
     }
 
+    private void notifyPlayQueueChanged(Track track) {
+        Intent intent = new Intent(AVRCP_META_CHANGED);
+        intent.putExtra("id", track.getId());
+        intent.putExtra("track", ScTextUtils.getClippedString(track.getTitle(), 40));
+        intent.putExtra("duration", track.duration);
+
+        if (track.getUserName() != null) {
+            intent.putExtra("artist", ScTextUtils.getClippedString(track.getUserName(), 30));
+        }
+
+        context.sendBroadcast(intent);
+    }
+
+    private void resetTrackInformation() {
+        Intent intent = new Intent(AVRCP_META_CHANGED);
+        intent.putExtra("id", "");
+        intent.putExtra("track", "");
+        intent.putExtra("duration", 0);
+        intent.putExtra("artist", "");
+
+        context.sendBroadcast(intent);
+    }
+
+    private Observable<Track> loadCurrentTrack() {
+        return trackOperations.loadTrack(playQueueManager.getCurrentTrackId(), AndroidSchedulers.mainThread());
+    }
+
+    private class CurrentUserChangedSubscriber extends DefaultSubscriber<CurrentUserChangedEvent> {
+        @Override
+        public void onNext(CurrentUserChangedEvent event) {
+            resetTrackInformation();
+        }
+    }
+
     private class PlayStateChangedSubscriber extends DefaultSubscriber<StateTransition> {
         @Override
         public void onNext(StateTransition state) {
             notifyPlayStateChanged(state.playSessionIsActive());
+        }
+    }
+
+    private class PlayQueueChangedSubscriber extends DefaultSubscriber<PlayQueueEvent> {
+        @Override
+        public void onNext(PlayQueueEvent event) {
+            loadCurrentTrack().subscribe(new CurrentTrackSubscriber());
+        }
+    }
+
+    private class CurrentTrackSubscriber extends DefaultSubscriber<Track> {
+        @Override
+        public void onNext(Track track) {
+            notifyPlayQueueChanged(track);
         }
     }
 }
--- app/src/test/java/com/soundcloud/android/peripherals/PeripheralsControllerTest.java
+++ app/src/test/java/com/soundcloud/android/peripherals/PeripheralsControllerTest.java
@@ -1,21 +1,31 @@
 package com.soundcloud.android.peripherals;
 
 import static com.soundcloud.android.Expect.expect;
+import static com.soundcloud.android.playback.service.Playa.StateTransition;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.soundcloud.android.events.CurrentUserChangedEvent;
 import com.soundcloud.android.events.EventBus;
 import com.soundcloud.android.events.EventQueue;
+import com.soundcloud.android.events.PlayQueueEvent;
 import com.soundcloud.android.model.Track;
 import com.soundcloud.android.model.User;
+import com.soundcloud.android.playback.service.PlayQueueManager;
 import com.soundcloud.android.playback.service.Playa;
 import com.soundcloud.android.robolectric.EventMonitor;
 import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.soundcloud.android.track.TrackOperations;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
+import rx.Observable;
+import rx.Scheduler;
 
 import android.content.Context;
 import android.content.Intent;
@@ -26,17 +36,27 @@ public class PeripheralsControllerTest {
     private PeripheralsController controller;
 
     @Mock
-    private EventBus eventBus;
+    private Context context;
 
     @Mock
-    private Context context;
+    private PlayQueueManager playQueueManager;
+
+    @Mock
+    private TrackOperations trackOperations;
+
+    @Mock
+    private EventBus eventBus;
 
     private EventMonitor eventMonitor;
 
+    @Captor
+    private ArgumentCaptor<Intent> captor;
+
     @Before
     public void setUp() throws Exception {
-        controller = new PeripheralsController(context, eventBus);
+        controller = new PeripheralsController(context, eventBus, playQueueManager, trackOperations);
         eventMonitor = EventMonitor.on(eventBus);
+        controller.subscribe();
     }
 
     @Test
@@ -50,16 +70,10 @@ public class PeripheralsControllerTest {
 
     @Test
     public void shouldNotifyWithCorrectIntent() throws Exception {
-        Track track = new Track(1L);
-        User user = new User("soundcloud:users:1");
-        user.username = "the artist";
-        track.setUser(user);
-        track.title = "a title";
-        track.duration = 123;
+        Track track = createTrack();
 
         controller.notifyMetaChanged(context, track, true);
-        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
-        verify(context).sendBroadcast(captor.capture());
+        verifyBroadcastSentAndCapture();
         expect(captor.getValue().getExtras().get("id")).toEqual(1L);
         expect(captor.getValue().getExtras().get("artist")).toEqual("the artist");
         expect(captor.getValue().getExtras().get("track")).toEqual("a title");
@@ -67,32 +81,86 @@ public class PeripheralsControllerTest {
         expect(captor.getValue().getExtras().get("duration")).toEqual(123);
     }
 
-    @Test
-    public void shouldSubscribeToPlayableChangeEvent() throws Exception {
-        controller.subscribe();
 
+    @Test
+    public void shouldSubscribeToPlayableChangeEvents() throws Exception {
         eventMonitor.verifySubscribedTo(EventQueue.PLAYBACK_STATE_CHANGED);
     }
 
     @Test
-    public void shouldSendBroadcastWithPlayingExtraOnReceivingPlaybackStateChange() throws Exception {
-        controller.subscribe();
+    public void shouldSendBroadcastWithPlayingExtraOnReceivingPlaybackState() throws Exception {
+        eventMonitor.publish(EventQueue.PLAYBACK_STATE_CHANGED, new StateTransition(Playa.PlayaState.PLAYING, Playa.Reason.NONE));
 
-        eventMonitor.publish(EventQueue.PLAYBACK_STATE_CHANGED, new Playa.StateTransition(Playa.PlayaState.PLAYING, Playa.Reason.NONE));
-
-        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
-        verify(context).sendBroadcast(captor.capture());
+        verifyBroadcastSentAndCapture();
         expect(captor.getValue().getExtras().get("playing")).toEqual(true);
     }
 
     @Test
+    public void shouldSendBroadcastWithPlayingExtraOnReceivingIdlePlayState() throws Exception {
+        eventMonitor.publish(EventQueue.PLAYBACK_STATE_CHANGED, new StateTransition(Playa.PlayaState.IDLE, Playa.Reason.NONE));
+
+        verifyBroadcastSentAndCapture();
+        expect(captor.getValue().getExtras().get("playing")).toEqual(false);
+    }
+
+    @Test
     public void shouldSendBroadcastWithPlayStateActionOnReceivingPlaybackStateChange() throws Exception {
-        controller.subscribe();
+        eventMonitor.publish(EventQueue.PLAYBACK_STATE_CHANGED, new StateTransition(Playa.PlayaState.PLAYING, Playa.Reason.NONE));
 
-        eventMonitor.publish(EventQueue.PLAYBACK_STATE_CHANGED, new Playa.StateTransition(Playa.PlayaState.PLAYING, Playa.Reason.NONE));
-
-        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
-        verify(context).sendBroadcast(captor.capture());
+        verifyBroadcastSentAndCapture();
         expect(captor.getValue().getAction()).toEqual("com.android.music.playstatechanged");
+    }
+
+    @Test
+    public void shouldSubscribeToThePlayQueueEvents() throws Exception {
+        eventMonitor.verifySubscribedTo(EventQueue.PLAY_QUEUE);
+    }
+
+    @Test
+    public void shouldBroadcastTrackInformationWhenThePlayQueueChanges() throws Exception {
+        final long currentTrackId = 3L;
+        when(playQueueManager.getCurrentTrackId()).thenReturn(currentTrackId);
+        final Track track = createTrack();
+        when(trackOperations.loadTrack(eq(currentTrackId), any(Scheduler.class))).thenReturn(Observable.just(track));
+
+        eventMonitor.publish(EventQueue.PLAY_QUEUE, PlayQueueEvent.fromQueueChange());
+
+        verifyBroadcastSentAndCapture();
+        expect(captor.getValue().getAction()).toEqual("com.android.music.metachanged");
+        expect(captor.getValue().getExtras().get("id")).toEqual(track.getId());
+        expect(captor.getValue().getExtras().get("artist")).toEqual(track.user.username);
+        expect(captor.getValue().getExtras().get("track")).toEqual(track.title);
+        expect(captor.getValue().getExtras().get("duration")).toEqual(track.duration);
+    }
+
+    @Test
+    public void shouldSubscribeToCurrentUserChangedEvent() throws Exception {
+        eventMonitor.verifySubscribedTo(EventQueue.CURRENT_USER_CHANGED);
+    }
+
+    @Test
+    public void shouldResetTrackInformationOnUserLogout() throws Exception {
+        eventMonitor.publish(EventQueue.CURRENT_USER_CHANGED, CurrentUserChangedEvent.forLogout());
+
+        verifyBroadcastSentAndCapture();
+        expect(captor.getValue().getAction()).toEqual("com.android.music.metachanged");
+        expect(captor.getValue().getExtras().get("id")).toEqual("");
+        expect(captor.getValue().getExtras().get("artist")).toEqual("");
+        expect(captor.getValue().getExtras().get("track")).toEqual("");
+        expect(captor.getValue().getExtras().get("duration")).toEqual(0);
+    }
+
+    private void verifyBroadcastSentAndCapture() {
+        verify(context).sendBroadcast(captor.capture());
+    }
+
+    private Track createTrack() {
+        Track track = new Track(1L);
+        User user = new User("soundcloud:users:1");
+        user.username = "the artist";
+        track.setUser(user);
+        track.title = "a title";
+        track.duration = 123;
+        return track;
     }
 }
