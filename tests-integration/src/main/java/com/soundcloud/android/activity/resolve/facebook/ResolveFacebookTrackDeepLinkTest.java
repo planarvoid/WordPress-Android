86858e97214d41200452afa74e0581615f24693f
Guillaume Lung <guillaume@soundcloud.com>
2014-06-26 11:22:28 +0200

    Fix a race condition in ResolveFacebookTrackDeepLinkTest test between track operation and logout.
    I removed logout since it is not necessary for this test.
    
    Close #1839

--- tests-integration/src/main/java/com/soundcloud/android/activity/resolve/facebook/ResolveFacebookTrackDeepLinkTest.java
+++ tests-integration/src/main/java/com/soundcloud/android/activity/resolve/facebook/ResolveFacebookTrackDeepLinkTest.java
@@ -6,19 +6,11 @@ import static org.hamcrest.Matchers.is;
 import com.soundcloud.android.TestConsts;
 import com.soundcloud.android.activity.resolve.ResolveBaseTest;
 import com.soundcloud.android.properties.Feature;
-import com.soundcloud.android.screens.LegacyPlayerScreen;
 import com.soundcloud.android.screens.elements.VisualPlayerElement;
-import com.soundcloud.android.tests.AccountAssistant;
-import com.soundcloud.android.tests.Waiter;
 
 import android.net.Uri;
 
 public class ResolveFacebookTrackDeepLinkTest extends ResolveBaseTest {
-    protected static final int DEFAULT_WAIT = 30 * 1000;
-    protected static LegacyPlayerScreen playerScreen;
-    protected static Waiter waiter;
-
-
     @Override
     protected Uri getUri() {
         return TestConsts.FACEBOOK_SOUND_DEEP_LINK;
@@ -32,11 +24,4 @@ public class ResolveFacebookTrackDeepLinkTest extends ResolveBaseTest {
             solo.assertActivity(com.soundcloud.android.playback.PlayerActivity.class, DEFAULT_WAIT);
         }
     }
-
-    @Override
-    public void tearDown() throws Exception {
-        AccountAssistant.logOut(getInstrumentation());
-        super.tearDown();
-    }
-
 }
