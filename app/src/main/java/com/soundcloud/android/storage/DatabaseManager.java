e2391f13ad1f6d41629ee624017b51d6516075c1
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-06-30 12:36:52 +0200

    Bump Database version to 25
    This will perform a migration that will clear the Activities table
    
    Closes #1808

--- app/src/main/java/com/soundcloud/android/storage/DatabaseManager.java
+++ app/src/main/java/com/soundcloud/android/storage/DatabaseManager.java
@@ -16,7 +16,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     /* package */ static final String TAG = "DatabaseManager";
 
     /* increment when schema changes */
-    public static final int DATABASE_VERSION = 24;
+    public static final int DATABASE_VERSION = 25;
     private static final String DATABASE_NAME = "SoundCloud";
 
     private static DatabaseManager instance;
