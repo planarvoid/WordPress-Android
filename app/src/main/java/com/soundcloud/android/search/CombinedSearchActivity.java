6f3b46293700e76b0227daf1cf1dc20b1fc91c97
Matthias Kaeppler <mk@soundcloud.com>
2014-07-09 11:22:56 +0200

    Remove manual back stack handling code
    This should fix #1855
    We couldn’t find any problems with the back stack, so should be fine.

--- app/src/main/java/com/soundcloud/android/search/CombinedSearchActivity.java
+++ app/src/main/java/com/soundcloud/android/search/CombinedSearchActivity.java
@@ -119,11 +119,6 @@ public class CombinedSearchActivity extends ScActivity implements PlaylistTagsFr
     private void addContent(Fragment fragment, String tag) {
         FragmentManager manager = getSupportFragmentManager();
 
-        // Clear the backstack before adding new content Fragment
-        if (manager.getBackStackEntryCount() > 0) {
-            manager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-        }
-
         manager.beginTransaction()
                 .replace(R.id.holder, fragment, tag)
                 .addToBackStack(tag)
