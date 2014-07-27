3fcee49942fd3b1b337c7b29f8976bf6a0b787ad
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-07-27 12:11:31 +0200

    Added compatibility tests for new signup resposes and refactored tests

--- app/src/test/java/com/soundcloud/android/onboarding/auth/tasks/SignupTaskTest.java
+++ app/src/test/java/com/soundcloud/android/onboarding/auth/tasks/SignupTaskTest.java
@@ -4,11 +4,9 @@ import static com.soundcloud.android.Expect.expect;
 
 import com.soundcloud.android.R;
 import com.soundcloud.android.onboarding.auth.TokenInformationGenerator;
-import com.soundcloud.android.onboarding.auth.tasks.AuthTaskResult;
-import com.soundcloud.android.onboarding.auth.tasks.SignupTask;
-import com.soundcloud.android.storage.UserStorage;
 import com.soundcloud.android.robolectric.DefaultTestRunner;
 import com.soundcloud.android.robolectric.TestHelper;
+import com.soundcloud.android.storage.UserStorage;
 import com.xtremelabs.robolectric.Robolectric;
 import org.apache.http.message.BasicHeader;
 import org.junit.Before;
@@ -38,7 +36,9 @@ public class SignupTaskTest {
     public void shouldReturnUser() throws Exception {
         TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
         Robolectric.addPendingHttpResponse(201, TestHelper.resourceAsString(getClass(), "me.json"));
+
         AuthTaskResult result = signupTask.doSignup(DefaultTestRunner.application, getParamsBundle());
+
         expect(result.getUser()).not.toBeNull();
         expect(result.getUser().username).toEqual("testing");
     }
@@ -53,44 +53,71 @@ public class SignupTaskTest {
     @Test
     public void shouldProcessErrorsDuringSignup() throws Exception {
         TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
-        Robolectric.addPendingHttpResponse(422, "{\"errors\":{\"error\":[\"Email has already been taken\",\"Email is already taken.\"]}}");
-        AuthTaskResult result = signupTask.doSignup(DefaultTestRunner.application, getParamsBundle());
-        expect(result.wasSuccess()).toBeFalse();
-        String[] errors = result.getErrors();
-        expect(Arrays.equals(errors, new String[]{"Email has already been taken", "Email is already taken."})).toBeTrue();
+        Robolectric.addPendingHttpResponse(422, "{\"errors\":{\"error\":[\"Email has already been taken\",\"Email is already taken\"]}}");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Email has already been taken", "Email is already taken"});
+    }
+
+    @Test
+    public void shouldProcessMultipleErrorsOfNewResponseBodyDuringSignup() throws Exception {
+        TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
+        Robolectric.addPendingHttpResponse(422, "{ \"error\": 101, \"email\": \"taken\", \"errors\": [{ \"error_message\":\"Email has already been taken\"},{\"error_message\":\"Address has already been taken\"}] }");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Email has already been taken", "Address has already been taken"});
+    }
+
+    @Test
+    public void shouldProcessErrorsOfNewResponseBodyForExistingEmailDuringSignup() throws Exception {
+        TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
+        Robolectric.addPendingHttpResponse(422, "{ \"error\": 101, \"email\": \"taken\", \"errors\": [{ \"error_message\": \"Email has already been taken.\" }] }");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Email has already been taken."});
+    }
+
+    @Test
+    public void shouldProcessErrorsOfNewResponseBodyForDomainBlacklistedDuringSignup() throws Exception {
+        TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
+        Robolectric.addPendingHttpResponse(422, "{ \"error\": 102, \"email\": \"domain_blacklisted\", \"errors\": [{ \"error_message\": \"Email domain is blacklisted.\" }] }");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Email domain is blacklisted."});
+    }
+
+    @Test
+    public void shouldProcessErrorsOfNewResponseBodyForCaptchaChallengeDuringSignup() throws Exception {
+        TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
+        Robolectric.addPendingHttpResponse(422, "{ \"error\": 103, \"captcha_challenge_response\": \"required\", \"errors\": [{ \"error_message\": \"Spam detected, login on web page with captcha.\" }] }");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Spam detected, login on web page with captcha."});
+    }
+
+    @Test
+    public void shouldProcessErrorsOfNewResponseBodyInvalidEmailDuringSignup() throws Exception {
+        TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
+        Robolectric.addPendingHttpResponse(422, "{ \"error\": 104, \"email\": \"is invalid\", \"errors\": [{ \"error_message\": \"Email is invalid.\" }] }");
+        signUpAndExpectErrorsInResponseBody(new String[]{"Email is invalid."});
     }
 
     @Test
     public void shouldProcessBadErrorResponse() throws Exception {
         TestHelper.addPendingHttpResponse(getClass(), "signup_token.json");
         Robolectric.addPendingHttpResponse(422, "ada");
-        AuthTaskResult result = signupTask.doSignup(DefaultTestRunner.application, getParamsBundle());
-        expect(result.wasSuccess()).toBeFalse();
-        expect(Arrays.equals(result.getErrors(), new String[]{})).toBeTrue();
+        signUpAndExpectErrorsInResponseBody(new String[]{});
     }
 
     @Test
     public void shouldHandleRevokedSignupScope() throws Exception {
         TestHelper.addPendingHttpResponse(getClass(), "signup_token_blank_scope.json");
-        AuthTaskResult result = signupTask.doSignup(DefaultTestRunner.application, getParamsBundle());
-        expect(result.wasSuccess()).toBeFalse();
-        String[] signupScopeError = {DefaultTestRunner.application.getString(R.string.signup_scope_revoked)};
-        String[] errors = result.getErrors();
-        expect(Arrays.equals(errors, signupScopeError)).toBeTrue();
+        signUpAndExpectErrorsInResponseBody(new String[]{DefaultTestRunner.application.getString(R.string.signup_scope_revoked)});
     }
 
     @Test
     public void shouldHandleErrorCodeFromTokenController() throws Exception {
         // we get a token which has signup scope, but still returns forbidden on token request
-
         TestHelper.addPendingHttpResponse(getClass(), ("signup_token.json"));
         Robolectric.addPendingHttpResponse(403, "",
             new BasicHeader("WWW-Authenticate", "OAuth realm=\"SoundCloud\", error=\"insufficient_scope\""));
+        signUpAndExpectErrorsInResponseBody(new String[]{DefaultTestRunner.application.getString(R.string.signup_scope_revoked)});
+    }
 
-        SignupTask task = signupTask;
-        AuthTaskResult result = task.doSignup(DefaultTestRunner.application, getParamsBundle());
+    private void signUpAndExpectErrorsInResponseBody(String[] expectedErrors) {
+        AuthTaskResult result = signupTask.doSignup(DefaultTestRunner.application, getParamsBundle());
         expect(result.wasSuccess()).toBeFalse();
-        String[] signupScopeError = {DefaultTestRunner.application.getString(R.string.signup_scope_revoked)};
-        expect(Arrays.equals(result.getErrors(), signupScopeError)).toBeTrue();
+        String[] errors = result.getErrors();
+        expect(Arrays.equals(errors, expectedErrors)).toBeTrue();
     }
 }
