9e312ced3359b6b835ba2be7eac289cb3067b4e5
jonschmidt <jon@soundcloud.com>
2014-07-28 17:40:37 +0200

    made skippy only lay 5-1 for caching reasons

--- app/src/main/java/com/soundcloud/android/playback/service/PlaybackServiceModule.java
+++ app/src/main/java/com/soundcloud/android/playback/service/PlaybackServiceModule.java
@@ -15,7 +15,7 @@ public class PlaybackServiceModule {
         if (applicationProperties.isReleaseBuild()){
             return new StreamPlaya.PlayerSwitcherInfo(9, 1);
         } else {
-            return new StreamPlaya.PlayerSwitcherInfo(3, 1);
+            return new StreamPlaya.PlayerSwitcherInfo(5, 1);
         }
     }
 
