b3d9607b40a045823380b286aa549cbe1e446cb2
Slawomir Smiechura <slawomir@soundcloud.com>
2014-08-04 22:22:46 +0200

    Fixed resolve playlist tests

--- tests-integration/src/main/java/com/soundcloud/android/activity/resolve/ResolveSetNormalUrlTest.java
+++ tests-integration/src/main/java/com/soundcloud/android/activity/resolve/ResolveSetNormalUrlTest.java
@@ -1,17 +1,22 @@
 package com.soundcloud.android.activity.resolve;
 
+import static com.soundcloud.android.tests.matcher.view.IsVisible.Visible;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.is;
 
 import com.soundcloud.android.TestConsts;
+import com.soundcloud.android.screens.PlaylistDetailsScreen;
 
 import android.net.Uri;
 
 public class ResolveSetNormalUrlTest extends ResolveBaseTest {
 
     public void testShouldOpenPlaylistDetails() throws Exception {
-        assertThat(getPlayerElement().getTrackTitle(), is(equalToIgnoringCase("Duruflé  Introit and Kyrie")));
+        PlaylistDetailsScreen playlistDetailsScreen = new PlaylistDetailsScreen(solo);
+
+        assertThat(playlistDetailsScreen, is(Visible()));
+        assertThat(playlistDetailsScreen.getTitle(), is(equalToIgnoringCase("Ecclesia Inspiration")));
     }
 
     @Override
