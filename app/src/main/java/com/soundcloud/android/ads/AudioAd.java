aa9b2ccbcb53d262762f3a832d510af197ad4020
Dave Cameron <dave.bc@soundcloud.com>
2014-08-08 16:26:17 +0200

    make AudioAd model immutable (use @JsonCreator instead of setters), add ad urn property
    Had to introduce 2 constructors to satisfy both Jackson and ModelCitizen. I think
    there are alternative approaches in Jackson, although not sure how far we want
    to go in that direction. The RelatedResource class, containing the VisualAd
    is the sticking point. And so far, VisualAd is not used. confusing.
    
    ref #2011

--- app/src/main/java/com/soundcloud/android/ads/AudioAd.java
+++ app/src/main/java/com/soundcloud/android/ads/AudioAd.java
@@ -1,6 +1,8 @@
 package com.soundcloud.android.ads;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.annotations.VisibleForTesting;
 import com.soundcloud.android.api.model.ApiTrack;
 import com.soundcloud.android.model.PropertySetSource;
 import com.soundcloud.propeller.PropertySet;
@@ -9,14 +11,35 @@ import android.net.Uri;
 
 public class AudioAd implements PropertySetSource {
 
-    private ApiTrack apiTrack;
+    private final ApiTrack apiTrack;
 
-    private VisualAd visualAd;
+    private final VisualAd visualAd;
 
-    private String trackingImpressionUrl;
-    private String trackingPlayUrl;
-    private String trackingFinishUrl;
-    private String trackingSkipUrl;
+    private final String trackingImpressionUrl;
+    private final String trackingPlayUrl;
+    private final String trackingFinishUrl;
+    private final String trackingSkipUrl;
+
+    @JsonCreator
+    public AudioAd(@JsonProperty("track") ApiTrack apiTrack,
+                   @JsonProperty("_embedded") RelatedResources relatedResources,
+                   @JsonProperty("tracking_impression_url") String trackingImpressionUrl,
+                   @JsonProperty("tracking_play_url") String trackingPlayUrl,
+                   @JsonProperty("tracking_finish_url") String trackingFinishUrl,
+                   @JsonProperty("tracking_skip_url") String trackingSkipUrl) {
+        this(apiTrack, relatedResources.visualAd, trackingImpressionUrl, trackingPlayUrl, trackingFinishUrl, trackingSkipUrl);
+    }
+
+    @VisibleForTesting
+    public AudioAd(ApiTrack apiTrack, VisualAd visualAd, String trackingImpressionUrl,
+                   String trackingPlayUrl, String trackingFinishUrl, String trackingSkipUrl) {
+        this.apiTrack = apiTrack;
+        this.visualAd = visualAd;
+        this.trackingImpressionUrl = trackingImpressionUrl;
+        this.trackingPlayUrl = trackingPlayUrl;
+        this.trackingFinishUrl = trackingFinishUrl;
+        this.trackingSkipUrl = trackingSkipUrl;
+    }
 
     public ApiTrack getApiTrack() {
         return apiTrack;
@@ -24,35 +47,6 @@ public class AudioAd implements PropertySetSource {
 
     public VisualAd getVisualAd() {
         return visualAd;
-    }
-
-    public void setVisualAd(VisualAd visualAd) {
-        this.visualAd = visualAd;
-    }
-
-    @JsonProperty("track")
-    public void setApiTrack(ApiTrack apiTrack) {
-        this.apiTrack = apiTrack;
-    }
-
-    @JsonProperty("tracking_impression_url")
-    public void setTrackingImpressionUrl(String trackingImpressionUrl) {
-        this.trackingImpressionUrl = trackingImpressionUrl;
-    }
-
-    @JsonProperty("tracking_play_url")
-    public void setTrackingPlayUrl(String trackingPlayUrl) {
-        this.trackingPlayUrl = trackingPlayUrl;
-    }
-
-    @JsonProperty("tracking_finish_url")
-    public void setTrackingFinishUrl(String trackingFinishUrl) {
-        this.trackingFinishUrl = trackingFinishUrl;
-    }
-
-    @JsonProperty("tracking_skip_url")
-    public void setTrackingSkipUrl(String trackingSkipUrl) {
-        this.trackingSkipUrl = trackingSkipUrl;
     }
 
     public String getTrackingImpressionUrl() {
@@ -71,16 +65,12 @@ public class AudioAd implements PropertySetSource {
         return trackingSkipUrl;
     }
 
-    @JsonProperty("_embedded")
-    public void setRelatedResources(RelatedResources relatedResources) {
-        this.visualAd = relatedResources.visualAd;
-    }
-
     private static class RelatedResources {
 
-        private VisualAd visualAd;
-        @JsonProperty("visual_ad")
-        void setVisualAd(VisualAd visualAd) {
+        private final VisualAd visualAd;
+
+        @JsonCreator
+        private RelatedResources(@JsonProperty("visual_ad") VisualAd visualAd) {
             this.visualAd = visualAd;
         }
 
--- app/src/test/java/com/soundcloud/android/blueprints/AudioAdBlueprint.java
+++ app/src/test/java/com/soundcloud/android/blueprints/AudioAdBlueprint.java
@@ -3,15 +3,35 @@ package com.soundcloud.android.blueprints;
 import com.soundcloud.android.ads.VisualAd;
 import com.soundcloud.android.api.model.ApiTrack;
 import com.soundcloud.android.ads.AudioAd;
+import com.soundcloud.android.robolectric.TestHelper;
+import com.tobedevoured.modelcitizen.CreateModelException;
+import com.tobedevoured.modelcitizen.ModelFactory;
 import com.tobedevoured.modelcitizen.annotation.Blueprint;
 import com.tobedevoured.modelcitizen.annotation.Mapped;
+import com.tobedevoured.modelcitizen.annotation.NewInstance;
+import com.tobedevoured.modelcitizen.field.ConstructorCallback;
 
 @Blueprint(AudioAd.class)
 public class AudioAdBlueprint {
 
-    @Mapped
-    ApiTrack apiTrack;
+    @NewInstance
+    ConstructorCallback constructor = new ConstructorCallback() {
+        @Override
+        public Object createInstance() {
+            ModelFactory factory = TestHelper.getModelFactory();
+            try {
+                return new AudioAd(
+                        factory.createModel(ApiTrack.class),
+                        factory.createModel(VisualAd.class),
+                        "",
+                        "",
+                        "",
+                        ""
+                );
+            } catch (CreateModelException e) {
+                return null;
+            }
+        }
+    };
 
-    @Mapped
-    VisualAd visualAd;
 }
