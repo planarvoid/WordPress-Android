fa0c89a5aedf7ca8abfb4aa68fb8859e37764ced
Slawomir Smiechura <slawomir@soundcloud.com>
2014-08-11 16:27:52 +0200

    Fix 'local' prefix.
    Even though we change the `versionName` we never touch the artefact name, build system should still point to the right apk.

--- buildsystem/build.rb
+++ buildsystem/build.rb
@@ -36,7 +36,7 @@ module Build
     if env == 'release'
       [version, build_number].compact.join("-")
     else
-      [version, build_number, env].compact.join("-")
+      [version, build_number, (ci? ? env : 'local')].compact.join("-")
     end
   end
 
@@ -85,7 +85,7 @@ module Build
   end
 
   def env
-    ci? ? Build::Configuration.build_env : 'local'
+    Build::Configuration.build_env
   end
 
 end
