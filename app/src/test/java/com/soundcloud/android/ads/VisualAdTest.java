cd072f74c51d6465d41498ca57873723ab329db9
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-08-11 14:19:14 +0200

    Assert that display properties are not null in visual ad test

--- app/src/test/java/com/soundcloud/android/ads/VisualAdTest.java
+++ app/src/test/java/com/soundcloud/android/ads/VisualAdTest.java
@@ -21,6 +21,6 @@ public class VisualAdTest {
         expect(visualAd.getTrackingClickUrls()).toEqual(Lists.newArrayList("https://promoted.soundcloud.com/track?reqType=SCAdClicked&protocolVersion=2.0&adId=263&zoneId=19&cb=7a76b73c420c4cb8b171f083b1f59a05"));
         expect(visualAd.getTrackingImpressionUrls()).toEqual(Lists.newArrayList("https://promoted.soundcloud.com/impression?adData=instance%3Asoundcloud%3Bad_id%3A263%3Bview_key%3A1401441272790582%3Bzone_id%3A19&loc=&listenerId=38cfbf01fa3450d214e1cc37548d18&sessionId=3e89b0ad42cff51ed4de13fd8ec97497&ip=%3A%3Affff%3A62.72.64.50&user_agent=http.async.client%2F0.5.2&cbs=5346259"));
 
-        expect(visualAd.getDisplayProperties().getDefaultBackground()).not.toBeNull();
+        expect(visualAd.getDisplayProperties()).not.toBeNull();
     }
 }
