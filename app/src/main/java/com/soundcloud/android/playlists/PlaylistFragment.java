86d74c608646c21415766d25533d0f5cbb4c2ce9
Karolina Rusin <kr@soundcloud.com>
2014-08-12 17:53:22 +0200

    Removed unused import

--- app/src/main/java/com/soundcloud/android/playlists/PlaylistFragment.java
+++ app/src/main/java/com/soundcloud/android/playlists/PlaylistFragment.java
@@ -39,7 +39,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
