9fb79930d9290b80a5bf031475ff99515fdc06a8
Matthias Kaeppler <mk@soundcloud.com>
2014-08-12 11:56:02 +0200

    Send OAuth token with play count requests when logged in

--- app/src/main/java/com/soundcloud/android/analytics/playcounts/PlayCountUrlBuilder.java
+++ app/src/main/java/com/soundcloud/android/analytics/playcounts/PlayCountUrlBuilder.java
@@ -1,8 +1,10 @@
 package com.soundcloud.android.analytics.playcounts;
 
+import com.soundcloud.android.accounts.AccountOperations;
 import com.soundcloud.android.api.APIEndpoints;
 import com.soundcloud.android.api.HttpProperties;
 import com.soundcloud.android.events.PlaybackSessionEvent;
+import com.soundcloud.api.Token;
 
 import android.net.Uri;
 
@@ -13,10 +15,12 @@ class PlayCountUrlBuilder {
     private static final String PUBLIC_API_BASE_URI = "https://api.soundcloud.com";
 
     private final HttpProperties httpProperties;
+    private final AccountOperations accountOperations;
 
     @Inject
-    PlayCountUrlBuilder(HttpProperties httpProperties) {
+    PlayCountUrlBuilder(HttpProperties httpProperties, AccountOperations accountOperations) {
         this.httpProperties = httpProperties;
+        this.accountOperations = accountOperations;
     }
 
     String buildUrl(PlaybackSessionEvent playbackSessionEvent) {
@@ -24,6 +28,11 @@ class PlayCountUrlBuilder {
         final Uri.Builder builder = Uri.parse(PUBLIC_API_BASE_URI + APIEndpoints.LOG_PLAY.unencodedPath(trackId))
                 .buildUpon()
                 .appendQueryParameter("client_id", httpProperties.getClientId());
+
+        final Token token = accountOperations.getSoundCloudToken();
+        if (token != null) {
+            builder.appendQueryParameter("oauth_token", token.access);
+        }
 
         final String policy = playbackSessionEvent.getTrackPolicy();
         if (policy != null) {
