8f3d6defc3e0b44ede117f32da9c48b1be303f4a
jonschmidt <jon@soundcloud.com>
2014-08-26 19:37:53 +0200

    Delay player expansion for 100 ms

--- app/src/main/java/com/soundcloud/android/view/adapters/PlayQueueChangedSubscriber.java
+++ app/src/main/java/com/soundcloud/android/view/adapters/PlayQueueChangedSubscriber.java
@@ -5,11 +5,20 @@ import com.soundcloud.android.events.PlayerUIEvent;
 import com.soundcloud.android.rx.eventbus.EventBus;
 import com.soundcloud.android.rx.observers.DefaultSubscriber;
 import com.soundcloud.android.tracks.TrackUrn;
+import rx.Observable;
+import rx.functions.Func1;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PlayQueueChangedSubscriber extends DefaultSubscriber<List<TrackUrn>> {
     private final EventBus eventBus;
+    private final Func1<Long, PlayerUIEvent> toPlayerExpandEvent = new Func1<Long, PlayerUIEvent>() {
+        @Override
+        public PlayerUIEvent call(Long aLong) {
+            return PlayerUIEvent.forExpandPlayer();
+        }
+    };
 
     public PlayQueueChangedSubscriber(EventBus eventBus) {
         this.eventBus = eventBus;
@@ -17,6 +26,8 @@ public class PlayQueueChangedSubscriber extends DefaultSubscriber<List<TrackUrn>
 
     @Override
     public void onCompleted() {
-        eventBus.publish(EventQueue.PLAYER_UI, PlayerUIEvent.forExpandPlayer());
+        Observable.timer(100, TimeUnit.MILLISECONDS)
+                .map(toPlayerExpandEvent)
+                .subscribe(eventBus.queue(EventQueue.PLAYER_UI));
     }
 }
