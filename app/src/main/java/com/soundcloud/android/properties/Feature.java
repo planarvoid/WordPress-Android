bbf1b4bae58c61c47fee6912959263c1ef230ab8
Jamie McDonald <jdamcd@gmail.com>
2014-09-10 14:36:53 +0200

    Add feature flag for audio ad leave behind
    Refs #2191

--- app/src/main/java/com/soundcloud/android/properties/Feature.java
+++ app/src/main/java/com/soundcloud/android/properties/Feature.java
@@ -1,8 +1,11 @@
 package com.soundcloud.android.properties;
 
+import com.soundcloud.android.R;
+
 public enum Feature {
 
-    TEST_FEATURE(-1);
+    TEST_FEATURE(-1),
+    LEAVE_BEHIND(R.bool.feature_leave_behind);
 
     private final int resourceId;
 
