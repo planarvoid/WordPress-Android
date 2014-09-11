3b473e20badc3f344987a61ee3d7a409d6392b8c
Jamie McDonald <jdamcd@gmail.com>
2014-09-11 12:01:20 +0200

    Add tests for the leave behind controller

---
+++ app/src/test/java/com/soundcloud/android/playback/ui/LeaveBehindControllerTest.java
@@ -0,0 +1,102 @@
+package com.soundcloud.android.playback.ui;
+
+import static com.soundcloud.android.Expect.expect;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.soundcloud.android.R;
+import com.soundcloud.android.ads.LeaveBehind;
+import com.soundcloud.android.image.ImageListener;
+import com.soundcloud.android.image.ImageOperations;
+import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.xtremelabs.robolectric.Robolectric;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+@RunWith(SoundCloudTestRunner.class)
+public class LeaveBehindControllerTest {
+
+    private LeaveBehindController controller;
+
+    private View trackView;
+    private Resources resources = Robolectric.application.getResources();
+    private LeaveBehind data = new LeaveBehind("http://image.url/image.png", "http://link.url");
+
+    @Mock private ImageOperations imageOperations;
+
+    @Captor private ArgumentCaptor<ImageListener> imageListenerCaptor;
+
+    @Before
+    public void setUp() throws Exception {
+        trackView = LayoutInflater.from(Robolectric.application).inflate(R.layout.player_track_page, mock(ViewGroup.class));
+        LeaveBehindController.Factory factory = new LeaveBehindController.Factory(imageOperations, resources);
+        controller = factory.create(trackView);
+    }
+
+    @Test
+    public void dismissSetsLeaveBehindVisibilityToGone() {
+        controller.setup(data);
+        captureImageListener().onLoadingComplete(null, null, null);
+
+        controller.dismiss();
+
+        View leaveBehind = trackView.findViewById(R.id.leave_behind);
+        expect(leaveBehind).toBeGone();
+    }
+
+    @Test
+    public void leaveBehindGoneOnLeaveBehindCloseClick() {
+        controller.setup(data);
+        View close = trackView.findViewById(R.id.leave_behind_close);
+        captureImageListener().onLoadingComplete(null, null, null);
+
+        close.performClick();
+
+        View leaveBehind = trackView.findViewById(R.id.leave_behind);
+        expect(leaveBehind).toBeGone();
+    }
+
+    @Test
+    public void leaveBehindIsVisibleAfterSetupWithSuccessfulImageLoad() {
+        controller.setup(data);
+
+        captureImageListener().onLoadingComplete(null, null, null);
+
+        View leaveBehind = trackView.findViewById(R.id.leave_behind);
+        expect(leaveBehind).toBeVisible();
+    }
+
+    @Test
+    public void leaveBehindIsGoneAfterSetupIfImageNotLoaded() {
+        controller.setup(data);
+        View leaveBehind = trackView.findViewById(R.id.leave_behind);
+        expect(leaveBehind).toBeGone();
+    }
+
+    @Test
+    public void loadsLeaveBehindImageFromModel() {
+        controller.setup(data);
+        verify(imageOperations).displayLeaveBehind(eq(Uri.parse(data.getImageUrl())), any(ImageView.class), any(ImageListener.class), any(Drawable.class));
+    }
+
+    private ImageListener captureImageListener() {
+        verify(imageOperations).displayLeaveBehind(any(Uri.class), any(ImageView.class), imageListenerCaptor.capture(), any(Drawable.class));
+        return imageListenerCaptor.getValue();
+    }
+
+
+}
