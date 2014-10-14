be79ece384dd2638e53277508a90806719aae0d0
jonschmidt <jon@soundcloud.com>
2014-10-14 14:52:52 -0400

    Fix LeaveBehind property owner

--- app/src/main/java/com/soundcloud/android/ads/LeaveBehindProperty.java
+++ app/src/main/java/com/soundcloud/android/ads/LeaveBehindProperty.java
@@ -7,12 +7,12 @@ import android.net.Uri;
 import java.util.List;
 
 public class LeaveBehindProperty {
-    public static final Property<String> LEAVE_BEHIND_URN = Property.of(ApiLeaveBehind.class, String.class);
-    public static final Property<String> IMAGE_URL = Property.of(ApiLeaveBehind.class, String.class);
-    public static final Property<Uri> CLICK_THROUGH_URL = Property.of(ApiLeaveBehind.class, Uri.class);
-    public static final Property<List<String>> TRACKING_IMPRESSION_URLS = Property.of(ApiLeaveBehind.class, (Class<List<String>>)(Class<?>) List.class);
-    public static final Property<List<String>> TRACKING_CLICK_URLS = Property.of(ApiLeaveBehind.class, (Class<List<String>>)(Class<?>) List.class);
-    public static final Property<Boolean> META_AD_COMPLETED = Property.of(ApiLeaveBehind.class, Boolean.class);
-    public static final Property<Boolean> META_AD_CLICKED = Property.of(ApiLeaveBehind.class, Boolean.class);
-    public static final Property<Boolean> IS_INTERSTITIAL = Property.of(ApiLeaveBehind.class, Boolean.class);
+    public static final Property<String> LEAVE_BEHIND_URN = Property.of(LeaveBehindProperty.class, String.class);
+    public static final Property<String> IMAGE_URL = Property.of(LeaveBehindProperty.class, String.class);
+    public static final Property<Uri> CLICK_THROUGH_URL = Property.of(LeaveBehindProperty.class, Uri.class);
+    public static final Property<List<String>> TRACKING_IMPRESSION_URLS = Property.of(LeaveBehindProperty.class, (Class<List<String>>)(Class<?>) List.class);
+    public static final Property<List<String>> TRACKING_CLICK_URLS = Property.of(LeaveBehindProperty.class, (Class<List<String>>)(Class<?>) List.class);
+    public static final Property<Boolean> META_AD_COMPLETED = Property.of(LeaveBehindProperty.class, Boolean.class);
+    public static final Property<Boolean> META_AD_CLICKED = Property.of(LeaveBehindProperty.class, Boolean.class);
+    public static final Property<Boolean> IS_INTERSTITIAL = Property.of(LeaveBehindProperty.class, Boolean.class);
 }
