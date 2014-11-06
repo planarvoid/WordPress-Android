60a61be5caa32f781ff2354bda31f4d980f16327
Pedro Lopes <lopes.m.pedro@gmail.com>
2014-11-06 12:17:24 +0100

    Remove the max cache size percentage user option
    and hardcode the skippy cache value to 300MB / 90%
    
    Refs #2415

--- app/src/main/java/com/soundcloud/android/playback/service/skippy/SkippyFactory.java
+++ app/src/main/java/com/soundcloud/android/playback/service/skippy/SkippyFactory.java
@@ -1,15 +1,11 @@
 package com.soundcloud.android.playback.service.skippy;
 
 import com.soundcloud.android.Consts;
-import com.soundcloud.android.R;
 import com.soundcloud.android.crypto.CryptoOperations;
 import com.soundcloud.android.properties.ApplicationProperties;
 import com.soundcloud.android.properties.Feature;
 import com.soundcloud.android.properties.FeatureFlags;
 import com.soundcloud.android.skippy.Skippy;
-
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 
 import javax.inject.Inject;
 
@@ -21,26 +17,22 @@ public class SkippyFactory {
     private static final int NO_CACHE_SIZE_PERCENTAGE = -1;
     private static final boolean ONE_TRACK_CACHE = true;
     private static final boolean ALL_TRACKS_CACHE = false;
-    private static final int MAX_CACHE_SIZE_BYTES = 1000000000;
-    private static final int DEFAULT_MAX_CACHE_SIZE_PERCENTAGE = 10;
+    private static final int MAX_CACHE_SIZE_BYTES = 300 * 1024 * 1024;
+    private static final int MAX_CACHE_SIZE_PERCENTAGE = 90;
     private static final String NO_CACHE = null;
     private static final byte[] NO_CACHE_ENCRYPTION_KEY = null;
 
 
     private final ApplicationProperties applicationProperties;
     private final FeatureFlags featureFlags;
-    private final SharedPreferences sharedPreferences;
     private final CryptoOperations cryptoOperations;
-    private final String maxSizePercentageKey;
 
     @Inject
-    SkippyFactory(CryptoOperations cryptoOperations, SharedPreferences sharedPreferences, ApplicationProperties applicationProperties,
-                  Resources resources, FeatureFlags featureFlags) {
+    SkippyFactory(CryptoOperations cryptoOperations, ApplicationProperties applicationProperties,
+                  FeatureFlags featureFlags) {
         this.cryptoOperations = cryptoOperations;
-        this.sharedPreferences = sharedPreferences;
         this.applicationProperties = applicationProperties;
         this.featureFlags = featureFlags;
-        maxSizePercentageKey = resources.getString(R.string.key_stream_cache_size);
     }
 
 
@@ -76,7 +68,7 @@ public class SkippyFactory {
         return new Skippy.Configuration(
                 PROGRESS_INTERVAL_MS,
                 MAX_CACHE_SIZE_BYTES,
-                sharedPreferences.getInt(maxSizePercentageKey, DEFAULT_MAX_CACHE_SIZE_PERCENTAGE),
+                MAX_CACHE_SIZE_PERCENTAGE,
                 Consts.EXTERNAL_SKIPPY_STREAM_DIRECTORY.getAbsolutePath(),
                 cryptoOperations.getKeyOrGenerateAndStore(KEY_PREFERENCE_NAME),
                 applicationProperties.useVerboseLogging(),
