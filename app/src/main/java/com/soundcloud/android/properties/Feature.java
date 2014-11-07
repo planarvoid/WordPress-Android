1f3b7f0fe6d8e096a3216d6a33bacff24113e402
Guillaume Lung <guillaume@soundcloud.com>
2014-11-07 14:41:09 +0100

    Add TRACK_ITEM_OVERFLOW feature flag. Refs #2435

--- app/src/main/java/com/soundcloud/android/properties/Feature.java
+++ app/src/main/java/com/soundcloud/android/properties/Feature.java
@@ -13,7 +13,8 @@ public enum Feature {
     INTERSTITIAL(R.bool.feature_interstitial),
     ADJUST_TRACKING(R.bool.feature_adjust),
     SECURE_STREAM_CACHE(R.bool.feature_secure_stream_cache),
-    API_MOBILE_STREAM(R.bool.feature_api_mobile_stream);
+    API_MOBILE_STREAM(R.bool.feature_api_mobile_stream),
+    TRACK_ITEM_OVERFLOW(R.bool.feature_track_item_overflow);
 
     private final int resourceId;
 
