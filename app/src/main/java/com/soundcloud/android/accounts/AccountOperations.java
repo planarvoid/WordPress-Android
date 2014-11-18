a1eafc7c6b9f8a49cb3e84b2575ec3afc445ad67
Matthias Kaeppler <mk@soundcloud.com>
2014-11-18 13:59:48 +0100

    Deal with null users coming back from user storage
    We can fix this more elegantly once we move our legacy user storage to Propeller, because it has a proper concept of an empty result set instead of just returning null.
    
    Fixes #2486

--- app/src/main/java/com/soundcloud/android/accounts/AccountOperations.java
+++ app/src/main/java/com/soundcloud/android/accounts/AccountOperations.java
@@ -154,9 +154,14 @@ public class AccountOperations extends ScheduledOperations {
         }
     }
 
-    private void updateLoggedInUser(final PublicApiUser user) {
-        loggedInUser = modelManager.cache(user, PublicApiResource.CacheUpdateMode.FULL);
-        loggedInUserUrn = user.getUrn();
+    // adding a null check here because of https://github.com/soundcloud/SoundCloud-Android/issues/2486
+    // This happens when you don't log out, but clear data; it should correct itself by going
+    // to AccountManager or constructing the Urn next time we try to get the logged in user.
+    private void updateLoggedInUser(@Nullable PublicApiUser user) {
+        if (user != null) {
+            loggedInUser = modelManager.cache(user, PublicApiResource.CacheUpdateMode.FULL);
+            loggedInUserUrn = user.getUrn();
+        }
     }
 
     public void clearLoggedInUser() {
