c87eda469b9875aeab6c5ef29ca6841f4c850616
Dave Cameron <dave.bc@soundcloud.com>
2014-12-12 17:21:56 -0500

    Working around null search results and silent logging them

--- app/src/main/java/com/soundcloud/android/search/SearchResult.java
+++ app/src/main/java/com/soundcloud/android/search/SearchResult.java
@@ -3,6 +3,7 @@ package com.soundcloud.android.search;
 import com.google.common.base.Optional;
 import com.soundcloud.android.api.model.Link;
 import com.soundcloud.android.model.PropertySetSource;
+import com.soundcloud.android.utils.ErrorUtils;
 import com.soundcloud.propeller.PropertySet;
 
 import java.util.ArrayList;
@@ -14,11 +15,28 @@ class SearchResult implements Iterable<PropertySet> {
     final Optional<Link> nextHref;
 
     SearchResult(List<? extends PropertySetSource> items, Optional<Link> nextHref) {
+        int emptyItems = 0;
         this.items = new ArrayList<>(items.size());
         for (PropertySetSource source : items) {
-            this.items.add(source.toPropertySet());
+            if (source == null) {
+                emptyItems++;
+            } else {
+                this.items.add(source.toPropertySet());
+            }
         }
         this.nextHref = nextHref;
+        if (emptyItems > 0) {
+            ErrorUtils.handleSilentException(getMissingItemException(items, nextHref, emptyItems));
+        }
+    }
+
+    private IllegalStateException getMissingItemException(List<? extends PropertySetSource> items, Optional<Link> nextHref, int emptyItems) {
+        return new IllegalStateException(
+                String.format(
+                        "%d/%d empty items in search result with nextHref %s",
+                        emptyItems,
+                        items.size(),
+                        (nextHref != null && nextHref.isPresent()) ? nextHref.get() : "none"));
     }
 
     @Override
