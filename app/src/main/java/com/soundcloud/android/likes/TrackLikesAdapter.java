b12396593c6a1e31e41c6bc14e7c5a33c4d73744
Fernando Cejas <fcejas@gmail.com>
2014-12-19 11:55:25 +0100

    Do not use EndlessAdapter on the TrackLikesAdapter
    Refs #2634

--- app/src/main/java/com/soundcloud/android/likes/TrackLikesAdapter.java
+++ app/src/main/java/com/soundcloud/android/likes/TrackLikesAdapter.java
@@ -1,16 +1,37 @@
 package com.soundcloud.android.likes;
 
 import com.soundcloud.android.tracks.TrackItemPresenter;
-import com.soundcloud.android.view.adapters.EndlessAdapter;
+import com.soundcloud.android.utils.CallsiteToken;
+import com.soundcloud.android.utils.ErrorUtils;
+import com.soundcloud.android.view.adapters.ItemAdapter;
+import com.soundcloud.android.view.adapters.ReactiveAdapter;
 import com.soundcloud.propeller.PropertySet;
 
 import javax.inject.Inject;
 
-public class TrackLikesAdapter extends EndlessAdapter<PropertySet> {
+public class TrackLikesAdapter extends ItemAdapter<PropertySet> implements ReactiveAdapter<Iterable<PropertySet>> {
+
+    private final CallsiteToken callsiteToken = CallsiteToken.build();
 
     @Inject
     public TrackLikesAdapter(TrackItemPresenter trackPresenter) {
         super(trackPresenter);
     }
 
+    @Override
+    public void onNext(Iterable<PropertySet> propertySets) {
+        for (PropertySet propertySet : propertySets) {
+            addItem(propertySet);
+        }
+    }
+
+    @Override
+    public void onCompleted() {
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        ErrorUtils.handleThrowable(e, callsiteToken);
+    }
 }
