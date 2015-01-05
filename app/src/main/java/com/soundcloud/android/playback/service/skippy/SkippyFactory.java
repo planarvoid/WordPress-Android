9495a89016c7cae636f703c80126d84a3b85e4be
Dave Cameron <dave.bc@soundcloud.com>
2015-01-05 13:05:00 -0500

    remove the Secure Stream Cache flag. now always secure.
    refs #2580

--- app/src/main/java/com/soundcloud/android/playback/service/skippy/SkippyFactory.java
+++ app/src/main/java/com/soundcloud/android/playback/service/skippy/SkippyFactory.java
@@ -3,8 +3,6 @@ package com.soundcloud.android.playback.service.skippy;
 import com.soundcloud.android.Consts;
 import com.soundcloud.android.crypto.CryptoOperations;
 import com.soundcloud.android.properties.ApplicationProperties;
-import com.soundcloud.android.properties.Feature;
-import com.soundcloud.android.properties.FeatureFlags;
 import com.soundcloud.android.skippy.Skippy;
 
 import javax.inject.Inject;
@@ -13,26 +11,17 @@ public class SkippyFactory {
 
     private static final String KEY_PREFERENCE_NAME = "skippy_cache";
     private static final int PROGRESS_INTERVAL_MS = 500;
-    private static final int NO_CACHE_SIZE = -1;
-    private static final int NO_CACHE_SIZE_PERCENTAGE = -1;
-    private static final boolean ONE_TRACK_CACHE = true;
     private static final boolean ALL_TRACKS_CACHE = false;
     private static final int MAX_CACHE_SIZE_BYTES = 300 * 1024 * 1024;
     private static final int CACHE_MIN_FREE_SPACE_AVAILABLE_PERCENTAGE = 10;
-    private static final String NO_CACHE = null;
-    private static final byte[] NO_CACHE_ENCRYPTION_KEY = null;
-
 
     private final ApplicationProperties applicationProperties;
-    private final FeatureFlags featureFlags;
     private final CryptoOperations cryptoOperations;
 
     @Inject
-    SkippyFactory(CryptoOperations cryptoOperations, ApplicationProperties applicationProperties,
-                  FeatureFlags featureFlags) {
+    SkippyFactory(CryptoOperations cryptoOperations, ApplicationProperties applicationProperties) {
         this.cryptoOperations = cryptoOperations;
         this.applicationProperties = applicationProperties;
-        this.featureFlags = featureFlags;
     }
 
 
@@ -45,26 +34,6 @@ public class SkippyFactory {
     }
 
     public Skippy.Configuration createConfiguration() {
-        if (featureFlags.isEnabled(Feature.SECURE_STREAM_CACHE)) {
-            return createConfigurationForSecureCache();
-        } else {
-            return createConfigurationForSingleTrackCache();
-        }
-    }
-
-    private Skippy.Configuration createConfigurationForSingleTrackCache() {
-        return new Skippy.Configuration(
-                PROGRESS_INTERVAL_MS,
-                NO_CACHE_SIZE,
-                NO_CACHE_SIZE_PERCENTAGE,
-                NO_CACHE,
-                NO_CACHE_ENCRYPTION_KEY,
-                applicationProperties.useVerboseLogging(),
-                ONE_TRACK_CACHE
-        );
-    }
-
-    private Skippy.Configuration createConfigurationForSecureCache() {
         return new Skippy.Configuration(
                 PROGRESS_INTERVAL_MS,
                 MAX_CACHE_SIZE_BYTES,
--- app/src/main/java/com/soundcloud/android/properties/Feature.java
+++ app/src/main/java/com/soundcloud/android/properties/Feature.java
@@ -9,7 +9,6 @@ public enum Feature {
     PAYMENTS_TEST(R.bool.feature_payments_test),
     OKHTTP(R.bool.feature_okhttp),
     GOOGLE_CAST(R.bool.feature_google_cast),
-    SECURE_STREAM_CACHE(R.bool.feature_secure_stream_cache),
     API_MOBILE_STREAM(R.bool.feature_api_mobile_stream),
     DEVICE_KEY_GENERATION(R.bool.feature_device_key_generation),
     ANDROID_L_MEDIA_NOTIFICATION(R.bool.feature_android_l_media_notification),
