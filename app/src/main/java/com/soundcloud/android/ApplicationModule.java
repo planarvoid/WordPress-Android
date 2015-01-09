7191f2e8215751ec3e6af42cf4fdb7fd16122cec
jonschmidt <jon@soundcloud.com>
2015-01-09 14:42:54 +0100

    bumping image blurring to JB MR2
    Fixes #2661

--- app/src/main/java/com/soundcloud/android/ApplicationModule.java
+++ app/src/main/java/com/soundcloud/android/ApplicationModule.java
@@ -187,7 +187,7 @@ public class ApplicationModule {
 
     @Provides
     public ImageProcessor provideImageProcessor(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             return new ImageProcessorJB(context);
         } else {
             return new ImageProcessorCompat();
