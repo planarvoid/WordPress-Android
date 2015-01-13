f4e34a0f852784854cb1e788d7cad2b56ef18cea
jonschmidt <jon@soundcloud.com>
2015-01-13 13:24:48 +0100

    Route Skippy Init Errors to Localytics
    This is so we can better aggregate this data

--- app/src/main/java/com/soundcloud/android/analytics/localytics/LocalyticsEvents.java
+++ app/src/main/java/com/soundcloud/android/analytics/localytics/LocalyticsEvents.java
@@ -9,6 +9,7 @@ final class LocalyticsEvents {
     static final String BUFFER_UNDERRUN = "Buffer Underrun";
     static final String SKIPPY_PLAY = "Skippy Play";
     static final String DEVICE_METRICS = "Device Metrics";
+    static final String SKIPPY_INITILIAZATION_ERROR = "Skippy Init Error";
 
     static final class UI {
         static final String FOLLOW = "Follow";
