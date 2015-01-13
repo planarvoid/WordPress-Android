3b923334746c7a071b3400a1d5f33613f83c9780
Pedro Lopes <lopes.m.pedro@gmail.com>
2015-01-13 16:18:15 +0100

    Add Playlist likes screen feature flag
    Refs #2675

--- app/src/main/java/com/soundcloud/android/properties/Feature.java
+++ app/src/main/java/com/soundcloud/android/properties/Feature.java
@@ -12,7 +12,8 @@ public enum Feature {
     API_MOBILE_STREAM(R.bool.feature_api_mobile_stream),
     OFFLINE_SYNC_FROM_LIKES(R.bool.feature_offline_sync_from_likes),
     TRACK_LIKES_SCREEN(R.bool.feature_track_likes_screen),
-    NEW_LIKES_SYNCER(R.bool.feature_new_likes_syncer);
+    NEW_LIKES_SYNCER(R.bool.feature_new_likes_syncer),
+    PLAYLIST_LIKES_SCREEN(R.bool.feature_playlist_likes_screen);
 
     private final int resourceId;
 
