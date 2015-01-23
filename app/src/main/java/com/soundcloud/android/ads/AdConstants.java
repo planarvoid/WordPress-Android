c9212404373370e85f19b2bbdabae012201a90d8
Karolina Rusin <kr@soundcloud.com>
2015-01-23 13:34:19 +0100

    PMD again - fix build

--- app/src/main/java/com/soundcloud/android/ads/AdConstants.java
+++ app/src/main/java/com/soundcloud/android/ads/AdConstants.java
@@ -3,6 +3,6 @@ package com.soundcloud.android.ads;
 import java.util.concurrent.TimeUnit;
 
 public final class AdConstants {
-    public static int UNSKIPPABLE_TIME_SECS = 15;
-    public static long UNSKIPPABLE_TIME_MS = TimeUnit.SECONDS.toMillis(UNSKIPPABLE_TIME_SECS);
+    public static final int UNSKIPPABLE_TIME_SECS = 15;
+    public static final long UNSKIPPABLE_TIME_MS = TimeUnit.SECONDS.toMillis(UNSKIPPABLE_TIME_SECS);
 }
