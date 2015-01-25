62b04082876fcb560a0032e22a46bef4bb0ed2db
Matthias Kaeppler <mk@soundcloud.com>
2015-01-25 13:48:07 +0100

    Remove a whole bunch of unused classes

--- app/src/test/java/com/soundcloud/android/rx/RxTestHelper.java
+++ app/src/test/java/com/soundcloud/android/rx/RxTestHelper.java
@@ -1,23 +1,9 @@
 package com.soundcloud.android.rx;
 
-import static rx.android.OperatorPaged.Page;
-import static rx.android.OperatorPaged.pagedWith;
-
 import rx.Observable;
-import rx.android.OperatorPaged;
 import rx.android.Pager;
 
 public class RxTestHelper {
-
-    public static <CollT extends Iterable<?>> OperatorPaged.LegacyPager<CollT> endlessPagerFrom(final Observable<CollT> observable) {
-        return new OperatorPaged.LegacyPager<CollT>() {
-            @Override
-            public Observable<Page<CollT>> call(CollT collection) {
-                final OperatorPaged.LegacyPager<CollT> recursivePager = endlessPagerFrom(observable);
-                return observable.lift(pagedWith(recursivePager));
-            }
-        };
-    }
 
     public static <T> Pager<T> pagerWithNextPage(final Observable<T> nextPage) {
         return new Pager<T>() {
