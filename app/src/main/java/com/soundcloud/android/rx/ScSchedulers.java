2d769fd6f2e199a0a4a25059705bf697857822d7
Guillaume Lung <guillaume@soundcloud.com>
2015-02-10 16:44:40 +0100

    Remove unused scheduler

--- app/src/main/java/com/soundcloud/android/rx/ScSchedulers.java
+++ app/src/main/java/com/soundcloud/android/rx/ScSchedulers.java
@@ -14,13 +14,11 @@ public final class ScSchedulers {
     public static final Scheduler STORAGE_SCHEDULER;
     public static final Scheduler API_SCHEDULER;
     public static final Scheduler GRAPHICS_SCHEDULER;
-    public static final Scheduler SEQUENTIAL_SYNCING_SCHEDULER;
 
     static {
         STORAGE_SCHEDULER = Schedulers.from(createExecutor("RxStorageThreadPool", 3));
         API_SCHEDULER = Schedulers.from(createExecutor("RxApiThreadPool", 3));
         GRAPHICS_SCHEDULER= Schedulers.from(createExecutor("RxGraphicsThreadPool", 1));
-        SEQUENTIAL_SYNCING_SCHEDULER = Schedulers.from(createExecutor("RxSequentialDownloadThreadPool", 1));
     }
 
     private static Executor createExecutor(final String threadIdentifier, int numThreads) {
