093dd5fb25e0892fc3b0774d0d8845fd7b5e50e5
Pedro Lopes <lopes.m.pedro@gmail.com>
2015-02-18 11:43:03 +0100

    Refactor remove entity from list subscriber

--- app/src/main/java/com/soundcloud/android/view/adapters/RemoveEntityListSubscriber.java
+++ app/src/main/java/com/soundcloud/android/view/adapters/RemoveEntityListSubscriber.java
@@ -15,20 +15,18 @@ public final class RemoveEntityListSubscriber extends DefaultSubscriber<EntitySt
 
     @Override
     public void onNext(final EntityStateChangedEvent event) {
-        // if (event.isTrackLike() && !event.getNextChangeSet().get(TrackProperty.IS_LIKED)) {
-        boolean changed = false;
         Urn unlikedUrn = event.getNextUrn();
-
-        for (int position = 0; position < adapter.getCount(); position++) {
-            if (adapter.getItem(position).get(EntityProperty.URN).equals(unlikedUrn)) {
-                changed = true;
-                adapter.removeAt(position);
+        int adapterCount = adapter.getCount();
+        for (int position = 0; position < adapterCount; position++) {
+            Urn itemUrn = adapter.getItem(position).get(EntityProperty.URN);
+            if (itemUrn.equals(unlikedUrn)) {
+                removeItemFromAdapterAt(position);
             }
         }
+    }
 
-        if (changed) {
-            adapter.notifyDataSetChanged();
-        }
-        //  }
+    private void removeItemFromAdapterAt(int position) {
+        adapter.removeAt(position);
+        adapter.notifyDataSetChanged();
     }
 }
