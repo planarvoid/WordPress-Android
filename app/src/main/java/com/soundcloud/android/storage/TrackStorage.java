planarvoid/WordPress-Android

commit 3c6f99cc098958643b2fc6d841c8bfba0498a70b

Use the fully qualified field name for the _ID and _TYPE fields
Fixes #2833

app/src/main/java/com/soundcloud/android/storage/TrackStorage.java
@@ -20,6 +20,7 @@ import rx.Subscriber;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 import javax.inject.Inject;
 import java.util.Collection;
@@ -130,14 +131,24 @@ public class TrackStorage extends ScheduledOperations implements Storage<PublicA
             public void call(Subscriber<? super List<Urn>> observer) {
 
                 final boolean isActivityCursor = Content.match(uri).isActivitiesItem();
-                final String idColumn = isActivityCursor ? TableColumns.ActivityView.SOUND_ID : TableColumns.SoundView._ID;
-                final String typeColumn = isActivityCursor ? TableColumns.ActivityView.SOUND_TYPE : TableColumns.SoundView._TYPE;
+                final String idColumn;
+                final String fullIdColumn;
+                final String fullTypeColumn;
+                if (isActivityCursor) {
+                    idColumn = TableColumns.ActivityView.SOUND_ID;
+                    fullIdColumn = Table.ActivityView + "." + TableColumns.ActivityView.SOUND_ID;
+                    fullTypeColumn = Table.ActivityView + "." + TableColumns.ActivityView.SOUND_TYPE;
+                } else {
+                    idColumn = TableColumns.SoundView._ID;
+                    fullIdColumn = Table.SoundView + "." + TableColumns.SoundView._ID + " as " + BaseColumns._ID;
+                    fullTypeColumn = Table.SoundView + "." + TableColumns.SoundView._TYPE;
+                }
 
                 // if playlist, adjust load uri to request the tracks instead of meta_data
                 final Uri adjustedUri = (Content.match(uri) == Content.PLAYLIST) ?
                         Content.PLAYLIST_TRACKS.forQuery(uri.getLastPathSegment()) : uri;
 
-                Cursor cursor = resolver.query(adjustedUri, new String[]{idColumn}, typeColumn + " = ?",
+                Cursor cursor = resolver.query(adjustedUri, new String[] { fullIdColumn }, fullTypeColumn + " = ?",
                         new String[]{String.valueOf(Playable.DB_TYPE_TRACK)}, null);
                 if (!observer.isUnsubscribed()) {
                     try {

commit febc04a037c4e7efc970dbb94bb55617f66c5f5c

Add an additional content match for my posts
This fixes the bug introduced by 3c6f99cc098958643b2fc6d841c8bfba0498a70b, on profile screen, my posts tab, in which a a track would not play when clicked.

With this fix, all the cases should be contemplated (my posts, my profile likes and activities)

app/src/main/java/com/soundcloud/android/storage/TrackStorage.java
@@ -134,7 +134,11 @@ public class TrackStorage extends ScheduledOperations implements Storage<PublicA
                 final String idColumn;
                 final String fullIdColumn;
                 final String fullTypeColumn;
-                if (isActivityCursor) {
+                if (Content.match(uri) == Content.ME_SOUNDS) {
+                    idColumn = TableColumns.SoundAssociationView._ID;
+                    fullIdColumn = Table.SoundAssociationView + "." + TableColumns.SoundAssociationView._ID;
+                    fullTypeColumn = Table.SoundAssociationView + "." + TableColumns.SoundAssociationView._TYPE;
+                } else if (isActivityCursor) {
                     idColumn = TableColumns.ActivityView.SOUND_ID;
                     fullIdColumn = Table.ActivityView + "." + TableColumns.ActivityView.SOUND_ID;
                     fullTypeColumn = Table.ActivityView + "." + TableColumns.ActivityView.SOUND_TYPE;