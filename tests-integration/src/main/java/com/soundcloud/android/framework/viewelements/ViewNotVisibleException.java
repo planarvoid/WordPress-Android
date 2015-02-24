c2d8df3a0fc2f3e222b0a387397d0fce50e62076
Brigit Lyons <brigit.lyons@soundcloud.com>
2015-02-24 16:29:41 +0100

    remove tests-integration folder

--- tests-integration/src/main/java/com/soundcloud/android/framework/viewelements/ViewNotVisibleException.java
+++
@@ -1,7 +0,0 @@
-package com.soundcloud.android.framework.viewelements;
-
-public class ViewNotVisibleException extends ViewException {
-    public ViewNotVisibleException() {
-        super("View is not visible, cannot click it!");
-    }
-}
