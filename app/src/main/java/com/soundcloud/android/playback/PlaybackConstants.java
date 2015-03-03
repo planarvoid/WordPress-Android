d6064032c4522ae09c849861b43a9ee5058a3668
jonschmidt <jon@soundcloud.com>
2015-03-03 14:46:16 +0100

    Force media player temporarily
    because Skippy crashes on OnePlus phones

--- app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
+++ app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
@@ -1,9 +1,8 @@
 package com.soundcloud.android.playback;
 
-import android.os.Build;
-
 public interface PlaybackConstants {
 
-    boolean FORCE_MEDIA_PLAYER = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
+    //boolean FORCE_MEDIA_PLAYER = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
+    boolean FORCE_MEDIA_PLAYER = true; // since Skippy is now broken on OnePlus phones
     long PROGRESS_DELAY_MS = 500L;
 }
