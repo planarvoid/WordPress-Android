da1a52aa2f64e699d8338bc8c418ce31c46af9bd
jonschmidt <jon@soundcloud.com>
2015-03-04 10:04:06 +0100

    Remove non-flag related test from new engage tests

--- app/src/androidTest/java/com/soundcloud/android/tests/playlist/PlaylistLikesNewEngagementsTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/playlist/PlaylistLikesNewEngagementsTest.java
@@ -3,12 +3,10 @@ package com.soundcloud.android.tests.playlist;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
 
 import com.soundcloud.android.framework.TestUser;
-import com.soundcloud.android.framework.helpers.NavigationHelper;
 import com.soundcloud.android.main.MainActivity;
+import com.soundcloud.android.properties.Flag;
 import com.soundcloud.android.screens.PlaylistDetailsScreen;
 import com.soundcloud.android.screens.PlaylistsScreen;
 import com.soundcloud.android.screens.elements.PlaylistItemOverflowMenu;
@@ -22,24 +20,9 @@ public class PlaylistLikesNewEngagementsTest extends ActivityTest<MainActivity> 
 
     @Override
     public void setUp() throws Exception {
+        setDependsOn(Flag.NEW_PLAYLIST_ENGAGEMENTS);
         TestUser.likesUser.logIn(getInstrumentation().getTargetContext());
         super.setUp();
-    }
-
-    public void testDrawerShowsPlaylists() {
-        final PlaylistsScreen playlistsScreen = NavigationHelper.openLikedPlaylists(menuScreen);
-        PlaylistDetailsScreen playlistDetailsScreen = playlistsScreen.clickPlaylistOnCurrentPageAt(0);
-        assertEquals("Should go to Playlist screen", true, playlistDetailsScreen.isVisible());
-    }
-
-    public void testLoadsNextPage() {
-        final PlaylistsScreen playlistsScreen = NavigationHelper.openLikedPlaylists(menuScreen);
-        int numberOfTracks = playlistsScreen.getLoadedTrackCount();
-        assertThat(numberOfTracks, is(greaterThan(0)));
-
-        playlistsScreen.scrollToBottomOfTracksListAndLoadMoreItems();
-
-        assertThat(playlistsScreen.getLoadedTrackCount(), is(greaterThan(numberOfTracks)));
     }
 
     // Given I liked a playlist
@@ -54,6 +37,19 @@ public class PlaylistLikesNewEngagementsTest extends ActivityTest<MainActivity> 
         playlistsScreen.touchLikedPlaylistsTab();
 
         assertEquals(expectedTitle, playlistsScreen.get(0).getTitle());
+    }
+
+    private void likePlaylistAt(PlaylistsScreen playlistsScreen, int index) {
+        final PlaylistDetailsScreen playlistDetailsScreen = playlistsScreen.clickPlaylistAt(index);
+        unlikePlaylistIfLiked(playlistDetailsScreen);
+        playlistDetailsScreen.touchToggleLike();
+        playlistDetailsScreen.clickBack();
+    }
+
+    private void unlikePlaylistIfLiked(PlaylistDetailsScreen playlistDetailsScreen) {
+        if (playlistDetailsScreen.isLiked()) {
+            playlistDetailsScreen.touchToggleLike();
+        }
     }
 
     public void testLikingAndUnlikingPlaylistFromOverflowMenu() {
@@ -91,18 +87,4 @@ public class PlaylistLikesNewEngagementsTest extends ActivityTest<MainActivity> 
         }
         playlistItemOverflowMenu.toggleLike();
     }
-
-    private void likePlaylistAt(PlaylistsScreen playlistsScreen, int index) {
-        final PlaylistDetailsScreen playlistDetailsScreen = playlistsScreen.clickPlaylistAt(index);
-        unlikePlaylistIfLiked(playlistDetailsScreen);
-        playlistDetailsScreen.touchToggleLike();
-        playlistDetailsScreen.clickBack();
-    }
-
-    private void unlikePlaylistIfLiked(PlaylistDetailsScreen playlistDetailsScreen) {
-        if (playlistDetailsScreen.isLiked()) {
-            playlistDetailsScreen.touchToggleLike();
-        }
-    }
-
 }
