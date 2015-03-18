935f247bb3d575e6fc27b0408965507aa396bfc7
Matthias Kaeppler <m.kaeppler@gmail.com>
2015-03-18 13:53:38 +0100

    Throw exception if WriteStorageCommand called from main thread

--- app/src/main/java/com/soundcloud/android/commands/WriteStorageCommand.java
+++ app/src/main/java/com/soundcloud/android/commands/WriteStorageCommand.java
@@ -1,18 +1,35 @@
 package com.soundcloud.android.commands;
 
+import com.google.common.base.Preconditions;
 import com.soundcloud.propeller.PropellerDatabase;
 import com.soundcloud.propeller.WriteResult;
+
+import android.os.Looper;
+
+import javax.inject.Provider;
 
 public abstract class WriteStorageCommand<I, R extends WriteResult, O> extends Command<I, O> {
 
     private final PropellerDatabase propeller;
+    private final Provider<Thread> currentThreadProvider;
 
     protected WriteStorageCommand(PropellerDatabase propeller) {
+        this(propeller, new Provider<Thread>() {
+            @Override
+            public Thread get() {
+                return Looper.getMainLooper().getThread();
+            }
+        });
+    }
+
+    protected WriteStorageCommand(PropellerDatabase propeller, Provider<Thread> currentThreadProvider) {
         this.propeller = propeller;
+        this.currentThreadProvider = currentThreadProvider;
     }
 
     @Override
     public O call(I input) {
+        Preconditions.checkState(currentThreadProvider.get() != Looper.getMainLooper().getThread());
         final R result = write(propeller, input);
         if (result.success()) {
             return transform(result);
--- tests-robolectric/src/test/java/com/soundcloud/android/commands/WriteStorageCommandTest.java
+++ tests-robolectric/src/test/java/com/soundcloud/android/commands/WriteStorageCommandTest.java
@@ -1,6 +1,7 @@
 package com.soundcloud.android.commands;
 
 import static com.soundcloud.android.Expect.expect;
+import static com.soundcloud.android.testsupport.InjectionSupport.providerOf;
 
 import com.soundcloud.android.robolectric.SoundCloudTestRunner;
 import com.soundcloud.android.testsupport.fixtures.TestStorageResults;
@@ -11,33 +12,49 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import android.os.Looper;
+
 @RunWith(SoundCloudTestRunner.class)
 public class WriteStorageCommandTest {
 
     @Mock private PropellerDatabase propeller;
-
-    private DefaultWriteStorageCommand<String, InsertResult> command = new DefaultWriteStorageCommand<String, InsertResult>(propeller) {
-        @Override
-        protected InsertResult write(PropellerDatabase propeller, String input) {
-            return TestStorageResults.successfulInsert();
-        }
-    };
-
-    private DefaultWriteStorageCommand<String, InsertResult> failedCommand = new DefaultWriteStorageCommand<String, InsertResult>(propeller) {
-        @Override
-        protected InsertResult write(PropellerDatabase propeller, String input) {
-            return TestStorageResults.failedInsert();
-        }
-    };
+    @Mock private Thread currentThread;
 
     @Test
     public void shouldReturnResultFromCallIfWriteSuccessful() throws Exception {
-        final InsertResult result = command.call("input");
+        DefaultWriteStorageCommand<String, InsertResult> successfulCommand =
+                new DefaultWriteStorageCommand<String, InsertResult>(propeller, providerOf(currentThread)) {
+            @Override
+            protected InsertResult write(PropellerDatabase propeller, String input) {
+                return TestStorageResults.successfulInsert();
+            }
+        };
+
+        final InsertResult result = successfulCommand.call("input");
         expect(result.success()).toBeTrue();
     }
 
     @Test(expected = PropellerWriteException.class)
     public void shouldRethrowWriteExceptionIfWriteFailed() {
+        DefaultWriteStorageCommand<String, InsertResult> failedCommand =
+                new DefaultWriteStorageCommand<String, InsertResult>(propeller, providerOf(currentThread)) {
+            @Override
+            protected InsertResult write(PropellerDatabase propeller, String input) {
+                return TestStorageResults.failedInsert();
+            }
+        };
         failedCommand.call("input");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowIfWriteOperationPerformedOnMainThread() throws Exception {
+        DefaultWriteStorageCommand<String, InsertResult> mainThreadCommand =
+                new DefaultWriteStorageCommand<String, InsertResult>(propeller, providerOf(Looper.getMainLooper().getThread())) {
+            @Override
+            protected InsertResult write(PropellerDatabase propeller, String input) {
+                return TestStorageResults.failedInsert();
+            }
+        };
+        mainThreadCommand.call("input");
     }
 }
