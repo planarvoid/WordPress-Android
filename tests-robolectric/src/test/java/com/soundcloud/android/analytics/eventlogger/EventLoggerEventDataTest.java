cb0cecce9167345c8e0dcdfdc6af5a7197910aac
Jamie McDonald <jdamcd@gmail.com>
2015-03-18 13:47:17 +0100

    Test for excluding empty event params

--- tests-robolectric/src/test/java/com/soundcloud/android/analytics/eventlogger/EventLoggerEventDataTest.java
+++ tests-robolectric/src/test/java/com/soundcloud/android/analytics/eventlogger/EventLoggerEventDataTest.java
@@ -1,5 +1,8 @@
 package com.soundcloud.android.analytics.eventlogger;
 
+import static com.soundcloud.android.Expect.expect;
+import static com.soundcloud.android.analytics.eventlogger.EventLoggerParam.*;
+
 import com.soundcloud.android.robolectric.SoundCloudTestRunner;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
@@ -12,6 +15,28 @@ public class EventLoggerEventDataTest {
     @Test
     public void implementsEqualsContract() throws Exception {
         EqualsVerifier.forClass(EventLoggerEventData.class)
-                .suppress(Warning.NONFINAL_FIELDS).verify();
+                .suppress(Warning.NONFINAL_FIELDS)
+                .verify();
     }
+
+    @Test
+    public void addsParamWithValueToPayload() {
+        EventLoggerEventData data = new EventLoggerEventData("event", "v0", "client", "1234", "4321", "123");
+
+        data.clickName("clicky");
+
+        expect(data.payload.get(CLICK_NAME)).toEqual("clicky");
+    }
+
+    @Test
+    public void excludesEmptyParamsFromPayload() {
+        EventLoggerEventData data = new EventLoggerEventData("event", "v0", "client", "1234", "4321", "123");
+
+        data.adUrn(null);
+        data.clickObject("");
+
+        expect(data.payload.containsKey(AD_URN)).toBeFalse();
+        expect(data.payload.containsKey(CLICK_OBJECT)).toBeFalse();
+    }
+
 }
