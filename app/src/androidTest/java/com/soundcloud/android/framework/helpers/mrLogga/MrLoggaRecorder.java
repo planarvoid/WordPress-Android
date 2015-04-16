39e3f20b9e123dec0b172fdb4a0e87c2374f95bc
Karolina Rusin <kr@soundcloud.com>
2015-04-16 16:51:32 +0200

    Move mrLogga recorder to separate class

---
+++ app/src/androidTest/java/com/soundcloud/android/framework/helpers/mrLogga/MrLoggaRecorder.java
@@ -0,0 +1,22 @@
+package com.soundcloud.android.framework.helpers.mrLogga;
+
+import static junit.framework.Assert.assertNotNull;
+
+public class MrLoggaRecorder {
+    private final MrLoggaLoggaClient client;
+
+    public MrLoggaRecorder(MrLoggaLoggaClient client) {
+        this.client = client;
+    }
+
+    public void startRecording(String scenarioName) {
+        assertNotNull("You have to name scenario you want to record. ", scenarioName);
+        client.startRecording(scenarioName);
+    }
+
+    public void finishRecording() {
+        client.finishRecording();
+    }
+
+
+}
--- app/src/androidTest/java/com/soundcloud/android/framework/helpers/mrLogga/TrackingActivityTest.java
+++ app/src/androidTest/java/com/soundcloud/android/framework/helpers/mrLogga/TrackingActivityTest.java
@@ -14,6 +14,7 @@ import android.preference.PreferenceManager;
 public abstract class TrackingActivityTest<T extends Activity> extends ActivityTest<T> {
 
     protected MrLoggaVerifier mrLoggaVerifier;
+    protected MrLoggaRecorder mrLoggaRecorder;
 
     public TrackingActivityTest(Class<T> activityClass) {
         super(activityClass);
@@ -25,7 +26,9 @@ public abstract class TrackingActivityTest<T extends Activity> extends ActivityT
 
         final Context context = getInstrumentation().getTargetContext();
         final MrLoggaLoggaClient client = new MrLoggaLoggaClient(context, new DeviceHelper(context, new BuildHelper()), new OkHttpClient());
+
         mrLoggaVerifier = new MrLoggaVerifier(client);
+        mrLoggaRecorder = new MrLoggaRecorder(client);
 
         enableEventLoggerInstantFlush(context);
     }
