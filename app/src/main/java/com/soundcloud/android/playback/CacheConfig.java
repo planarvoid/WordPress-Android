d50f241e7ce75660fc035241144a2ea9b9933697
jonschmidt <jon@soundcloud.com>
2015-04-28 14:14:59 +0200

    Configure stream cache based on country

--- app/src/main/java/com/soundcloud/android/playback/CacheConfig.java
+++ app/src/main/java/com/soundcloud/android/playback/CacheConfig.java
@@ -1,7 +1,28 @@
 package com.soundcloud.android.playback;
 
+import com.soundcloud.android.utils.ScTextUtils;
+
+import android.support.annotation.Nullable;
+
+import java.util.Locale;
+
 public class CacheConfig {
 
-    public static final long MAX_SIZE_BYTES = 60 * 1024 * 1024; // 60MB
+    static final long MIN_SIZE_BYTES = 60  * 1024 * 1024; // 60MB
+    static final long MAX_SIZE_BYTES = 500 * 1024 * 1024; // 500MB
+
+    public static long getCacheSize(@Nullable String countryCode){
+        if (ScTextUtils.isBlank(countryCode)
+                || Locale.US.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.GERMANY.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.FRANCE.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.UK.getCountry().equalsIgnoreCase(countryCode)) {
+            return MIN_SIZE_BYTES;
+
+        } else {
+            return MAX_SIZE_BYTES;
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/soundcloud/android/utils/CountryProvider.java
@@ -0,0 +1,7 @@
+package com.soundcloud.android.utils;
+
+import android.support.annotation.Nullable;
+
+public interface CountryProvider {
+    @Nullable String getCountryCode();
+}
