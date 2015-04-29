97261cc3f8de9b234e60594f642b8880fc53d2d2
Karolina Rusin <kr@soundcloud.com>
2015-04-29 18:31:22 +0200

    disable age gating tests :/

--- app/src/androidTest/java/com/soundcloud/android/tests/ageGating/NotFollowingAsTooYoungUser.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/ageGating/NotFollowingAsTooYoungUser.java
@@ -29,7 +29,8 @@ public class NotFollowingAsTooYoungUser extends ActivityTest<LauncherActivity> {
         playlistTagsScreen = new MainScreen(solo).actionBar().clickSearchButton();
     }
 
-    public void testBelow18UsersAreNotAbleToFollowAgeGatedUsers() {
+    // *** Disabling until Github Issue #2877 is fixed ***/
+    public void ignore_testBelow18UsersAreNotAbleToFollowAgeGatedUsers() {
         ProfileScreen annoyMouseUserScreen = playlistTagsScreen
                 .actionBar()
                 .doSearch("annoymouse")
