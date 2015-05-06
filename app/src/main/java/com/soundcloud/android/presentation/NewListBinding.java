6c708ef9ba7c9f9bc9833298621c21c2e8ca0149
Matthias Kaeppler <mk@soundcloud.com>
2015-05-06 18:35:16 +0200

    Rename type variables

--- app/src/main/java/com/soundcloud/android/presentation/NewListBinding.java
+++ app/src/main/java/com/soundcloud/android/presentation/NewListBinding.java
@@ -27,12 +27,12 @@ public class NewListBinding<Item> {
 
     private final ItemAdapter<Item> adapter;
 
-    public static <T, IT extends Iterable<T>> Builder<IT, T, IT> from(Observable<IT> source) {
-        return from(source, UtilityFunctions.<IT>identity());
+    public static <Item, T extends Iterable<Item>> Builder<T, Item, T> from(Observable<T> source) {
+        return from(source, UtilityFunctions.<T>identity());
     }
 
-    public static <S, T, IT extends Iterable<T>> Builder<S, T, IT> from(
-            Observable<S> source, Func1<S, IT> transformer) {
+    public static <S, Item, T extends Iterable<Item>> Builder<S, Item, T> from(
+            Observable<S> source, Func1<S, T> transformer) {
         return new Builder<>(source, transformer);
     }
 
@@ -76,34 +76,34 @@ public class NewListBinding<Item> {
         return adapter;
     }
 
-    public static class Builder<S, T, IT extends Iterable<T>> {
+    public static class Builder<S, Item, T extends Iterable<Item>> {
 
         private final Observable<S> source;
-        private final Func1<S, IT> transformer;
-        private ItemAdapter<T> adapter;
+        private final Func1<S, T> transformer;
+        private ItemAdapter<Item> adapter;
         private PagingFunction<S> pagingFunction;
 
-        Builder(Observable<S> source, Func1<S, IT> transformer) {
+        Builder(Observable<S> source, Func1<S, T> transformer) {
             this.source = source;
             this.transformer = transformer;
         }
 
-        public Builder<S, T, IT> withPager(PagingFunction<S> pagingFunction) {
+        public Builder<S, Item, T> withPager(PagingFunction<S> pagingFunction) {
             this.pagingFunction = pagingFunction;
             return this;
         }
 
-        public Builder<S, T, IT> withAdapter(ItemAdapter<T> adapter) {
+        public Builder<S, Item, T> withAdapter(ItemAdapter<Item> adapter) {
             this.adapter = adapter;
             return this;
         }
 
-        public NewListBinding<T> build() {
+        public NewListBinding<Item> build() {
             if (pagingFunction != null) {
                 checkArgument(adapter instanceof PagingItemAdapter,
                         "adapter in paged binding must be " + PagingItemAdapter.class);
-                final NewPager<S, IT> pager = NewPager.create(pagingFunction, transformer);
-                return new PagedListBinding<>(pager.page(source), (PagingItemAdapter<T>) adapter, pager);
+                final NewPager<S, T> pager = NewPager.create(pagingFunction, transformer);
+                return new PagedListBinding<>(pager.page(source), (PagingItemAdapter<Item>) adapter, pager);
             } else {
                 return new NewListBinding<>(source.map(transformer), adapter);
             }
