efe1151c177aa1f61a1e4ba4bd8c6dafd7a23bc9
Karolina Rusin <kr@soundcloud.com>
2015-05-19 14:26:34 +0200

    Waiting before finish logging and validation.

--- app/src/androidTest/java/com/soundcloud/android/tests/player/TrackingPlayerTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/player/TrackingPlayerTest.java
@@ -43,6 +43,8 @@ public class TrackingPlayerTest extends TrackingActivityTest<MainActivity> {
     }
 
     private void finishMrLogga() {
+        waiter.waitFiveSeconds();
+
         mrLoggaVerifier.finishLogging();
         mrLoggaVerifier.isValid(TEST_SCENARIO);
     }
