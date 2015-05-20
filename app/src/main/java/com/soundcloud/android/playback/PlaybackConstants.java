dc53cf1a6ffb3f645f9a6f6a43e41695e5074070
jonschmidt <jon@soundcloud.com>
2015-05-20 18:31:35 +0200

    allow OnePluses with Lollipop or greater

--- app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
+++ app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
@@ -6,6 +6,8 @@ public interface PlaybackConstants {
 
     static final String ONE_PLUS_CM = "bacon";
 
-    boolean FORCE_MEDIA_PLAYER = ONE_PLUS_CM.equalsIgnoreCase(Build.HARDWARE);
+    boolean FORCE_MEDIA_PLAYER = (ONE_PLUS_CM.equalsIgnoreCase(Build.HARDWARE)
+            && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP);
+
     long PROGRESS_DELAY_MS = 500L;
 }
