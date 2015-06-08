1dfb18f6774cab9d333b52fe784db54415f715e3
Horaci Cuevas <horaci@gmail.com>
2015-06-08 11:21:42 +0200

    redone twitter url screen test

--- app/src/androidTest/java/com/soundcloud/android/tests/activity/resolve/ResolveTwitterUriTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/activity/resolve/ResolveTwitterUriTest.java
@@ -1,46 +1,31 @@
 package com.soundcloud.android.tests.activity.resolve;
 
-import static com.soundcloud.android.framework.TestUser.defaultUser;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
-import com.soundcloud.android.tests.TestConsts;
-import com.soundcloud.android.deeplinks.ResolveActivity;
 import com.soundcloud.android.screens.elements.VisualPlayerElement;
-import com.soundcloud.android.framework.AccountAssistant;
-import com.soundcloud.android.tests.ActivityTest;
-import com.soundcloud.android.framework.Waiter;
+import com.soundcloud.android.tests.TestConsts;
 
-import android.content.Intent;
 import android.net.Uri;
 
-public class ResolveTwitterUriTest extends ActivityTest<ResolveActivity> {
-    protected static VisualPlayerElement playerScreen;
-    protected static Waiter waiter;
+public class ResolveTwitterUriTest extends ResolveBaseTest {
 
-    public ResolveTwitterUriTest() {
-        super(ResolveActivity.class);
-    }
+    private static final String TRACK_NAME = "Tycho - From Home";
 
     @Override
-    protected void logInHelper() {
-        defaultUser.logIn(getInstrumentation().getTargetContext());
+    protected Uri getUri() {
+        return TestConsts.TWITTER_SOUND_URI;
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        Uri uri = TestConsts.TWITTER_SOUND_URI;
-        setActivityIntent(new Intent(Intent.ACTION_VIEW).setData(uri));
-        super.setUp();
+    public void testTwitterUrl() {
+        final VisualPlayerElement playerElement = getPlayerElement();
+        assertThat(playerElement.getTrackTitle(), is(equalToIgnoringCase(TRACK_NAME)));
+        // make sure recommendations load
+        waiter.waitFiveSeconds();
 
-        waiter = new Waiter(solo);
-        playerScreen = new VisualPlayerElement(solo);
-    }
-
-    public void testResolveExploreUrl() {
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        AccountAssistant.logOut(getInstrumentation());
-        super.tearDown();
+        playerElement.swipeNext();
+        assertThat(playerElement.getTrackTitle(), is(not(equalToIgnoringCase(TRACK_NAME))));
     }
 }
