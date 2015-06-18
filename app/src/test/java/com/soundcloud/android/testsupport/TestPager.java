44e3e5f64053fb73802bbc983a582ef83d726ad3
Matthias Kaeppler <mk@soundcloud.com>
2015-06-18 10:57:57 +0200

    Migrate ListPresenterTest to RL3

---
+++ app/src/test/java/com/soundcloud/android/testsupport/TestPager.java
@@ -0,0 +1,16 @@
+package com.soundcloud.android.testsupport;
+
+import com.soundcloud.android.rx.Pager;
+import rx.Observable;
+
+public class TestPager {
+
+    public static <T> Pager.PagingFunction<T> singlePageFunction() {
+        return new Pager.PagingFunction<T>() {
+            @Override
+            public Observable<T> call(T t) {
+                return Pager.finish();
+            }
+        };
+    }
+}
