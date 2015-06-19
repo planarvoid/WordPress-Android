5050108ad5b9cf2de3a2d59cfc20d40056b6acc1
jonschmidt <jon@soundcloud.com>
2015-06-19 13:53:06 -0400

    Revert "Revert "Use type token to fetch policies""
    This reverts commit ad40d7c68b4e8f8448b491fd19bd682cf2e104fd.

--- tests-robolectric/src/test/java/com/soundcloud/android/policies/FetchPoliciesCommandTest.java
+++ tests-robolectric/src/test/java/com/soundcloud/android/policies/FetchPoliciesCommandTest.java
@@ -2,14 +2,15 @@ package com.soundcloud.android.policies;
 
 import static com.soundcloud.android.Expect.expect;
 import static com.soundcloud.android.matchers.SoundCloudMatchers.isApiRequestTo;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 
+import com.google.common.reflect.TypeToken;
 import com.soundcloud.android.api.ApiClient;
 import com.soundcloud.android.api.ApiEndpoints;
+import com.soundcloud.android.api.model.ModelCollection;
 import com.soundcloud.android.model.Urn;
-import com.soundcloud.android.policies.FetchPoliciesCommand.PolicyInfoCollection;
 import com.soundcloud.android.robolectric.SoundCloudTestRunner;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,7 +28,7 @@ public class FetchPoliciesCommandTest {
 
     private final List<String> body = Arrays.asList("soundcloud:playlists:1");
     private final PolicyInfo policy = new PolicyInfo("soundcloud:playlists:1", true, "Allow", false);
-    private final PolicyInfoCollection policies = new PolicyInfoCollection();
+    private final ModelCollection<PolicyInfo> policies = new ModelCollection<>(Arrays.asList(policy));
 
     @Mock private ApiClient apiClient;
 
@@ -40,7 +41,7 @@ public class FetchPoliciesCommandTest {
     @Test
     public void fetchesPoliciesForGivenTracks() throws Exception {
         when(apiClient.fetchMappedResponse(argThat(
-                isApiRequestTo("POST", ApiEndpoints.POLICIES.path()).withContent(body)), eq(PolicyInfoCollection.class))).thenReturn(policies);
+                isApiRequestTo("POST", ApiEndpoints.POLICIES.path()).withContent(body)), any(TypeToken.class))).thenReturn(policies);
 
         Collection<PolicyInfo> result = command.with(Arrays.asList(Urn.forPlaylist(1))).call();
 
