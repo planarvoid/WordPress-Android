af80f6788aece3eef5edd4e5ab2524b4f90d7c0e
Matthias Kaeppler <mk@soundcloud.com>
2015-06-24 16:54:15 +0200

    Fix broken import

--- app/src/test/java/com/soundcloud/android/testsupport/fixtures/DatabaseFixtures.java
+++ app/src/test/java/com/soundcloud/android/testsupport/fixtures/DatabaseFixtures.java
@@ -11,7 +11,6 @@ import com.soundcloud.android.storage.Table;
 import com.soundcloud.android.storage.TableColumns;
 import com.soundcloud.android.sync.likes.ApiLike;
 import com.soundcloud.android.sync.posts.ApiPost;
-import com.soundcloud.android.testsupport.ModelFixtures;
 
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
