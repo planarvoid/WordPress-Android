cea86e16817f170c3e425a6e4b54437fb62c22de
Karolina Rusin <kr@soundcloud.com>
2015-06-29 17:48:59 +0200

    Move EntityStateChangeEventTest to Robolectric3

--- app/src/test/java/com/soundcloud/android/events/EntityStateChangedEventTest.java
+++ app/src/test/java/com/soundcloud/android/events/EntityStateChangedEventTest.java
@@ -1,39 +1,38 @@
 package com.soundcloud.android.events;
 
-import static com.soundcloud.android.Expect.expect;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.soundcloud.android.testsupport.PlatformUnitTest;
 import com.soundcloud.android.testsupport.fixtures.TestPropertySets;
 import com.soundcloud.android.tracks.TrackProperty;
 import com.soundcloud.propeller.PropertySet;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 
-@RunWith(SoundCloudTestRunner.class)
-public class EntityStateChangedEventTest {
+public class EntityStateChangedEventTest extends PlatformUnitTest {
 
     @Test
     public void shouldIndicateSingularChangeEvents() {
         EntityStateChangedEvent multiChangeEvent = EntityStateChangedEvent.fromSync(
                 Arrays.asList(TestPropertySets.fromApiTrack(), TestPropertySets.fromApiTrack()));
-        expect(multiChangeEvent.isSingularChange()).toBeFalse();
+        assertThat(multiChangeEvent.isSingularChange()).isFalse();
+
         EntityStateChangedEvent singleChangeEvent = EntityStateChangedEvent.fromSync(TestPropertySets.fromApiTrack());
-        expect(singleChangeEvent.isSingularChange()).toBeTrue();
+        assertThat(singleChangeEvent.isSingularChange()).isTrue();
     }
 
     @Test
     public void shouldReturnSingleUrnFromSingularChangeEvent() {
         PropertySet track = TestPropertySets.fromApiTrack();
         EntityStateChangedEvent singleChangeEvent = EntityStateChangedEvent.fromSync(track);
-        expect(singleChangeEvent.getNextUrn()).toEqual(track.get(TrackProperty.URN));
+        assertThat(singleChangeEvent.getNextUrn()).isEqualTo(track.get(TrackProperty.URN));
     }
 
     @Test
     public void shouldReturnSingleChangeSetFromSingularChangeEvent() {
         PropertySet track = TestPropertySets.fromApiTrack();
         EntityStateChangedEvent singleChangeEvent = EntityStateChangedEvent.fromSync(track);
-        expect(singleChangeEvent.getNextChangeSet()).toEqual(track);
+        assertThat(singleChangeEvent.getNextChangeSet()).isEqualTo(track);
     }
 }
