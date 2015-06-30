ffd168b5aea08b38512a9535ad49913d1f7e7af2
Karolina Rusin <kr@soundcloud.com>
2015-06-30 16:00:24 +0200

    Remove unused operations

--- app/src/main/java/com/soundcloud/android/offline/OfflineContentStartReceiver.java
+++ app/src/main/java/com/soundcloud/android/offline/OfflineContentStartReceiver.java
@@ -1,21 +1,15 @@
 package com.soundcloud.android.offline;
 
-import com.soundcloud.android.SoundCloudApplication;
 import com.soundcloud.android.utils.Log;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import javax.inject.Inject;
-
 public class OfflineContentStartReceiver extends BroadcastReceiver {
-
-    @Inject OfflineContentOperations operations;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SoundCloudApplication.getObjectGraph().inject(this);
         Log.d(OfflineContentService.TAG, "Offline Content Start Receiver notified. Starting service.");
         OfflineContentService.start(context);
     }
