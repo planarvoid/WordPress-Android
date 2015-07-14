f7abb00e1dc37fa550f7520b1738af8ad9e9cfb5
Fernando Cejas <fcejas@gmail.com>
2015-07-14 16:43:18 +0200

    Optimize imports.
    Ref #3407

--- app/src/main/java/com/soundcloud/android/playback/ui/SlidingPlayerController.java
+++ app/src/main/java/com/soundcloud/android/playback/ui/SlidingPlayerController.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
