079e1e83eae8d7a467edac5196d7e3548b28cab6
jonschmidt <jon@soundcloud.com>
2015-08-06 11:50:17 +0200

    Force MediaPlayer on Marshall Phone

--- app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
+++ app/src/main/java/com/soundcloud/android/playback/PlaybackConstants.java
@@ -1,5 +1,15 @@
 package com.soundcloud.android.playback;
 
-public class PlaybackConstants {
-    public static final long PROGRESS_DELAY_MS = 500L;
+import android.os.Build;
+
+public interface PlaybackConstants {
+
+    String MARSHALL_BRAND = "Marshall";
+    String MARSHALL_MODEL = "London";
+
+    // Force MP for marshall, as Skippy does not support their EQ needs yet
+    boolean FORCE_MEDIA_PLAYER = MARSHALL_MODEL.equalsIgnoreCase(Build.MODEL)
+            && MARSHALL_BRAND.equals(Build.BRAND);
+
+    long PROGRESS_DELAY_MS = 500L;
 }
