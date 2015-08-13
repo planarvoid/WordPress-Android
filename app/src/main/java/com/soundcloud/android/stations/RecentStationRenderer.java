3fdf219918f2106dffebb3c3bb492040958d3acf
Brian Egan and Guillaume Lung <egan+guillaume@soundcloud.com>
2015-08-13 15:14:06 +0200

    Introduce the Station Item Design and grid layout.
    https://soundcloud.atlassian.net/browse/STAT-142

--- app/src/main/java/com/soundcloud/android/stations/RecentStationRenderer.java
+++ app/src/main/java/com/soundcloud/android/stations/RecentStationRenderer.java
@@ -1,28 +1,48 @@
 package com.soundcloud.android.stations;
 
+import com.soundcloud.android.R;
+import com.soundcloud.android.image.ApiImageSize;
+import com.soundcloud.android.image.ImageOperations;
 import com.soundcloud.android.presentation.CellRenderer;
 
+import android.content.res.Resources;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import javax.inject.Inject;
 import java.util.List;
 
 class RecentStationRenderer implements CellRenderer<Station> {
+    private final ImageOperations imageOperations;
+    private final Resources resources;
 
     @Inject
-    public RecentStationRenderer() {
+    public RecentStationRenderer(ImageOperations imageOperations, Resources resources) {
+        this.imageOperations = imageOperations;
+        this.resources = resources;
     }
 
     @Override
-    public View createItemView(ViewGroup viewGroup) {
-        return new TextView(viewGroup.getContext());
+    public View createItemView(ViewGroup parent) {
+        return LayoutInflater.from(parent.getContext()).inflate(R.layout.station_item, parent, false);
     }
 
     @Override
     public void bindItemView(int position, View view, List<Station> stations) {
         final Station station = stations.get(position);
-        ((TextView) view).setText(station.getTitle());
+        final ImageView artwork = (ImageView) view.findViewById(R.id.artwork);
+        final TextView title = (TextView) view.findViewById(R.id.title);
+
+        title.setText(station.getTitle());
+
+        imageOperations.displayInAdapterView(
+                station.getUrn(),
+                ApiImageSize.getFullImageSize(resources),
+                artwork
+        );
     }
+
 }
