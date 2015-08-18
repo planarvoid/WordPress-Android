deb58c06a9e9cd3fa11415f78f471a1571fe9362
Karolina Rusin <kr@soundcloud.com>
2015-08-18 15:29:23 +0200

    Moving all policy test package over to R3
    Refs #3706

--- app/src/test/java/com/soundcloud/android/policies/FetchPoliciesCommandTest.java
+++ app/src/test/java/com/soundcloud/android/policies/FetchPoliciesCommandTest.java
@@ -1,7 +1,7 @@
 package com.soundcloud.android.policies;
 
-import static com.soundcloud.android.Expect.expect;
 import static com.soundcloud.android.testsupport.matchers.RequestMatchers.isApiRequestTo;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.when;
@@ -10,41 +10,39 @@ import com.soundcloud.android.api.ApiClient;
 import com.soundcloud.android.api.ApiEndpoints;
 import com.soundcloud.android.api.model.ModelCollection;
 import com.soundcloud.android.model.Urn;
-import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.soundcloud.android.testsupport.AndroidUnitTest;
 import com.soundcloud.java.reflect.TypeToken;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-@RunWith(SoundCloudTestRunner.class)
-public class FetchPoliciesCommandTest {
+public class FetchPoliciesCommandTest extends AndroidUnitTest {
 
     private FetchPoliciesCommand command;
 
-    private final List<String> body = Arrays.asList("soundcloud:playlists:1");
+    private final List<String> body = Collections.singletonList("soundcloud:playlists:1");
     private final PolicyInfo policy = new PolicyInfo("soundcloud:playlists:1", true, "Allow", false);
-    private final ModelCollection<PolicyInfo> policies = new ModelCollection<>(Arrays.asList(policy));
+    private final ModelCollection<PolicyInfo> policies = new ModelCollection<>(Collections.singletonList(policy));
 
     @Mock private ApiClient apiClient;
 
     @Before
     public void setUp() {
-        policies.setCollection(Arrays.asList(policy));
         this.command = new FetchPoliciesCommand(apiClient);
     }
 
     @Test
     public void fetchesPoliciesForGivenTracks() throws Exception {
         when(apiClient.fetchMappedResponse(argThat(
-                isApiRequestTo("POST", ApiEndpoints.POLICIES.path()).withContent(body)), any(TypeToken.class))).thenReturn(policies);
+                isApiRequestTo("POST", ApiEndpoints.POLICIES.path()).withContent(body)), any(TypeToken.class)))
+                .thenReturn(policies);
 
-        Collection<PolicyInfo> result = command.with(Arrays.asList(Urn.forPlaylist(1))).call();
+        Collection<PolicyInfo> result = command.with(Collections.singletonList(Urn.forPlaylist(1))).call();
 
-        expect(result).toContainExactly(policy);
+        assertThat(result).contains(policy);
     }
 }
