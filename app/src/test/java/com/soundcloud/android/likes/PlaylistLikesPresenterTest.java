acaa5046ecc04b27f5b98a4b2c6a1d787aa94cd6
Matthias Kaeppler <mk@soundcloud.com>
2015-09-15 15:28:37 +0200

    Fix PlaylistLikesPresenterTest

--- app/src/test/java/com/soundcloud/android/likes/PlaylistLikesPresenterTest.java
+++ app/src/test/java/com/soundcloud/android/likes/PlaylistLikesPresenterTest.java
@@ -4,8 +4,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.soundcloud.android.Navigator;
+import com.soundcloud.android.R;
 import com.soundcloud.android.analytics.Screen;
-import com.soundcloud.android.image.ImageOperations;
 import com.soundcloud.android.model.Urn;
 import com.soundcloud.android.playlists.PlaylistItem;
 import com.soundcloud.android.playlists.PlaylistLikesAdapter;
@@ -25,8 +25,8 @@ import rx.Observable;
 
 import android.app.Activity;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ListView;
 
 import java.util.List;
 
@@ -35,13 +35,12 @@ public class PlaylistLikesPresenterTest extends AndroidUnitTest {
     private PlaylistLikesPresenter presenter;
 
     @Mock private PlaylistLikeOperations likeOperations;
-    @Mock private ImageOperations imageOperations;
     @Mock private SwipeRefreshAttacher swipeRefreshAttacher;
     @Mock private PlaylistLikesAdapter adapter;
     @Mock private Fragment fragment;
     @Mock private Activity context;
     @Mock private View fragmentView;
-    @Mock private ListView listView;
+    @Mock private RecyclerView recyclerView;
     @Mock private View itemView;
     @Mock private EmptyView emptyView;
     @Mock private Navigator navigator;
@@ -51,15 +50,20 @@ public class PlaylistLikesPresenterTest extends AndroidUnitTest {
     @Before
     public void setUp() throws Exception {
         // TODO: Extract this common ListPresenter setup to a common base test class
-        presenter = new PlaylistLikesPresenter(imageOperations, swipeRefreshAttacher, likeOperations,
+        presenter = new PlaylistLikesPresenter(swipeRefreshAttacher, likeOperations,
                 adapter, testEventBus, navigator);
-        when(fragmentView.findViewById(android.R.id.list)).thenReturn(listView);
+        when(fragmentView.findViewById(R.id.ak_recycler_view)).thenReturn(recyclerView);
         when(fragmentView.findViewById(android.R.id.empty)).thenReturn(emptyView);
         when(likeOperations.likedPlaylists()).thenReturn(Observable.<List<PropertySet>>empty());
         when(likeOperations.pagingFunction()).thenReturn(TestPager.<List<PropertySet>>singlePageFunction());
         when(likeOperations.onPlaylistLiked()).thenReturn(Observable.<PropertySet>empty());
         when(likeOperations.onPlaylistUnliked()).thenReturn(Observable.<Urn>empty());
-        when(itemView.getContext()).thenReturn(context);
+
+        when(fragmentView.getContext()).thenReturn(context());
+        when(fragmentView.getResources()).thenReturn(resources());
+        when(itemView.getContext()).thenReturn(context());
+        when(itemView.getResources()).thenReturn(resources());
+        when(recyclerView.getAdapter()).thenReturn(adapter);
     }
 
     @Test
@@ -71,7 +75,7 @@ public class PlaylistLikesPresenterTest extends AndroidUnitTest {
 
         presenter.onItemClicked(itemView, 0);
 
-        verify(navigator).openPlaylist(context, clickedPlaylist.get(PlaylistProperty.URN), Screen.SIDE_MENU_PLAYLISTS);
+        verify(navigator).openPlaylist(context(), clickedPlaylist.get(PlaylistProperty.URN), Screen.SIDE_MENU_PLAYLISTS);
     }
 
     @Test
