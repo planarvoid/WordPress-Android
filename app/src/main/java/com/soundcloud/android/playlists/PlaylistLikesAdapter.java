7250e55511e5d70bbfabecf80c095b5b5b279026
Matthias Kaeppler <mk@soundcloud.com>
2015-09-16 10:45:44 +0200

    Simplify ViewHolder stuff in adapter

--- app/src/main/java/com/soundcloud/android/playlists/PlaylistLikesAdapter.java
+++ app/src/main/java/com/soundcloud/android/playlists/PlaylistLikesAdapter.java
@@ -1,13 +1,13 @@
 package com.soundcloud.android.playlists;
 
 import com.soundcloud.android.presentation.PagingRecyclerItemAdapter;
+import com.soundcloud.android.presentation.RecyclerItemAdapter;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import javax.inject.Inject;
 
-public class PlaylistLikesAdapter extends PagingRecyclerItemAdapter<PlaylistItem, PlaylistLikesAdapter.ViewHolder> {
+public class PlaylistLikesAdapter extends PagingRecyclerItemAdapter<PlaylistItem, RecyclerItemAdapter.ViewHolder> {
 
     private static final int PLAYLIST_ITEM_VIEW_TYPE = 0;
 
@@ -17,19 +17,12 @@ public class PlaylistLikesAdapter extends PagingRecyclerItemAdapter<PlaylistItem
     }
 
     @Override
-    protected ViewHolder createViewHolder(View view) {
-        return new ViewHolder(view);
+    protected RecyclerItemAdapter.ViewHolder createViewHolder(View view) {
+        return new RecyclerItemAdapter.ViewHolder(view);
     }
 
     @Override
     public int getBasicItemViewType(int i) {
         return PLAYLIST_ITEM_VIEW_TYPE;
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
     }
 }
