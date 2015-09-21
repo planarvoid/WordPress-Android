c9ec6932bd1f42c15da055206ba9d6e278c16615
Horaci Cuevas <horaci@gmail.com>
2015-09-21 11:43:40 +0200

    removed unused import

--- app/src/main/java/com/soundcloud/android/stream/SoundStreamPresenter.java
+++ app/src/main/java/com/soundcloud/android/stream/SoundStreamPresenter.java
@@ -31,7 +31,6 @@ import rx.subscriptions.CompositeSubscription;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.View;
 
 import javax.inject.Inject;
