1839dbd2eb210110b03fae5d45d1fd55291625bc
Horaci Cuevas <horaci@gmail.com>
2015-09-22 10:50:42 +0200

    Rename connection types to lowercase

--- app/src/main/java/com/soundcloud/android/events/ConnectionType.java
+++ app/src/main/java/com/soundcloud/android/events/ConnectionType.java
@@ -1,8 +1,8 @@
 package com.soundcloud.android.events;
 
 public enum ConnectionType {
-    TWO_G("2G"),
-    THREE_G("3G"),
+    TWO_G("2g"),
+    THREE_G("3g"),
     FOUR_G("4g"),
     WIFI("wifi"),
     OFFLINE("offline"),
