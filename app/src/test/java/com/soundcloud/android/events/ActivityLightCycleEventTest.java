14fbaa223f78ea71fd367dec3cc340dba1c9f06f
Horaci Cuevas <horaci@gmail.com>
2015-09-29 15:43:19 +0200

    Remove unused throws Exception

--- app/src/test/java/com/soundcloud/android/events/ActivityLightCycleEventTest.java
+++ app/src/test/java/com/soundcloud/android/events/ActivityLightCycleEventTest.java
@@ -13,35 +13,35 @@ public class ActivityLightCycleEventTest extends AndroidUnitTest {
     @Mock Activity activity;
 
     @Test
-    public void forOnCreateFactoryMethodShouldReturnAnEventInstanceForOnCreateEvent() throws Exception {
+    public void forOnCreateFactoryMethodShouldReturnAnEventInstanceForOnCreateEvent() {
         ActivityLifeCycleEvent activityLifeCycleEvent = ActivityLifeCycleEvent.forOnCreate(activity);
         assertThat(activityLifeCycleEvent.getKind()).isEqualTo(ActivityLifeCycleEvent.ON_CREATE_EVENT);
         assertThat(activityLifeCycleEvent.getActivityClass().getSuperclass().isAssignableFrom(Activity.class)).isTrue();
     }
 
     @Test
-    public void forOnResumeFactoryMethodShouldReturnAnEventInstanceForOnResumeEvent() throws Exception {
+    public void forOnResumeFactoryMethodShouldReturnAnEventInstanceForOnResumeEvent() {
         ActivityLifeCycleEvent activityLifeCycleEvent = ActivityLifeCycleEvent.forOnResume(activity);
         assertThat(activityLifeCycleEvent.getKind()).isEqualTo(ActivityLifeCycleEvent.ON_RESUME_EVENT);
         assertThat(activityLifeCycleEvent.getActivityClass().getSuperclass().isAssignableFrom(Activity.class)).isTrue();
     }
 
     @Test
-    public void forOnResumeFactoryMethodShouldReturnAnEventInstanceForOnPauseEvent() throws Exception {
+    public void forOnResumeFactoryMethodShouldReturnAnEventInstanceForOnPauseEvent() {
         ActivityLifeCycleEvent activityLifeCycleEvent = ActivityLifeCycleEvent.forOnPause(activity);
         assertThat(activityLifeCycleEvent.getKind()).isEqualTo(ActivityLifeCycleEvent.ON_PAUSE_EVENT);
         assertThat(activityLifeCycleEvent.getActivityClass().getSuperclass().isAssignableFrom(Activity.class)).isTrue();
     }
 
     @Test
-    public void forOnPauseFactoryMethodShouldReturnAnEventInstanceForOnStartEvent() throws Exception {
+    public void forOnPauseFactoryMethodShouldReturnAnEventInstanceForOnStartEvent() {
         ActivityLifeCycleEvent activityLifeCycleEvent = ActivityLifeCycleEvent.forOnStart(activity);
         assertThat(activityLifeCycleEvent.getKind()).isEqualTo(ActivityLifeCycleEvent.ON_START_EVENT);
         assertThat(activityLifeCycleEvent.getActivityClass().getSuperclass().isAssignableFrom(Activity.class)).isTrue();
     }
 
     @Test
-    public void forOnPauseFactoryMethodShouldReturnAnEventInstanceForOnStopEvent() throws Exception {
+    public void forOnPauseFactoryMethodShouldReturnAnEventInstanceForOnStopEvent() {
         ActivityLifeCycleEvent activityLifeCycleEvent = ActivityLifeCycleEvent.forOnStop(activity);
         assertThat(activityLifeCycleEvent.getKind()).isEqualTo(ActivityLifeCycleEvent.ON_STOP_EVENT);
         assertThat(activityLifeCycleEvent.getActivityClass().getSuperclass().isAssignableFrom(Activity.class)).isTrue();
