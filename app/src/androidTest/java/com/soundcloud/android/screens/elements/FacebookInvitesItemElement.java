4d7e4c189a29a63ac41c8eb0a0ea697d13616d6b
Slawomir Smiechura <slawomir@soundcloud.com>
2015-11-08 14:32:55 +0100

    Wait for notification to disappear after closing

--- app/src/androidTest/java/com/soundcloud/android/screens/elements/FacebookInvitesItemElement.java
+++ app/src/androidTest/java/com/soundcloud/android/screens/elements/FacebookInvitesItemElement.java
@@ -2,6 +2,7 @@ package com.soundcloud.android.screens.elements;
 
 import com.soundcloud.android.R;
 import com.soundcloud.android.framework.Han;
+import com.soundcloud.android.framework.Waiter;
 import com.soundcloud.android.framework.viewelements.TextElement;
 import com.soundcloud.android.framework.viewelements.ViewElement;
 import com.soundcloud.android.framework.with.With;
@@ -10,14 +11,17 @@ import com.soundcloud.android.screens.StreamScreen;
 public class FacebookInvitesItemElement {
     private final ViewElement wrapped;
     private final Han testDriver;
+    private final Waiter waiter;
 
     public FacebookInvitesItemElement(Han testDriver, ViewElement wrapped) {
         this.wrapped = wrapped;
         this.testDriver = testDriver;
+        this.waiter = new Waiter(testDriver);
     }
 
     public StreamScreen close() {
         wrapped.findElement(With.id(R.id.close_button)).click();
+        waiter.waitForElementToBeInvisible(With.id(R.id.close_button));
         return new StreamScreen(testDriver);
     }
 
