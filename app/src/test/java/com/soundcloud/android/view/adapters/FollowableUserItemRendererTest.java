17cdcb519d6a659699c4f30e318686bcf0e1792c
Horaci Cuevas <horaci@gmail.com>
2015-11-09 08:47:29 -0500

    Migrated robolectric test

--- app/src/test/java/com/soundcloud/android/view/adapters/FollowableUserItemRendererTest.java
+++ app/src/test/java/com/soundcloud/android/view/adapters/FollowableUserItemRendererTest.java
@@ -1,36 +1,33 @@
 package com.soundcloud.android.view.adapters;
 
-import static com.soundcloud.android.Expect.expect;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.soundcloud.android.R;
+import com.soundcloud.android.analytics.EngagementsTracking;
 import com.soundcloud.android.api.model.ApiUser;
 import com.soundcloud.android.associations.NextFollowingOperations;
 import com.soundcloud.android.image.ImageOperations;
 import com.soundcloud.android.properties.FeatureFlags;
 import com.soundcloud.android.properties.Flag;
-import com.soundcloud.android.robolectric.SoundCloudTestRunner;
+import com.soundcloud.android.testsupport.AndroidUnitTest;
 import com.soundcloud.android.testsupport.fixtures.ModelFixtures;
 import com.soundcloud.android.testsupport.fixtures.TestPropertySets;
 import com.soundcloud.android.users.UserItem;
 import com.soundcloud.android.util.CondensedNumberFormatter;
-import com.xtremelabs.robolectric.Robolectric;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ToggleButton;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Locale;
 
-@RunWith(SoundCloudTestRunner.class)
-public class FollowableUserItemRendererTest {
+public class FollowableUserItemRendererTest extends AndroidUnitTest {
 
     private FollowableUserItemRenderer renderer;
 
@@ -38,42 +35,42 @@ public class FollowableUserItemRendererTest {
     @Mock private LayoutInflater inflater;
     @Mock private ImageOperations imageOperations;
     @Mock private NextFollowingOperations followingOperations;
+    @Mock private EngagementsTracking engagementsTracking;
 
     private final CondensedNumberFormatter numberFormatter =
-            CondensedNumberFormatter.create(Locale.US, Robolectric.application.getResources());
+            CondensedNumberFormatter.create(Locale.US, resources());
 
     private View itemView;
     private ApiUser user;
 
     @Before
     public void setup() {
-        renderer = new FollowableUserItemRenderer(imageOperations, numberFormatter, followingOperations, featureFlags);
+        renderer = new FollowableUserItemRenderer(imageOperations, numberFormatter, followingOperations, featureFlags, engagementsTracking);
 
-        final Context context = Robolectric.application;
-        itemView = LayoutInflater.from(context).inflate(R.layout.user_list_item, new FrameLayout(context), false);
+        itemView = LayoutInflater.from(context()).inflate(R.layout.user_list_item, new FrameLayout(context()), false);
         user = ModelFixtures.create(ApiUser.class);
-        
+
         when(featureFlags.isEnabled(Flag.FOLLOW_USER_SEARCH)).thenReturn(true);
     }
 
     @Test
     public void shouldSetFollowedToggleButton() {
         UserItem followedUserItem = UserItem.from(TestPropertySets.userFollowing(user, true));
-        renderer.bindItemView(0, itemView, Arrays.asList(followedUserItem));
+        renderer.bindItemView(0, itemView, Collections.singletonList(followedUserItem));
 
         ToggleButton followButton = getFollowToggleButton();
-        expect(followButton.isChecked()).toEqual(true);
-        expect(followButton.getVisibility()).toEqual(View.VISIBLE);
+        assertThat(followButton.isChecked()).isTrue();
+        assertThat(followButton.getVisibility()).isEqualTo(View.VISIBLE);
     }
 
     @Test
     public void shouldNotSetFollowedToggleButton() {
         UserItem unfollowedUserItem = UserItem.from(TestPropertySets.userFollowing(user, false));
-        renderer.bindItemView(0, itemView, Arrays.asList(unfollowedUserItem));
+        renderer.bindItemView(0, itemView, Collections.singletonList(unfollowedUserItem));
 
         ToggleButton followButton = getFollowToggleButton();
-        expect(followButton.isChecked()).toEqual(false);
-        expect(followButton.getVisibility()).toEqual(View.VISIBLE);
+        assertThat(followButton.isChecked()).isFalse();
+        assertThat(followButton.getVisibility()).isEqualTo(View.VISIBLE);
     }
 
     private ToggleButton getFollowToggleButton() {
