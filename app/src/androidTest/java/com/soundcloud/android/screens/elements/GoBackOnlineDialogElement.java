f5a9b22e6a70f7fc2e28849157921bdc92281b95
Slawomir Smiechura <slawomir@soundcloud.com>
2015-11-09 13:24:36 +0100

    Fixes another issue when testing dialogs

--- app/src/androidTest/java/com/soundcloud/android/screens/elements/GoBackOnlineDialogElement.java
+++ app/src/androidTest/java/com/soundcloud/android/screens/elements/GoBackOnlineDialogElement.java
@@ -4,15 +4,23 @@ import static com.soundcloud.android.framework.with.With.text;
 
 import com.soundcloud.android.R;
 import com.soundcloud.android.framework.Han;
+import com.soundcloud.android.framework.viewelements.ViewElement;
 import com.soundcloud.android.framework.with.With;
 
 public class GoBackOnlineDialogElement extends Element {
+
+    private With continueButtonLocator = text(solo.getString(R.string.offline_dialog_go_online_continue));
 
     public GoBackOnlineDialogElement(Han solo) {
         super(solo, With.id(R.id.go_back_online_dialog));
     }
 
     public void clickContinue() {
-        solo.findElement(text(solo.getString(R.string.offline_dialog_go_online_continue))).click();
+        continueButton().click();
+        waiter.waitForElementToBeInvisible(continueButtonLocator);
+    }
+
+    private ViewElement continueButton() {
+        return solo.findElement(continueButtonLocator);
     }
 }
