712492727607c644354af65eac10a70b4f8cb26f
Brigit Lyons <brigit.lyons@soundcloud.com>
2015-11-12 11:21:40 +0100

    observe toasts after activity has been started

--- app/src/androidTest/java/com/soundcloud/android/tests/ActivityTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/ActivityTest.java
@@ -50,7 +50,6 @@ public abstract class ActivityTest<T extends Activity> extends ActivityInstrumen
         AccountAssistant.logOut(getInstrumentation());
         assertNull(AccountAssistant.getAccount(getInstrumentation().getTargetContext()));
 
-        observeToasts();
         networkManagerClient = new NetworkManagerClient(getInstrumentation().getContext());
 
         testCaseName = String.format("%s.%s", getClass().getName(), getName());
@@ -63,6 +62,8 @@ public abstract class ActivityTest<T extends Activity> extends ActivityInstrumen
         beforeStartActivity();
         logIn();
         getActivity();
+
+        observeToasts();
 
         mainNavHelper = new MainNavigationHelper(solo);
 
