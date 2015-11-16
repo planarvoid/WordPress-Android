4c94427971a448f9a2e710ef6dfbdf9b9d713066
Karolina Rusin <kr@soundcloud.com>
2015-11-16 12:11:33 +0100

    Disable experiments in debug builds.

--- app/src/main/java/com/soundcloud/android/configuration/experiments/ActiveExperiments.java
+++ app/src/main/java/com/soundcloud/android/configuration/experiments/ActiveExperiments.java
@@ -1,5 +1,7 @@
 package com.soundcloud.android.configuration.experiments;
 
+import com.soundcloud.android.properties.ApplicationProperties;
+
 import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
@@ -9,15 +11,19 @@ class ActiveExperiments {
     private static final String[] LAYERS = { "android_listening" };
     private static final List<String> EXPERIMENTS = Collections.singletonList(ShareButtonExperiment.NAME);
 
+    private ApplicationProperties applicationProperties;
+
     @Inject
-    ActiveExperiments() {}
+    ActiveExperiments(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
 
     public String[] getRequestLayers() {
         return LAYERS;
     }
 
     public boolean isActive(String experimentName) {
-        return EXPERIMENTS.contains(experimentName);
+        return EXPERIMENTS.contains(experimentName) && !applicationProperties.isDebugBuild();
     }
 
 }
