d4cd9ef9000fe97ceb312984fd319277db6e219f
Fernando Cejas <fcejas@gmail.com>
2015-11-19 18:42:42 +0100

    Enable Follow User on Search Results feature. Fix #4378
    Remove follow user on search feature flag.
    
    Move subscription to onCreate.
    
    Rename lightcycle subscription.

--- app/src/main/java/com/soundcloud/android/view/adapters/FollowableUserItemRenderer.java
+++ app/src/main/java/com/soundcloud/android/view/adapters/FollowableUserItemRenderer.java
@@ -6,8 +6,6 @@ import com.soundcloud.android.R;
 import com.soundcloud.android.analytics.EngagementsTracking;
 import com.soundcloud.android.associations.FollowingOperations;
 import com.soundcloud.android.image.ImageOperations;
-import com.soundcloud.android.properties.FeatureFlags;
-import com.soundcloud.android.properties.Flag;
 import com.soundcloud.android.users.UserItem;
 import com.soundcloud.android.util.CondensedNumberFormatter;
 
@@ -20,16 +18,15 @@ import java.util.List;
 public class FollowableUserItemRenderer extends UserItemRenderer {
 
     private final FollowingOperations followingOperations;
-    private final FeatureFlags featureFlags;
     private final EngagementsTracking engagementsTracking;
 
     @Inject
-    public FollowableUserItemRenderer(ImageOperations imageOperations, CondensedNumberFormatter numberFormatter,
-                                      FollowingOperations followingOperations, FeatureFlags featureFlags,
+    public FollowableUserItemRenderer(ImageOperations imageOperations,
+                                      CondensedNumberFormatter numberFormatter,
+                                      FollowingOperations followingOperations,
                                       EngagementsTracking engagementsTracking) {
         super(imageOperations, numberFormatter);
         this.followingOperations = followingOperations;
-        this.featureFlags = featureFlags;
         this.engagementsTracking = engagementsTracking;
     }
 
@@ -41,19 +38,14 @@ public class FollowableUserItemRenderer extends UserItemRenderer {
 
     private void setupFollowToggle(View itemView, final UserItem user) {
         final ToggleButton toggleFollow = ((ToggleButton) itemView.findViewById(R.id.toggle_btn_follow));
-
-        if(featureFlags.isEnabled(Flag.FOLLOW_USER_SEARCH)) {
-            toggleFollow.setVisibility(View.VISIBLE);
-            toggleFollow.setChecked(user.isFollowedByMe());
-            toggleFollow.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    fireAndForget(followingOperations.toggleFollowing(user.getEntityUrn(), toggleFollow.isChecked()));
-                    engagementsTracking.followUserUrn(user.getEntityUrn(), toggleFollow.isChecked());
-                }
-            });
-        } else {
-            toggleFollow.setVisibility(View.GONE);
-        }
+        toggleFollow.setVisibility(View.VISIBLE);
+        toggleFollow.setChecked(user.isFollowedByMe());
+        toggleFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fireAndForget(followingOperations.toggleFollowing(user.getEntityUrn(), toggleFollow.isChecked()));
+                engagementsTracking.followUserUrn(user.getEntityUrn(), toggleFollow.isChecked());
+            }
+        });
     }
 }
