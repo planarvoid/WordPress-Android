c756ab738428ec8a0dec898c2ef0c6c959abf3b1
Matthias Kaeppler <mk@soundcloud.com>
2015-12-03 10:57:26 +0100

    Un-ignore acceptance test

--- app/src/androidTest/java/com/soundcloud/android/tests/discovery/SearchTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/discovery/SearchTest.java
@@ -75,7 +75,6 @@ public class SearchTest extends ActivityTest<MainActivity> {
         assertThat("Profile screen should be visible", profileScreen.isVisible());
     }
 
-    @Ignore
     public void testTappingUserOnPeopleTabOpensProfile() {
         ProfileScreen profileScreen = searchScreen.doSearch("emptyuser").goToPeopleTab().clickFirstUserItem();
 
