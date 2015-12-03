d7bfa84201b4f5fc202205cf2b5a4b1f4bacf512
Fernando Cejas <fcejas@gmail.com>
2015-12-03 11:20:56 +0100

    Add final to transition duration constants in TransitionUtils class.

--- app/src/main/java/com/soundcloud/android/utils/TransitionUtils.java
+++ app/src/main/java/com/soundcloud/android/utils/TransitionUtils.java
@@ -12,8 +12,8 @@ import android.view.animation.Interpolator;
 @SuppressLint("NewApi")
 public class TransitionUtils {
 
-    public static int ENTER_DURATION = 500;
-    public static int EXIT_DURATION = 200;
+    public static final int ENTER_DURATION = 500;
+    public static final int EXIT_DURATION = 200;
 
     public static void setChangeBoundsEnterTransition(Window window, int duration, Interpolator interpolator) {
         if (transitionsSupported()) {
