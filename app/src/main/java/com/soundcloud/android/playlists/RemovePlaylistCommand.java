ae334d63e4f49f264676068a0c81a5e1846f2834
Karolina Kafel <kr@soundcloud.com>
2016-01-18 15:43:57 +0100

    Remove associated activity when removing a playlist
    Fixes #4673

--- app/src/main/java/com/soundcloud/android/playlists/RemovePlaylistCommand.java
+++ app/src/main/java/com/soundcloud/android/playlists/RemovePlaylistCommand.java
@@ -24,12 +24,18 @@ public class RemovePlaylistCommand extends DefaultWriteStorageCommand<Urn, TxnRe
         return propeller.runTransaction(new PropellerDatabase.Transaction() {
             @Override
             public void steps(PropellerDatabase propeller) {
+
                 step(propeller.delete(Tables.OfflineContent.TABLE, filter()
                         .whereEq(Tables.OfflineContent._TYPE, Tables.OfflineContent.TYPE_PLAYLIST)
                         .whereEq(Tables.OfflineContent._ID, playlist.getNumericId())));
+
                 step(propeller.delete(Table.Sounds, filter()
                         .whereEq(TableColumns.Sounds._TYPE, TableColumns.Sounds.TYPE_PLAYLIST)
                         .whereEq(TableColumns.Sounds._ID, playlist.getNumericId())));
+
+                step(propeller.delete(Table.Activities, filter()
+                        .whereEq(TableColumns.Activities.SOUND_TYPE, TableColumns.Sounds.TYPE_PLAYLIST)
+                        .whereEq(TableColumns.Activities.SOUND_ID, playlist.getNumericId())));
             }
         });
     }
--- app/src/test/java/com/soundcloud/android/playlists/RemovePlaylistCommandTest.java
+++ app/src/test/java/com/soundcloud/android/playlists/RemovePlaylistCommandTest.java
@@ -2,14 +2,19 @@ package com.soundcloud.android.playlists;
 
 import static android.provider.BaseColumns._ID;
 import static com.soundcloud.android.storage.Table.Sounds;
+import static com.soundcloud.android.storage.TableColumns.Activities.SOUND_ID;
+import static com.soundcloud.android.storage.TableColumns.Activities.SOUND_TYPE;
 import static com.soundcloud.android.storage.TableColumns.ResourceTable._TYPE;
 import static com.soundcloud.android.storage.TableColumns.Sounds.TYPE_PLAYLIST;
 import static com.soundcloud.propeller.query.Query.from;
 import static com.soundcloud.propeller.test.assertions.QueryAssertions.assertThat;
 
 import com.soundcloud.android.api.model.ApiPlaylist;
+import com.soundcloud.android.storage.Table;
 import com.soundcloud.android.storage.Tables;
+import com.soundcloud.android.sync.activities.ApiPlaylistRepostActivity;
 import com.soundcloud.android.testsupport.StorageIntegrationTest;
+import com.soundcloud.android.testsupport.fixtures.ModelFixtures;
 import com.soundcloud.propeller.query.Query;
 import org.junit.Before;
 import org.junit.Test;
@@ -47,6 +52,21 @@ public class RemovePlaylistCommandTest extends StorageIntegrationTest {
         final Query query = from(Tables.OfflineContent.TABLE)
                 .whereEq(_ID, playlist.getId())
                 .whereEq(_TYPE, TYPE_PLAYLIST);
+
+        assertThat(select(query)).isEmpty();
+    }
+
+    @Test
+    public void removedActivitiesAssociatedWithRemovedPlaylist() {
+        final ApiPlaylist playlist = ModelFixtures.create(ApiPlaylist.class);
+        final ApiPlaylistRepostActivity apiActivityItem = ModelFixtures.apiPlaylistRepostActivity(playlist);
+        testFixtures().insertPlaylistRepostActivity(apiActivityItem);
+
+        command.call(playlist.getUrn());
+
+        final Query query = from(Table.Activities)
+                .whereEq(SOUND_ID, playlist.getId())
+                .whereEq(SOUND_TYPE, TYPE_PLAYLIST);
 
         assertThat(select(query)).isEmpty();
     }
