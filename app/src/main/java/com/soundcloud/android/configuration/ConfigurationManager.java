990dfd7774b5767fbe4b923b3c0ad95b2ba3ea60
Jamie McDonald <jdamcd@gmail.com>
2016-01-28 11:12:56 +0100

    Update device management logout
    Offline content is now cleared as part of the logout flow, so remove duplicate logic.

--- app/src/main/java/com/soundcloud/android/configuration/ConfigurationManager.java
+++ app/src/main/java/com/soundcloud/android/configuration/ConfigurationManager.java
@@ -1,12 +1,9 @@
 package com.soundcloud.android.configuration;
 
 import com.soundcloud.android.accounts.AccountOperations;
-import com.soundcloud.android.offline.OfflineContentOperations;
 import com.soundcloud.android.rx.RxUtils;
 import com.soundcloud.android.rx.observers.DefaultSubscriber;
-import rx.Observable;
 import rx.Subscription;
-import rx.functions.Func1;
 
 import android.util.Log;
 
@@ -17,25 +14,16 @@ public class ConfigurationManager {
     private static final String TAG = "Configuration";
 
     private final ConfigurationOperations configurationOperations;
-    private final OfflineContentOperations offlineContentOperations;
     private final AccountOperations accountOperations;
     private final DeviceManagementStorage deviceManagementStorage;
 
     private Subscription subscription = RxUtils.invalidSubscription();
 
-    private final Func1<Void, Observable<Void>> clearOfflineContent = new Func1<Void, Observable<Void>>() {
-        @Override
-        public Observable<Void> call(Void ignore) {
-            return offlineContentOperations.clearOfflineContent();
-        }
-    };
-
     @Inject
     public ConfigurationManager(ConfigurationOperations configurationOperations,
-                                OfflineContentOperations offlineContentOperations, AccountOperations accountOperations,
+                                AccountOperations accountOperations,
                                 DeviceManagementStorage deviceManagementStorage) {
         this.configurationOperations = configurationOperations;
-        this.offlineContentOperations = offlineContentOperations;
         this.accountOperations = accountOperations;
         this.deviceManagementStorage = deviceManagementStorage;
     }
@@ -61,7 +49,7 @@ public class ConfigurationManager {
             if (configuration.deviceManagement.isNotAuthorized()) {
                 Log.d(TAG, "Unauthorized device, logging out");
                 deviceManagementStorage.setDeviceConflict();
-                fireAndForget(accountOperations.logout().flatMap(clearOfflineContent));
+                fireAndForget(accountOperations.logout());
             } else {
                 configurationOperations.saveConfiguration(configuration);
             }
--- app/src/test/java/com/soundcloud/android/configuration/ConfigurationManagerTest.java
+++ app/src/test/java/com/soundcloud/android/configuration/ConfigurationManagerTest.java
@@ -1,6 +1,5 @@
 package com.soundcloud.android.configuration;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -9,7 +8,6 @@ import static org.mockito.Mockito.when;
 import com.soundcloud.android.accounts.AccountOperations;
 import com.soundcloud.android.configuration.experiments.Layer;
 import com.soundcloud.android.configuration.features.Feature;
-import com.soundcloud.android.offline.OfflineContentOperations;
 import com.soundcloud.android.testsupport.AndroidUnitTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,7 +21,6 @@ import java.util.Collections;
 public class ConfigurationManagerTest extends AndroidUnitTest {
 
     @Mock private ConfigurationOperations configurationOperations;
-    @Mock private OfflineContentOperations offlineContentOperations;
     @Mock private AccountOperations accountOperations;
     @Mock private DeviceManagementStorage deviceManagementStorage;
 
@@ -31,13 +28,13 @@ public class ConfigurationManagerTest extends AndroidUnitTest {
 
     @Before
     public void setUp() throws Exception {
-        manager = new ConfigurationManager(configurationOperations, offlineContentOperations, accountOperations, deviceManagementStorage);
+        manager = new ConfigurationManager(configurationOperations, accountOperations, deviceManagementStorage);
     }
 
     @Test
     public void updateWithAuthorizedDeviceResponseSavesConfiguration() {
         Configuration configuration = new Configuration(Collections.<Feature>emptyList(),
-                new UserPlan("free", Arrays.asList("mid_tier")), Collections.<Layer>emptyList(), new DeviceManagement(true, null));
+                new UserPlan("free", Arrays.asList("high_tier")), Collections.<Layer>emptyList(), new DeviceManagement(true, null));
         when(configurationOperations.update()).thenReturn(Observable.just(configuration));
 
         manager.update();
@@ -48,19 +45,16 @@ public class ConfigurationManagerTest extends AndroidUnitTest {
     @Test
     public void updateWithUnauthorizedDeviceResponseLogsOutAndClearsContent() {
         Configuration configurationWithDeviceConflict = new Configuration(Collections.<Feature>emptyList(),
-                new UserPlan("free", Arrays.asList("mid_tier")), Collections.<Layer>emptyList(), new DeviceManagement(false, null));
+                new UserPlan("free", Arrays.asList("high_tier")), Collections.<Layer>emptyList(), new DeviceManagement(false, null));
 
         when(configurationOperations.update()).thenReturn(Observable.just(configurationWithDeviceConflict));
 
         final PublishSubject<Void> logoutSubject = PublishSubject.create();
-        final PublishSubject<Void> clearOfflineContentSubject = PublishSubject.create();
         when(accountOperations.logout()).thenReturn(logoutSubject);
-        when(offlineContentOperations.clearOfflineContent()).thenReturn(clearOfflineContentSubject);
 
         manager.update();
 
         logoutSubject.onNext(null);
-        assertThat(clearOfflineContentSubject.hasObservers()).isTrue();
         verify(configurationOperations, never()).saveConfiguration(any(Configuration.class));
     }
 
