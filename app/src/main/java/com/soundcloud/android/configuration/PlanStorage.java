ae366adaae91c7afcc258595bf9377e51bff5666
Matthias Kaeppler <mk@soundcloud.com>
2016-02-08 11:26:58 +0100

    Some code cleanup around PlanStorage

--- app/src/main/java/com/soundcloud/android/configuration/PlanStorage.java
+++ app/src/main/java/com/soundcloud/android/configuration/PlanStorage.java
@@ -17,10 +17,22 @@ import java.util.List;
 
 public class PlanStorage {
 
-    private static final String PLAN = "plan";
-    private static final String UPSELLS = "upsells";
+    private static final String KEY_PLAN = "plan";
+    private static final String KEY_UPSELLS = "upsells";
+    private static final Func1<String, Boolean> UPSELLS_PREFERENCE = new Func1<String, Boolean>() {
+        @Override
+        public Boolean call(String key) {
+            return KEY_UPSELLS.equals(key);
+        }
+    };
 
     private final SharedPreferences sharedPreferences;
+    private final Func1<String, List<Plan>> toUpsells = new Func1<String, List<Plan>>() {
+        @Override
+        public List<Plan> call(String key) {
+            return getUpsells();
+        }
+    };
 
     @Inject
     public PlanStorage(@Named(StorageModule.FEATURES) SharedPreferences sharedPreferences) {
@@ -28,36 +40,26 @@ public class PlanStorage {
     }
 
     public void updatePlan(Plan plan) {
-        sharedPreferences.edit().putString(PLAN, plan.planId).apply();
+        sharedPreferences.edit().putString(KEY_PLAN, plan.planId).apply();
     }
 
     public void updateUpsells(List<Plan> plans) {
         Log.d(TAG, "updating upsells: " + plans);
-        sharedPreferences.edit().putStringSet(UPSELLS, Plan.toIds(plans)).apply();
+        sharedPreferences.edit().putStringSet(KEY_UPSELLS, Plan.toIds(plans)).apply();
     }
 
     public Plan getPlan() {
-        return Plan.fromId(sharedPreferences.getString(PLAN, Plan.FREE_TIER.planId));
+        return Plan.fromId(sharedPreferences.getString(KEY_PLAN, Plan.FREE_TIER.planId));
     }
 
     public List<Plan> getUpsells() {
-        return Plan.fromIds(sharedPreferences.getStringSet(UPSELLS, Collections.<String>emptySet()));
+        return Plan.fromIds(sharedPreferences.getStringSet(KEY_UPSELLS, Collections.<String>emptySet()));
     }
 
     public Observable<List<Plan>> getUpsellUpdates() {
         return Observable.create(new PreferenceChangeOnSubscribe(sharedPreferences))
-                .filter(new Func1<String, Boolean>() {
-                    @Override
-                    public Boolean call(String s) {
-                        return s.equals(UPSELLS);
-                    }
-                })
-                .map(new Func1<String, List<Plan>>() {
-                    @Override
-                    public List<Plan> call(String s) {
-                        return getUpsells();
-                    }
-                });
+                .filter(UPSELLS_PREFERENCE)
+                .map(toUpsells);
     }
 
     public void clear() {
