9c1c7622b0a613918fdf5e526a259b14d4ec7e13
jonschmidt <jon@soundcloud.com>
2016-02-19 18:03:55 +0100

    implement parcelable in preloadItem
    Should fix broken acceptance tests and preloading

--- app/src/main/java/com/soundcloud/android/playback/PreloadItem.java
+++ app/src/main/java/com/soundcloud/android/playback/PreloadItem.java
@@ -3,8 +3,10 @@ package com.soundcloud.android.playback;
 import auto.parcel.AutoParcel;
 import com.soundcloud.android.model.Urn;
 
+import android.os.Parcelable;
+
 @AutoParcel
-public abstract class PreloadItem {
+public abstract class PreloadItem implements Parcelable {
 
     public abstract Urn getUrn();
 
