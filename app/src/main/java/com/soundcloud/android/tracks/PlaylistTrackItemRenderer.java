52303a921af436e6273cfb8222a235dee00e1574
Horaci Cuevas <horaci@gmail.com>
2016-02-23 16:05:33 +0100

    Restore separators for blocked tracks in playlist details

--- app/src/main/java/com/soundcloud/android/tracks/PlaylistTrackItemRenderer.java
+++ app/src/main/java/com/soundcloud/android/tracks/PlaylistTrackItemRenderer.java
@@ -7,14 +7,15 @@ import com.soundcloud.android.analytics.PromotedSourceInfo;
 import com.soundcloud.android.analytics.ScreenProvider;
 import com.soundcloud.android.configuration.FeatureOperations;
 import com.soundcloud.android.image.ImageOperations;
+import com.soundcloud.android.model.Urn;
 import com.soundcloud.android.util.CondensedNumberFormatter;
 import com.soundcloud.rx.eventbus.EventBus;
-import com.soundcloud.android.model.Urn;
 
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class PlaylistTrackItemRenderer extends DownloadableTrackItemRenderer {
 
@@ -44,5 +45,20 @@ public class PlaylistTrackItemRenderer extends DownloadableTrackItemRenderer {
     protected void showTrackItemMenu(View button, TrackItem track, int position) {
         trackItemMenuPresenter.show((FragmentActivity) button.getContext(), button, track, position, pageUrn,
                 removeTrackListener, promotedSourceInfo);
+    }
+
+    @Override
+    public void bindItemView(int position, View itemView, List<TrackItem> trackItems) {
+        super.bindItemView(position, itemView, trackItems);
+        disableBlockedTrackClicks(itemView, trackItems.get(position));
+    }
+
+    private void disableBlockedTrackClicks(View itemView, TrackItem track) {
+        if (track.isBlocked()) {
+            // note: TrackItemRenderer already calls `setClickable(false)` but this doesn't appear
+            // to work for the ListView widget (it's still clickable, and shows ripples on touch)
+            // http://stackoverflow.com/questions/4636270/android-listview-child-view-setenabled-and-setclickable-do-nothing
+            itemView.setOnClickListener(null);
+        }
     }
 }
