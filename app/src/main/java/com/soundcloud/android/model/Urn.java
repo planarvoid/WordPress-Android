13d89e98c2afa9aa00e246c80458dade464fae81
Guillaume Lung <guillaume@soundcloud.com>
2016-05-31 17:55:39 +0200

    Explicitly add the artist stations urn as part of the stations Urn.
    Fix : 
    - Save artist stations to recent stations. 
    - Display the station context in the player.

--- app/src/main/java/com/soundcloud/android/model/Urn.java
+++ app/src/main/java/com/soundcloud/android/model/Urn.java
@@ -112,7 +112,9 @@ public final class Urn extends ContentStringHelper<Urn> {
 
     public boolean isStation() {
         return isSoundCloud()
-                && (collection == UrnCollection.STATIONS || collection == UrnCollection.TRACK_STATIONS);
+                && (collection == UrnCollection.STATIONS
+                || collection == UrnCollection.TRACK_STATIONS
+                || collection == UrnCollection.ARTIST_STATIONS);
     }
 
     public boolean isAd() {
