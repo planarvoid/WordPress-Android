a436bd9f0a8b3ca6dd51c99bce734ebab0a57d28
Guillaume Lung <guillaume@soundcloud.com>
2016-06-01 16:52:54 +0200

    Remove dead code.

--- app/src/main/java/com/soundcloud/android/api/legacy/Endpoints.java
+++ app/src/main/java/com/soundcloud/android/api/legacy/Endpoints.java
@@ -9,22 +9,12 @@ public interface Endpoints {
     String TOKEN = "/oauth2/token";
 
     String TRACKS = "/tracks";
-    String TRACK_DETAILS = "/tracks/%d";
-    String TRACK_FAVORITERS = "/tracks/%d/favoriters";
 
     String PLAYLISTS = "/playlists";
 
-    String USERS = "/users";
-    String USER_DETAILS = "/users/%d";
-    String USER_FOLLOWINGS = "/users/%d/followings";
-    String USER_FOLLOWERS = "/users/%d/followers";
-
     String MY_DETAILS = "/me";
-    String MY_FOLLOWERS = "/me/followers";
     String MY_FOLLOWINGS = "/me/followings";
     String MY_FOLLOWING = "/me/followings/%d";
-
-    String SUGGESTED_USERS = "/users/suggested";
 
     String RESOLVE = "/resolve";
 
