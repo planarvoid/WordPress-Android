07d908bb7b2aa69f2df6c744e7d083869108cd4c
Fernando Cejas <fcejas@gmail.com>
2016-06-23 12:03:47 +0200

    Kill legacy robolectric unit tests.

--- tests-robolectric/src/test/java/com/soundcloud/android/api/legacy/RequestMatcher.java
+++
@@ -1,7 +0,0 @@
-package com.soundcloud.android.api.legacy;
-
-import org.apache.http.HttpRequest;
-
-public interface RequestMatcher {
-    public boolean matches(HttpRequest request);
-}
--- tests-robolectric/src/test/java/com/soundcloud/android/robolectric/shadows/ShadowBaseBundle.java
+++
@@ -1,9 +0,0 @@
-package com.soundcloud.android.robolectric.shadows;
-
-
-import com.xtremelabs.robolectric.internal.Implements;
-import com.xtremelabs.robolectric.shadows.ShadowBundle;
-
-@Implements(android.os.BaseBundle.class)
-public class ShadowBaseBundle extends ShadowBundle {
-}
