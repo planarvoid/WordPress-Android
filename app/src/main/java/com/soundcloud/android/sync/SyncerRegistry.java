80e953f837234cf2a9140fa8e1a33da251265389
Fernando Cejas <fcejas@gmail.com>
2016-07-20 15:47:23 +0200

    Add documentation to SyncerRegistry class.

--- app/src/main/java/com/soundcloud/android/sync/SyncerRegistry.java
+++ app/src/main/java/com/soundcloud/android/sync/SyncerRegistry.java
@@ -48,7 +48,8 @@ public class SyncerRegistry {
     }
 
     /**
-     * TODO
+     * A SyncProvider is an abstraction consisting of setup methods
+     * for the data syncing job.
      */
     public static abstract class SyncProvider {
         private final Syncable syncable;
@@ -62,26 +63,26 @@ public class SyncerRegistry {
         }
 
         /**
-         * TODO
-         * @return
+         * Provides a {@link Callable} that will perform the syncing task.
          */
         public abstract Callable<Boolean> syncer();
 
         /**
-         * TODO
-         * @return
+         * Provides a value that tells the syncing service when the data
+         * is out of synchronization in order to schedule a sync job.
          */
         public abstract Boolean isOutOfSync();
 
         /**
-         * TODO
-         * @return
+         * Returns a value that tells the syncer how often the data should be synced.
+         * It has no effect if {@link SyncProvider#usePeriodicSync()} is False.
          */
         public abstract long staleTime();
 
         /**
-         * TODO
-         * @return
+         * Provides a value that tells the syncing service whether should
+         * use periodic sync schedules: syncing time configuration is
+         * returned by {@link SyncProvider#staleTime()} method.
          */
         public abstract boolean usePeriodicSync();
     }
