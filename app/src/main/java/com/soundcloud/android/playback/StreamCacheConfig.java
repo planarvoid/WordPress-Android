bbeebeebde77b2a2d0639284b8a7a50f87955f31
Horaci Cuevas <horaci@gmail.com>
2016-09-07 17:00:49 +0200

    Revert "Adjust cache size" and increase MIN_SIZE_BYTES
    This reverts commit 72dc56971a9fad2c9cee742fb4694f3af1e2ee0e.

--- app/src/main/java/com/soundcloud/android/playback/StreamCacheConfig.java
+++ app/src/main/java/com/soundcloud/android/playback/StreamCacheConfig.java
@@ -2,32 +2,48 @@ package com.soundcloud.android.playback;
 
 import com.soundcloud.android.storage.StorageModule;
 import com.soundcloud.android.utils.IOUtils;
+import com.soundcloud.android.utils.TelphonyBasedCountryProvider;
 import com.soundcloud.annotations.VisibleForTesting;
+import com.soundcloud.java.strings.Strings;
 
 import android.support.annotation.Nullable;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.File;
+import java.util.Locale;
 
 public class StreamCacheConfig {
 
     @VisibleForTesting
-    static final long SIZE_BYTES = 500 * 1024 * 1024; // 500MB
+    static final long MIN_SIZE_BYTES = 120 * 1024 * 1024; // 120MB
+    static final long MAX_SIZE_BYTES = 500 * 1024 * 1024; // 500MB
     static final int STREAM_CACHE_MIN_FREE_SPACE_AVAILABLE_PERCENTAGE = 1;
 
+    private final TelphonyBasedCountryProvider countryProvider;
     @Nullable
     private final File streamCacheDirectory;
     private final IOUtils ioUtils;
 
     @Inject
-    public StreamCacheConfig(@Named(StorageModule.STREAM_CACHE_DIRECTORY) File streamCacheDirectory, IOUtils ioUtils) {
+    public StreamCacheConfig(TelphonyBasedCountryProvider countryProvider,
+                             @Named(StorageModule.STREAM_CACHE_DIRECTORY) File streamCacheDirectory, IOUtils ioUtils) {
+        this.countryProvider = countryProvider;
         this.streamCacheDirectory = streamCacheDirectory;
         this.ioUtils = ioUtils;
     }
 
     public long getStreamCacheSize() {
-        return SIZE_BYTES;
+        final String countryCode = countryProvider.getCountryCode();
+        if (Strings.isBlank(countryCode)
+                || Locale.US.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.GERMANY.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.FRANCE.getCountry().equalsIgnoreCase(countryCode)
+                || Locale.UK.getCountry().equalsIgnoreCase(countryCode)) {
+            return MIN_SIZE_BYTES;
+        } else {
+            return MAX_SIZE_BYTES;
+        }
     }
 
     @Nullable
