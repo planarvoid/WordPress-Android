9b9ed0713a8584565a6ae7731c029704ecc4c6c5
jonschmidt <jon@soundcloud.com>
2016-09-15 11:46:09 +0200

    Remove unused posts storage operations (#6029)

--- app/src/main/java/com/soundcloud/android/profile/PostsStorage.java
+++ app/src/main/java/com/soundcloud/android/profile/PostsStorage.java
@@ -1,79 +1,34 @@
 package com.soundcloud.android.profile;
 
-import static com.soundcloud.android.storage.Table.Likes;
 import static com.soundcloud.android.storage.Table.Posts;
 import static com.soundcloud.android.storage.Table.SoundView;
-import static com.soundcloud.android.storage.Table.Users;
-import static com.soundcloud.android.storage.TableColumns.PlaylistTracks.PLAYLIST_ID;
-import static com.soundcloud.propeller.query.ColumnFunctions.count;
 import static com.soundcloud.propeller.query.Field.field;
-import static com.soundcloud.propeller.query.Filter.filter;
 import static com.soundcloud.propeller.query.Query.Order.DESC;
 import static com.soundcloud.propeller.query.Query.on;
 
-import com.soundcloud.android.accounts.AccountOperations;
 import com.soundcloud.android.api.model.Sharing;
-import com.soundcloud.android.model.PlayableProperty;
 import com.soundcloud.android.model.PostProperty;
 import com.soundcloud.android.model.Urn;
-import com.soundcloud.android.playlists.PlaylistMapper;
-import com.soundcloud.android.playlists.PlaylistProperty;
-import com.soundcloud.android.storage.Table;
 import com.soundcloud.android.storage.TableColumns;
 import com.soundcloud.android.tracks.TrackProperty;
 import com.soundcloud.java.collections.PropertySet;
-import com.soundcloud.java.optional.Optional;
-import com.soundcloud.java.strings.Strings;
 import com.soundcloud.propeller.CursorReader;
 import com.soundcloud.propeller.query.Query;
-import com.soundcloud.propeller.query.Where;
 import com.soundcloud.propeller.rx.PropellerRx;
 import com.soundcloud.propeller.rx.RxResultMapper;
 import rx.Observable;
-import rx.functions.Func2;
 
 import android.provider.BaseColumns;
-import android.support.annotation.NonNull;
 
 import javax.inject.Inject;
-import java.util.List;
 
 public class PostsStorage {
 
     private final PropellerRx propellerRx;
-    private final AccountOperations accountOperations;
-
-    private final static String LIKED_ID = "liked_id";
-
-    private static final Func2<List<PropertySet>, String, List<PropertySet>> COMBINE_REPOSTER = new Func2<List<PropertySet>, String, List<PropertySet>>() {
-        @Override
-        public List<PropertySet> call(List<PropertySet> propertySets, String username) {
-            for (PropertySet propertySet : propertySets) {
-                if (propertySet.getOrElse(PlayableProperty.IS_USER_REPOST, false)) {
-                    propertySet.put(PostProperty.REPOSTER, username);
-                }
-            }
-            return propertySets;
-        }
-    };
 
     @Inject
-    public PostsStorage(PropellerRx propellerRx, AccountOperations accountOperations) {
+    public PostsStorage(PropellerRx propellerRx) {
         this.propellerRx = propellerRx;
-        this.accountOperations = accountOperations;
-    }
-
-    Observable<List<PropertySet>> loadPostsForPlayback() {
-        return propellerRx.query(buildQueryForPlayback()).map(new PostsForPlaybackMapper()).toList();
-    }
-
-    Observable<List<PropertySet>> loadPosts(int limit, long fromTimestamp) {
-        return Observable.zip(
-                propellerRx.query(buildPostsQuery(limit, fromTimestamp)).map(new PostsMapper()).toList(),
-                propellerRx.query(buildUserQuery()).map(RxResultMapper.scalar(String.class))
-                           .firstOrDefault(Strings.EMPTY),
-                COMBINE_REPOSTER
-        );
     }
 
     public Observable<PropertySet> loadLastPublicPostedTrack() {
@@ -97,164 +52,6 @@ public class PostsStorage {
                     .groupBy(SoundView.field(TableColumns.SoundView._ID) + "," + SoundView.field(TableColumns.SoundView._TYPE))
                     .order(Posts.field(TableColumns.Posts.CREATED_AT), DESC)
                     .limit(1);
-    }
-
-    private Query buildQueryForPlayback() {
-        return Query.from(Posts.name())
-                    .select(
-                            field(SoundView.field(TableColumns.SoundView._TYPE)).as(TableColumns.SoundView._TYPE),
-                            field(SoundView.field(TableColumns.SoundView._ID)).as(TableColumns.SoundView._ID),
-                            field(Posts.field(TableColumns.Posts.TYPE)).as(TableColumns.Posts.TYPE))
-                    .innerJoin(SoundView.name(),
-                               on(SoundView.field(TableColumns.SoundView._ID),
-                                  Posts.field(TableColumns.Posts.TARGET_ID))
-                                       .whereEq(SoundView.field(TableColumns.SoundView._TYPE),
-                                                Posts.field(TableColumns.Posts.TARGET_TYPE)))
-                    .groupBy(SoundView.field(TableColumns.SoundView._ID) + "," + SoundView.field(TableColumns.SoundView._TYPE))
-                    .order(Table.Posts.field(TableColumns.Posts.CREATED_AT), DESC);
-    }
-
-    private Query buildPostsQuery(int limit, long fromTimestamp) {
-        return Query.from(Posts.name())
-                    .select(
-                            field(SoundView.field(TableColumns.SoundView._TYPE)).as(TableColumns.SoundView._TYPE),
-                            field(SoundView.field(TableColumns.SoundView._ID)).as(TableColumns.SoundView._ID),
-                            field(SoundView.field(TableColumns.SoundView.TITLE)).as(TableColumns.SoundView.TITLE),
-                            field(SoundView.field(TableColumns.SoundView.ARTWORK_URL)).as(TableColumns.SoundView.ARTWORK_URL),
-                            field(SoundView.field(TableColumns.SoundView.USERNAME)).as(TableColumns.SoundView.USERNAME),
-                            field(SoundView.field(TableColumns.SoundView.TRACK_COUNT)).as(TableColumns.SoundView.TRACK_COUNT),
-                            field(SoundView.field(TableColumns.SoundView.LIKES_COUNT)).as(TableColumns.SoundView.LIKES_COUNT),
-                            field(SoundView.field(TableColumns.SoundView.SHARING)).as(TableColumns.SoundView.SHARING),
-                            field(SoundView.field(TableColumns.SoundView.SNIPPET_DURATION)).as(TableColumns.SoundView.SNIPPET_DURATION),
-                            field(SoundView.field(TableColumns.SoundView.FULL_DURATION)).as(TableColumns.SoundView.FULL_DURATION),
-                            field(SoundView.field(TableColumns.SoundView.PLAYBACK_COUNT)).as(TableColumns.SoundView.PLAYBACK_COUNT),
-                            field(SoundView.field(TableColumns.SoundView.POLICIES_BLOCKED)).as(TableColumns.SoundView.POLICIES_BLOCKED),
-                            field(SoundView.field(TableColumns.SoundView.POLICIES_SNIPPED)).as(TableColumns.SoundView.POLICIES_SNIPPED),
-                            field(SoundView.field(TableColumns.SoundView.POLICIES_SUB_MID_TIER)).as(TableColumns.SoundView.POLICIES_SUB_MID_TIER),
-                            field(SoundView.field(TableColumns.SoundView.POLICIES_SUB_HIGH_TIER)).as(TableColumns.SoundView.POLICIES_SUB_HIGH_TIER),
-                            field(Posts.field(TableColumns.Posts.TYPE)).as(TableColumns.Posts.TYPE),
-                            field(Posts.field(TableColumns.Posts.CREATED_AT)).as(TableColumns.Posts.CREATED_AT),
-                            field(Likes.field(TableColumns.Likes._ID)).as(LIKED_ID),
-                            count(PLAYLIST_ID).as(PlaylistMapper.LOCAL_TRACK_COUNT))
-                    .innerJoin(SoundView.name(),
-                               on(SoundView.field(TableColumns.SoundView._ID),
-                                  Posts.field(TableColumns.Posts.TARGET_ID))
-                                       .whereEq(SoundView.field(TableColumns.SoundView._TYPE),
-                                                Posts.field(TableColumns.Posts.TARGET_TYPE)))
-                    .leftJoin(Table.PlaylistTracks.name(), playlistTracksFilter())
-                    .leftJoin(Likes.name(),
-                              on(SoundView.field(TableColumns.SoundView._ID), Likes.field(TableColumns.Likes._ID))
-                                      .whereEq(SoundView.field(TableColumns.SoundView._TYPE),
-                                               Likes.field(TableColumns.Likes._TYPE)))
-                    .whereLt(Posts.field(TableColumns.Posts.CREATED_AT), fromTimestamp)
-                    .groupBy(SoundView.field(TableColumns.SoundView._ID) + "," + SoundView.field(TableColumns.SoundView._TYPE))
-                    .order(Table.Posts.field(TableColumns.Posts.CREATED_AT), DESC)
-                    .limit(limit);
-    }
-
-    private Query buildUserQuery() {
-        return Query.from(Users.name())
-                    .select(field(Users.field(TableColumns.Users.USERNAME)))
-                    .whereEq(Users.field(TableColumns.Users._ID),
-                             accountOperations.getLoggedInUserUrn().getNumericId());
-    }
-
-    @NonNull
-    private Where playlistTracksFilter() {
-        return filter()
-                .whereEq(SoundView.field(TableColumns.SoundView._TYPE), TableColumns.Sounds.TYPE_PLAYLIST)
-                .whereEq(SoundView.field(TableColumns.SoundView._ID), PLAYLIST_ID);
-    }
-
-    private static class PostsMapper extends RxResultMapper<PropertySet> {
-
-        private PostedPlaylistMapper playlistMapper = new PostedPlaylistMapper();
-        private PostedTracksMapper postedTracksMapper = new PostedTracksMapper();
-
-        @Override
-        public PropertySet map(CursorReader reader) {
-            if (reader.getInt(TableColumns.SoundView._TYPE) == TableColumns.Sounds.TYPE_TRACK) {
-                return postedTracksMapper.map(reader);
-            } else {
-                return playlistMapper.map(reader);
-            }
-        }
-    }
-
-    private static class PostedPlaylistMapper extends RxResultMapper<PropertySet> {
-
-        @Override
-        public PropertySet map(CursorReader cursorReader) {
-            final PropertySet propertySet = PropertySet.create(cursorReader.getColumnCount());
-            propertySet.put(PlaylistProperty.URN, Urn.forPlaylist(cursorReader.getLong(BaseColumns._ID)));
-            propertySet.put(PlaylistProperty.TITLE, cursorReader.getString(TableColumns.SoundView.TITLE));
-            propertySet.put(PlaylistProperty.IMAGE_URL_TEMPLATE, Optional.fromNullable(
-                    cursorReader.getString(TableColumns.SoundView.ARTWORK_URL)));
-            propertySet.put(PlaylistProperty.CREATOR_NAME, cursorReader.getString(TableColumns.SoundView.USERNAME));
-            propertySet.put(PlaylistProperty.TRACK_COUNT, readTrackCount(cursorReader));
-            propertySet.put(PlaylistProperty.LIKES_COUNT, cursorReader.getInt(TableColumns.SoundView.LIKES_COUNT));
-            propertySet.put(PlaylistProperty.IS_PRIVATE,
-                            Sharing.PRIVATE.name()
-                                           .equalsIgnoreCase(cursorReader.getString(TableColumns.SoundView.SHARING)));
-            propertySet.put(PlayableProperty.IS_USER_LIKE, cursorReader.isNotNull(LIKED_ID));
-            propertySet.put(PlayableProperty.IS_USER_REPOST,
-                            TableColumns.Posts.TYPE_REPOST.equals(cursorReader.getString(TableColumns.Posts.TYPE)));
-            propertySet.put(PostProperty.CREATED_AT, cursorReader.getDateFromTimestamp(TableColumns.Posts.CREATED_AT));
-            return propertySet;
-        }
-
-        int readTrackCount(CursorReader cursorReader) {
-            return Math.max(cursorReader.getInt(PlaylistMapper.LOCAL_TRACK_COUNT),
-                            cursorReader.getInt(TableColumns.SoundView.TRACK_COUNT));
-        }
-    }
-
-    private static class PostedTracksMapper extends RxResultMapper<PropertySet> {
-        @Override
-        public PropertySet map(CursorReader cursorReader) {
-            final PropertySet propertySet = PropertySet.create(cursorReader.getColumnCount());
-            propertySet.put(TrackProperty.URN, Urn.forTrack(cursorReader.getLong(BaseColumns._ID)));
-            propertySet.put(TrackProperty.TITLE, cursorReader.getString(TableColumns.SoundView.TITLE));
-            propertySet.put(TrackProperty.IMAGE_URL_TEMPLATE, Optional.fromNullable(
-                    cursorReader.getString(TableColumns.SoundView.ARTWORK_URL)));
-            propertySet.put(TrackProperty.CREATOR_NAME, cursorReader.getString(TableColumns.SoundView.USERNAME));
-            propertySet.put(TrackProperty.SNIPPET_DURATION,
-                            cursorReader.getLong(TableColumns.SoundView.SNIPPET_DURATION));
-            propertySet.put(TrackProperty.FULL_DURATION, cursorReader.getLong(TableColumns.SoundView.FULL_DURATION));
-            propertySet.put(TrackProperty.PLAY_COUNT, cursorReader.getInt(TableColumns.SoundView.PLAYBACK_COUNT));
-            propertySet.put(TrackProperty.LIKES_COUNT, cursorReader.getInt(TableColumns.SoundView.LIKES_COUNT));
-            propertySet.put(TrackProperty.IS_PRIVATE,
-                            Sharing.PRIVATE.name()
-                                           .equalsIgnoreCase(cursorReader.getString(TableColumns.SoundView.SHARING)));
-            propertySet.put(PlayableProperty.IS_USER_LIKE, cursorReader.isNotNull(LIKED_ID));
-            propertySet.put(PlayableProperty.IS_USER_REPOST,
-                            TableColumns.Posts.TYPE_REPOST.equals(cursorReader.getString(TableColumns.Posts.TYPE)));
-            propertySet.put(PostProperty.CREATED_AT, cursorReader.getDateFromTimestamp(TableColumns.Posts.CREATED_AT));
-            propertySet.put(TrackProperty.BLOCKED, cursorReader.getBoolean(TableColumns.SoundView.POLICIES_BLOCKED));
-            propertySet.put(TrackProperty.SNIPPED, cursorReader.getBoolean(TableColumns.SoundView.POLICIES_SNIPPED));
-            propertySet.put(TrackProperty.SUB_MID_TIER,
-                            cursorReader.getBoolean(TableColumns.SoundView.POLICIES_SUB_MID_TIER));
-            propertySet.put(TrackProperty.SUB_HIGH_TIER,
-                            cursorReader.getBoolean(TableColumns.SoundView.POLICIES_SUB_HIGH_TIER));
-            return propertySet;
-        }
-    }
-
-    private class PostsForPlaybackMapper extends RxResultMapper<PropertySet> {
-
-        @Override
-        public PropertySet map(CursorReader cursorReader) {
-            final PropertySet propertySet = PropertySet.create(cursorReader.getColumnCount());
-            if (cursorReader.getInt(TableColumns.SoundView._TYPE) == TableColumns.Sounds.TYPE_TRACK) {
-                propertySet.put(TrackProperty.URN, Urn.forTrack(cursorReader.getLong(BaseColumns._ID)));
-            } else {
-                propertySet.put(PlaylistProperty.URN, Urn.forPlaylist(cursorReader.getLong(BaseColumns._ID)));
-            }
-            if (TableColumns.Posts.TYPE_REPOST.equals(cursorReader.getString(TableColumns.Posts.TYPE))) {
-                propertySet.put(PostProperty.REPOSTER_URN, accountOperations.getLoggedInUserUrn());
-            }
-            return propertySet;
-        }
     }
 
     private class LastPostedTrackMapper extends RxResultMapper<PropertySet> {
--- app/src/test/java/com/soundcloud/android/profile/PostsStorageTest.java
+++ app/src/test/java/com/soundcloud/android/profile/PostsStorageTest.java
@@ -1,17 +1,11 @@
 package com.soundcloud.android.profile;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.soundcloud.android.accounts.AccountOperations;
-import com.soundcloud.android.api.model.ApiPlaylist;
 import com.soundcloud.android.api.model.ApiTrack;
 import com.soundcloud.android.api.model.ApiUser;
-import com.soundcloud.android.model.PlayableProperty;
 import com.soundcloud.android.model.PostProperty;
-import com.soundcloud.android.model.Urn;
-import com.soundcloud.android.playlists.PlaylistProperty;
-import com.soundcloud.android.sync.likes.ApiLike;
 import com.soundcloud.android.testsupport.StorageIntegrationTest;
 import com.soundcloud.android.tracks.TrackProperty;
 import com.soundcloud.java.collections.PropertySet;
@@ -20,8 +14,6 @@ import org.junit.Test;
 import org.mockito.Mock;
 import rx.observers.TestSubscriber;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -29,15 +21,11 @@ public class PostsStorageTest extends StorageIntegrationTest {
 
     private static final Date POSTED_DATE_1 = new Date(100000);
     private static final Date POSTED_DATE_2 = new Date(200000);
-    private static final Date POSTED_DATE_3 = new Date(300000);
-    private static final Date POSTED_DATE_4 = new Date(400000);
 
     private PostsStorage storage;
     private ApiUser user;
     private PropertySet post1;
     private PropertySet post2;
-    private PropertySet post3;
-    private PropertySet post4;
 
     final TestSubscriber<List<PropertySet>> subscriber = new TestSubscriber<>();
 
@@ -47,104 +35,9 @@ public class PostsStorageTest extends StorageIntegrationTest {
     public void setUp() {
         user = testFixtures().insertUser();
 
-        storage = new PostsStorage(propellerRx(), accountOperations);
+        storage = new PostsStorage(propellerRx());
 
         when(accountOperations.getLoggedInUserUrn()).thenReturn(user.getUrn());
-    }
-
-    @Test
-    public void shouldLoadAllPostsForPlayback() {
-        post1 = createPlaylistPostAt(POSTED_DATE_1);
-        post2 = createTrackPostAt(POSTED_DATE_2);
-        post3 = createTrackRepostAt(POSTED_DATE_3);
-        post4 = createPlaylistRepostAt(POSTED_DATE_4);
-
-        storage.loadPostsForPlayback().subscribe(subscriber);
-
-        subscriber.assertValue(
-                Arrays.asList(
-                        post4.slice(PlaylistProperty.URN).put(PostProperty.REPOSTER_URN, user.getUrn()),
-                        post3.slice(TrackProperty.URN).put(PostProperty.REPOSTER_URN, user.getUrn()),
-                        post2.slice(TrackProperty.URN),
-                        post1.slice(TrackProperty.URN))
-        );
-    }
-
-    @Test
-    public void shouldLoadAllPosts() {
-        post1 = createPlaylistPostAt(POSTED_DATE_1);
-        post2 = createTrackPostAt(POSTED_DATE_2);
-        post3 = createTrackRepostAt(POSTED_DATE_3);
-        post4 = createPlaylistRepostAt(POSTED_DATE_4);
-
-        storage.loadPosts(10, Long.MAX_VALUE).subscribe(subscriber);
-
-        subscriber.assertValue(Arrays.asList(post4, post3, post2, post1));
-    }
-
-    @Test
-    public void shouldReturnTrackCountAsMaximumOfRemoteAndLocalCounts() {
-        post1 = createPlaylistPostAt(POSTED_DATE_1);
-        post2 = createPlaylistRepostAt(POSTED_DATE_2);
-        assertThat(post1.get(PlaylistProperty.TRACK_COUNT)).isEqualTo(2);
-
-        final Urn playlistUrn = post1.get(PlaylistProperty.URN);
-        testFixtures().insertPlaylistTrack(playlistUrn, 0);
-        testFixtures().insertPlaylistTrack(playlistUrn, 1);
-        testFixtures().insertPlaylistTrack(playlistUrn, 2);
-
-        storage.loadPosts(10, Long.MAX_VALUE).subscribe(subscriber);
-
-        final List<PropertySet> result = subscriber.getOnNextEvents().get(0);
-        assertThat(result.get(1).get(PlaylistProperty.URN)).isEqualTo(playlistUrn);
-        assertThat(result.get(1).get(PlaylistProperty.TRACK_COUNT)).isEqualTo(3);
-    }
-
-    @Test
-    public void shouldAdhereToLimit() {
-        post1 = createTrackPostAt(POSTED_DATE_1);
-        post2 = createPlaylistPostAt(POSTED_DATE_2);
-        storage.loadPosts(1, Long.MAX_VALUE).subscribe(subscriber);
-
-        subscriber.assertValue(Collections.singletonList(post2));
-    }
-
-    @Test
-    public void shouldAdhereToPostedTime() throws Exception {
-        post1 = createTrackPostAt(POSTED_DATE_2);
-        post2 = createPlaylistPostAt(POSTED_DATE_1);
-
-        // 2 old items, reposted after the above tracks
-        post3 = createTrackRepostAt(POSTED_DATE_4, POSTED_DATE_1);
-        post4 = createPlaylistRepostAt(POSTED_DATE_4, POSTED_DATE_1);
-
-        storage.loadPosts(2, POSTED_DATE_3.getTime()).subscribe(subscriber);
-
-        subscriber.assertValue(Arrays.asList(post1, post2));
-    }
-
-    @Test
-    public void shouldIncludeLikeStatusInPlaylistPosts() throws Exception {
-        post1 = createPlaylistPostAt(POSTED_DATE_1).put(PlayableProperty.IS_USER_LIKE, true);
-        post2 = createPlaylistRepostAt(POSTED_DATE_2).put(PlayableProperty.IS_USER_LIKE, true);
-
-        testFixtures().insertLike(new ApiLike(post1.get(PlaylistProperty.URN), new Date()));
-        testFixtures().insertLike(new ApiLike(post2.get(PlaylistProperty.URN), new Date()));
-
-        storage.loadPosts(2, Long.MAX_VALUE).subscribe(subscriber);
-
-        subscriber.assertValue(Arrays.asList(post2, post1));
-    }
-
-    @Test
-    public void shouldNotIncludePlaylistsNotPresentInThePostsTable() throws Exception {
-        post1 = createTrackPostAt(POSTED_DATE_1);
-        post2 = createTrackRepostAt(POSTED_DATE_2);
-        createTrackAt(POSTED_DATE_3);
-
-        storage.loadPosts(10, Long.MAX_VALUE).subscribe(subscriber);
-
-        subscriber.assertValue(Arrays.asList(post2, post1));
     }
 
     @Test
@@ -169,97 +62,12 @@ public class PostsStorageTest extends StorageIntegrationTest {
         subscriber.assertValue(post2);
     }
 
-    private PropertySet createTrackPostAt(Date postedAt) {
-        ApiTrack track = createTrackAt(postedAt);
-        createTrackPostWithId(track.getUrn().getNumericId(), postedAt);
-        return createTrackPostPropertySet(track);
-    }
-
-    private PropertySet createTrackRepostAt(Date postedAt) {
-        return createTrackRepostAt(postedAt, postedAt);
-    }
-
-    private PropertySet createTrackRepostAt(Date postedAt, Date createdAt) {
-        ApiTrack track = createTrackAt(createdAt);
-        createTrackRepostWithId(track.getUrn().getNumericId(), postedAt);
-        return createTrackPostPropertySet(track).put(TrackProperty.IS_USER_REPOST, true)
-                                                .put(PostProperty.REPOSTER, user.getUsername());
-    }
-
-    private PropertySet createPlaylistPostAt(Date postedAt) {
-        ApiPlaylist playlist = createPlaylistAt(postedAt);
-        createPlaylistPostWithId(playlist.getUrn().getNumericId(), postedAt);
-        return createPlaylistPostPropertySet(playlist);
-    }
-
-    private PropertySet createPlaylistRepostAt(Date postedAt) {
-        return createPlaylistRepostAt(postedAt, postedAt);
-    }
-
-    private PropertySet createPlaylistRepostAt(Date postedAt, Date createdAt) {
-        ApiPlaylist playlist = createPlaylistAt(createdAt);
-        createPlaylistRepostWithId(playlist.getUrn().getNumericId(), postedAt);
-        return createPlaylistPostPropertySet(playlist)
-                .put(TrackProperty.IS_USER_REPOST, true)
-                .put(PostProperty.REPOSTER, user.getUsername());
-    }
-
-    private PropertySet createPlaylistPostPropertySet(ApiPlaylist playlist) {
-        return playlist.toPropertySet().slice(
-                PlaylistProperty.URN,
-                PlaylistProperty.TITLE,
-                PlaylistProperty.CREATOR_NAME,
-                PlaylistProperty.IMAGE_URL_TEMPLATE,
-                PlaylistProperty.TRACK_COUNT,
-                PlaylistProperty.LIKES_COUNT,
-                PlaylistProperty.IS_PRIVATE
-        ).put(PostProperty.CREATED_AT, playlist.getCreatedAt())
-                       .put(PlayableProperty.IS_USER_LIKE, false)
-                       .put(PlayableProperty.IS_USER_REPOST, false);
-    }
-
-    private PropertySet createTrackPostPropertySet(ApiTrack track) {
-        return track.toPropertySet().slice(
-                TrackProperty.URN,
-                TrackProperty.TITLE,
-                TrackProperty.IMAGE_URL_TEMPLATE,
-                TrackProperty.CREATOR_NAME,
-                TrackProperty.LIKES_COUNT,
-                TrackProperty.PLAY_COUNT,
-                TrackProperty.SNIPPET_DURATION,
-                TrackProperty.FULL_DURATION,
-                TrackProperty.IS_PRIVATE,
-                TrackProperty.BLOCKED,
-                TrackProperty.SNIPPED,
-                TrackProperty.SUB_MID_TIER,
-                TrackProperty.SUB_HIGH_TIER
-        ).put(PostProperty.CREATED_AT, track.getCreatedAt())
-                    .put(PlayableProperty.IS_USER_LIKE, false)
-                    .put(PlayableProperty.IS_USER_REPOST, false);
-    }
-
-    private ApiPlaylist createPlaylistAt(Date creationDate) {
-        return testFixtures().insertPlaylistWithCreationDate(user, creationDate);
-    }
-
     private ApiTrack createTrackAt(Date creationDate) {
         return testFixtures().insertTrackWithCreationDate(user, creationDate);
     }
 
-    private void createPlaylistPostWithId(long playlistId, Date postedAt) {
-        testFixtures().insertPlaylistPost(playlistId, postedAt.getTime(), false);
-    }
-
     private void createTrackPostWithId(long trackId, Date postedAt) {
         testFixtures().insertTrackPost(trackId, postedAt.getTime(), false);
-    }
-
-    private void createTrackRepostWithId(long trackId, Date postedAt) {
-        testFixtures().insertTrackRepost(trackId, postedAt.getTime());
-    }
-
-    private void createPlaylistRepostWithId(long trackId, Date postedAt) {
-        testFixtures().insertPlaylistRepost(trackId, postedAt.getTime());
     }
 
     private PropertySet createTrackPostForLastPostedAt(Date postedAt) {
