4f81d260b5babb2e43f7ce44dbfaa21bc56c1dfa
Marvin Ramin <marvin.ramin@soundcloud.com>
2016-11-01 15:23:14 +0100

    clean DevToolsHelper

--- app/src/main/java/com/soundcloud/android/DevToolsHelper.java
+++ app/src/main/java/com/soundcloud/android/DevToolsHelper.java
@@ -1,37 +1,25 @@
 package com.soundcloud.android;
 
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
-import com.soundcloud.android.analytics.AnalyticsModule;
-import com.soundcloud.android.api.ApiModule;
 import com.soundcloud.android.properties.ApplicationProperties;
 import com.squareup.leakcanary.LeakCanary;
-import okhttp3.OkHttpClient;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
 public class DevToolsHelper {
     private final ApplicationProperties applicationProperties;
-    private final OkHttpClient defaultClient;
-    private final OkHttpClient trackingClient;
 
     @Inject
-    DevToolsHelper(ApplicationProperties applicationProperties,
-                   @Named(ApiModule.API_HTTP_CLIENT) OkHttpClient defaultClient,
-                   @Named(AnalyticsModule.TRACKING_HTTP_CLIENT) OkHttpClient trackingClient) {
+    DevToolsHelper(ApplicationProperties applicationProperties) {
         this.applicationProperties = applicationProperties;
-        this.defaultClient = defaultClient;
-        this.trackingClient = trackingClient;
+    }
+
+    private void leakCanary(SoundCloudApplication application) {
+        LeakCanary.install(application);
     }
 
     void initialize(SoundCloudApplication application) {
         if (applicationProperties.isDevelopmentMode()) {
             leakCanary(application);
         }
-    }
-
-    private void leakCanary(SoundCloudApplication application) {
-        LeakCanary.install(application);
     }
 }
