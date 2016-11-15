4ff864759d2a9215c5f93048fb2e146a3a781e68
Fernando Cejas <fcejas@gmail.com>
2016-11-15 16:14:20 +0100

    Rolling back to Android Support Library version 24 due to many tests failing.

--- app/src/test/java/com/soundcloud/android/testsupport/ApplicationStub.java
+++ app/src/test/java/com/soundcloud/android/testsupport/ApplicationStub.java
@@ -1,22 +1,8 @@
 package com.soundcloud.android.testsupport;
 
-import com.google.firebase.FirebaseApp;
-
 import android.app.Application;
 
 /**
  * Used in Robolectric tests so that we don't go through our untestable Application#onCreate
  */
-public class ApplicationStub extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        initializeFirebase();
-    }
-
-    private void initializeFirebase() {
-        if (FirebaseApp.getApps(this).size() == 0) {
-            FirebaseApp.initializeApp(this);
-        }
-    }
-}
+public class ApplicationStub extends Application {}
