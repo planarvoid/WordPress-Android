cdcf72b19fac8013a0c54a4660b459b952943284
jonschmidt <jonschmidt@users.noreply.github.com>
2016-11-24 15:58:32 +0100

    Trying to debug recycler view exception (#6324)
    Refs #6323
    
    Apparently this can happen if you update the underlying data from a different thread. I am guessing this might happen if one of the many things that modifies the stream does this.

--- app/src/main/java/com/soundcloud/android/stream/StreamAdapter.java
+++ app/src/main/java/com/soundcloud/android/stream/StreamAdapter.java
@@ -10,7 +10,9 @@ import com.soundcloud.android.stations.StationsOnboardingStreamItemRenderer;
 import com.soundcloud.android.suggestedcreators.SuggestedCreatorsItemRenderer;
 import com.soundcloud.android.upsell.StreamUpsellItemRenderer;
 import com.soundcloud.android.upsell.UpsellItemRenderer;
+import com.soundcloud.java.checks.Preconditions;
 
+import android.os.Looper;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -64,11 +66,42 @@ public class StreamAdapter extends PagingRecyclerItemAdapter<StreamItem, Recycle
         return new ViewHolder(itemView);
     }
 
+    @Override
+    public void addItem(StreamItem item) {
+        checkMainThread();
+        super.addItem(item);
+    }
+
     public void addItem(int position, StreamItem item) {
+        checkMainThread();
         if (position < getItemCount()) {
             items.add(position, item);
             notifyItemInserted(position);
         }
+    }
+
+    @Override
+    public void clear() {
+        checkMainThread();
+        super.clear();
+    }
+
+    @Override
+    public void prependItem(StreamItem item) {
+        checkMainThread();
+        super.prependItem(item);
+    }
+
+    @Override
+    public void removeItem(int position) {
+        checkMainThread();
+        super.removeItem(position);
+    }
+
+    // debugging https://github.com/soundcloud/SoundCloud-Android/issues/6323
+    private void checkMainThread() {
+        Preconditions.checkState(Looper.myLooper() == Looper.getMainLooper(),
+                                 "Altering adapter off main thread");
     }
 
     void onFollowingEntityChange(EntityStateChangedEvent event) {
