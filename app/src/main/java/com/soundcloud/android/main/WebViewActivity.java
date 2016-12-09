287ab549591c605ef4d28559024facfe3b9ce951
Jamie McDonald <jdamcd@gmail.com>
2016-12-09 11:17:51 -0500

    Support back nav in default WebView Activity (#6634)

--- app/src/main/java/com/soundcloud/android/main/WebViewActivity.java
+++ app/src/main/java/com/soundcloud/android/main/WebViewActivity.java
@@ -4,27 +4,30 @@ import com.soundcloud.android.R;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebViewActivity extends RootActivity {
 
+    @Nullable private WebView webView;
+
     public void onCreate(Bundle savedInstanceState) {
         final Uri uri = getIntent() != null ? getIntent().getData() : null;
         if (uri != null) {
             super.onCreate(savedInstanceState);
-            WebView view = (WebView) findViewById(R.id.webview);
-            view.getSettings().setJavaScriptEnabled(true);
-            view.getSettings().setDomStorageEnabled(true);
-            view.getSettings().setBlockNetworkImage(false);
-            view.getSettings().setLoadsImagesAutomatically(true);
-            view.setWebViewClient(new WebViewClient() {
+            webView = (WebView) findViewById(R.id.webview);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setBlockNetworkImage(false);
+            webView.getSettings().setLoadsImagesAutomatically(true);
+            webView.setWebViewClient(new WebViewClient() {
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
                     return false;
                 }
             });
-            view.loadUrl(uri.toString());
+            webView.loadUrl(uri.toString());
         } else {
             finish();
         }
@@ -38,6 +41,15 @@ public class WebViewActivity extends RootActivity {
     @Override
     protected void setActivityContentView() {
         setContentView(R.layout.web_view);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
