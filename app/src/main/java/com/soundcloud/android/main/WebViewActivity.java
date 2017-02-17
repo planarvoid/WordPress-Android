14531f15d824488f24d962034b56c0deddb3428c
Marvin Ramin <marvin.ramin@gmail.com>
2017-02-17 12:01:12 +0100

    Allow WebViews to launch deeplinks (#7165)
    * allow launching deeplinks from webviews
    
    * change weird captcha error string
    
    * only handle soundcloud:// links in the app

--- app/src/main/java/com/soundcloud/android/main/WebViewActivity.java
+++ app/src/main/java/com/soundcloud/android/main/WebViewActivity.java
@@ -1,7 +1,9 @@
 package com.soundcloud.android.main;
 
 import com.soundcloud.android.R;
+import com.soundcloud.android.deeplinks.DeepLink;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -22,8 +24,16 @@ public class WebViewActivity extends RootActivity {
             webView.getSettings().setBlockNetworkImage(false);
             webView.getSettings().setLoadsImagesAutomatically(true);
             webView.setWebViewClient(new WebViewClient() {
+                // this method was deprecated and the new one added in SDK 24
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    Uri uri = Uri.parse(url);
+                    if (DeepLink.isHierarchicalSoundCloudScheme(uri)) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(uri);
+                        startActivity(intent);
+                        return true;
+                    }
                     return false;
                 }
             });
