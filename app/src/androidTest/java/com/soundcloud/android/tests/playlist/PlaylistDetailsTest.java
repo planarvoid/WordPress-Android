46a90d4ff9d3e75b50327c7b02ae1f5f9b3c051f
Brian Egan <brian@brianegan.com>
2017-02-23 15:13:12 +0100

    Remove OtherPlaylistsByUserTest on old playlist page, as it should no longer exist there. (#7222)

--- app/src/androidTest/java/com/soundcloud/android/tests/playlist/PlaylistDetailsTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/playlist/PlaylistDetailsTest.java
@@ -3,12 +3,10 @@ package com.soundcloud.android.tests.playlist;
 import static com.soundcloud.android.framework.matcher.element.IsVisible.visible;
 import static com.soundcloud.android.framework.matcher.player.IsExpanded.expanded;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 import com.soundcloud.android.framework.TestUser;
-import com.soundcloud.android.framework.annotation.OtherPlaylistsByUserTest;
 import com.soundcloud.android.framework.helpers.mrlogga.TrackingActivityTest;
 import com.soundcloud.android.main.LauncherActivity;
 import com.soundcloud.android.screens.CollectionScreen;
@@ -20,7 +18,6 @@ import com.soundcloud.android.screens.elements.VisualPlayerElement;
 
 public class PlaylistDetailsTest extends TrackingActivityTest<LauncherActivity> {
     private static final String TEST_PLAYLIST_SHUFFLE = "playlist-shuffle-events";
-    private static final String TEST_PLAYLIST_DETAIL_MORE_PLAYLISTS = "playlist-detail-more-playlists";
 
     private PlaylistsScreen playlistsScreen;
 
@@ -78,20 +75,5 @@ public class PlaylistDetailsTest extends TrackingActivityTest<LauncherActivity> 
         menu.clickRemoveFromPlaylist();
 
         assertThat(playlistDetailsScreen.getTrackCount(), is(initialTrackCount - 1));
-    }
-
-
-    @OtherPlaylistsByUserTest
-    public void testGoToMorePlaylistsByUser() throws Exception {
-        startEventTracking();
-
-        PlaylistDetailsScreen playlistDetailsScreen = playlistsScreen.scrollToAndClickPlaylistWithTitle("whatever");
-
-        String title = playlistDetailsScreen.getFirstPlaylistInMoreTitle();
-        PlaylistDetailsScreen nextPlaylistDetailsScreen = playlistDetailsScreen.clickFirstPlaylistInMorePlaylists();
-
-        assertThat(nextPlaylistDetailsScreen.getTitle(), is(equalTo(title)));
-
-        finishEventTracking(TEST_PLAYLIST_DETAIL_MORE_PLAYLISTS);
     }
 }
