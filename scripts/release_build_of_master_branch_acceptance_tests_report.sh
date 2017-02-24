fe3d4735705d4a88bdc00472400f9769fbd088c4
Brigit Lyons <brigit.lyons@soundcloud.com>
2017-02-24 16:55:43 +0100

    fix slackbot (#7221)

--- scripts/release_build_of_master_branch_acceptance_tests_report.sh
+++ scripts/release_build_of_master_branch_acceptance_tests_report.sh
@@ -13,16 +13,14 @@ function payload {
 }
 
 function getNumberOfFailingTests {
-	echo $(curl -s "http://mobile-jenkins.int.s-cloud.net/job/${JOB_NAME}/${BUILD_NUMBER}/testReport/api/json?pretty=true" | \
+	echo $(curl -s "http://chaos-hare.mobile.s-cloud.net:8080/job/${JOB_NAME}/${BUILD_NUMBER}/testReport/api/json?pretty=true" | \
                                python -c "import sys, json; print json.load(sys.stdin)['failCount']")
 }
 
 FAILED_TEST_COUNT=$(getNumberOfFailingTests)
 
 if [ "$FAILED_TEST_COUNT" -le 0 ]; then
-    curl -X POST --data-urlencode payload="$(payload "<$JOB_URL|Night Release Build of Master Branch #$BUILD_NUMBER> Passed" $PASS_ICON)" $URL
+    curl -X POST --data-urlencode payload="$(payload "<$JOB_URL|Nightly Release Build of Master Branch #$BUILD_NUMBER> Passed" $PASS_ICON)" $URL
 else
-    curl -X POST --data-urlencode payload="$(payload "<$JOB_URL|Night Release Build of Master Branch #$BUILD_NUMBER> $FAILED_TEST_COUNT tests failed" $FAIL_ICON)" $URL
+    curl -X POST --data-urlencode payload="$(payload "<$JOB_URL|Nightly Release Build of Master Branch #$BUILD_NUMBER> had $FAILED_TEST_COUNT failing tests" $FAIL_ICON)" $URL
 fi
-
-
