ff2c67df6c46686251457c8c59174ac70f8a3469
Marvin Ramin <marvin.ramin@gmail.com>
2017-03-16 14:57:24 +0100

    mock signup response for captcha behavior (#7420)

--- app/src/androidTest/java/com/soundcloud/android/tests/auth/signup/ByEmailShowingCaptchaTest.java
+++ app/src/androidTest/java/com/soundcloud/android/tests/auth/signup/ByEmailShowingCaptchaTest.java
@@ -1,23 +1,33 @@
 package com.soundcloud.android.tests.auth.signup;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import com.soundcloud.android.api.ApiEndpoints;
 import com.soundcloud.android.screens.auth.signup.SignupSpamScreen;
 import com.soundcloud.android.tests.auth.SignUpTest;
 
 public class ByEmailShowingCaptchaTest extends SignUpTest {
 
+    @Override
+    protected void addInitialStubMappings() {
+        stubFor(post(urlPathEqualTo(ApiEndpoints.SIGN_UP.path()))
+                        .willReturn(aResponse().withStatus(428).withBody("{\"error_key\": \"captcha_required\"}")));
+    }
+
     public void testSignupTriggersCaptcha() throws Exception {
         signUpMethodScreen = homeScreen.clickSignUpButton();
         signUpBasicsScreen = signUpMethodScreen.clickByEmailButton();
 
-        signUpBasicsScreen.typeEmail(generateEmail());
-        signUpBasicsScreen.typePassword("password123");
-        signUpBasicsScreen.typeAge(21);
-
-        signUpBasicsScreen.signup();
-        signUpBasicsScreen.acceptTerms();
+        signUpBasicsScreen.typeEmail("someemail-which-triggers-captcha@siliconninjas.net")
+                          .typePassword("password123")
+                          .typeAge(21)
+                          .signup()
+                          .acceptTerms();
 
         SignupSpamScreen dialog = new SignupSpamScreen(solo);
         assertThat(dialog.isVisible(), is(true));
@@ -26,12 +36,4 @@ public class ByEmailShowingCaptchaTest extends SignUpTest {
         // and check if the activity would loose focus.
         // This does not work if the user is asked to choose which application will open the captcha URL
     }
-
-    // All the emails with the domain "@siliconninjas.net" or "@test.allthecaptchas.io"
-    // will have responses which will trigger a captcha
-    // this is, a 422 response with the response body { "error": 103 }
-    protected String generateEmail() {
-        return "someemail-which-triggers-captcha@siliconninjas.net";
-    }
-
 }
