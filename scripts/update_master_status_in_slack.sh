a8f0882531fdcf46bb6d766fca0873b555776c11
Marvin Ramin <marvin.ramin@soundcloud.com>
2017-04-06 13:16:46 +0200

    small adjustment to the master slack reporter

--- scripts/update_master_status_in_slack.sh
+++ scripts/update_master_status_in_slack.sh
@@ -3,8 +3,7 @@
 set -e
 
 URL=$( cat /home/mobile/installs/webhook.properties | grep -e "url=.*" | cut -d = -f 2)
-TARGET_CHANNEL='@marvin.ramin'
-USERNAME='ci-slackbot'
+TARGET_CHANNEL='#android-build-status'
 PASS_ICON=':green_build:'
 FAIL_ICON=':red_build:'
 
@@ -13,12 +12,12 @@ PREVIOUS_BUILD=$(expr ${CURRENT_BUILD} - 1)
 
 BASE_URL=$(echo ${JOB_URL} | sed 's/mobile-jenkins.int.s-cloud.net/chaos-hare.mobile.s-cloud.net:8080/g')
 function payload {
-    echo "{\"channel\":\"$TARGET_CHANNEL\", \"username\": \"$USERNAME\", \"text\": \"$1\", \"icon_emoji\": \"$2\"}"
+    echo "{\"channel\":\"$TARGET_CHANNEL\", \"username\": \"$3\", \"text\": \"$1\", \"icon_emoji\": \"$2\"}"
 }
 
 function getSubBuildData {
-    local SUBRESULTS=$(curl -s "${BASE_URL}${CURRENT_BUILD}/wfapi/describe" | jq -r '.stages[] | select(.name | contains("Reporting") | not) | .name , .status')
-    local FORMATTED=$(echo ${SUBRESULTS} | sed 's/SUCCESS/:green_build:\\n/g' | sed 's/FAILED/:red_build:\\n/g')
+    local SUBRESULTS=$(curl -s "${BASE_URL}${CURRENT_BUILD}/wfapi/describe" | jq -r '.stages[] | select(.name | contains("Reporting") | not) | .status , .name')
+    local FORMATTED=$(echo ${SUBRESULTS} | sed 's/SUCCESS/\\n:green_build:/g' | sed 's/FAILED/\\n:red_build:/g')
     echo ${FORMATTED}
 }
 
@@ -36,7 +35,7 @@ if [ "$CURRENT_BUILD_STATUS" = "SUCCESS" -a "$PREVIOUS_BUILD_STATUS" = "FAILED" 
 elif [ "$CURRENT_BUILD_STATUS" = "FAILED" -a "$PREVIOUS_BUILD_STATUS" = "SUCCESS" ]; then
     SUB_BUILD_INFO=$(getSubBuildData)
     echo Sending :red-build: message to ${TARGET_CHANNEL}
-    curl -X POST --data-urlencode payload="$(payload "\`${JOB_NAME}\` is :red_build: \n ${SUB_BUILD_INFO} See the build: ${BASE_URL}${CURRENT_BUILD}" ${FAIL_ICON})" ${URL}
+    curl -X POST --data-urlencode payload="$(payload "${SUB_BUILD_INFO} See the build: ${BASE_URL}${CURRENT_BUILD}" ${FAIL_ICON} ${JOB_NAME})" ${URL}
 else
     echo "No status to show, status unchanged at ${CURRENT_BUILD_STATUS}"
 fi
