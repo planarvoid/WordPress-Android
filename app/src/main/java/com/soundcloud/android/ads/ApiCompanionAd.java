9c7357529d35c819f7be78e4de50d9dce25fd83a
Sunny Klair <sunny.klair@soundcloud.com>
2017-04-12 16:23:26 -0400

    Refactoring visual ad models for readability/maintainability

--- app/src/main/java/com/soundcloud/android/ads/ApiCompanionAd.java
+++ app/src/main/java/com/soundcloud/android/ads/ApiCompanionAd.java
@@ -1,30 +1,27 @@
 package com.soundcloud.android.ads;
 
-import android.support.annotation.Nullable;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
 import com.soundcloud.android.model.Urn;
 import com.soundcloud.java.optional.Optional;
 
 import java.util.List;
 
-class ApiCompanionAd extends ApiBaseAdVisual {
-
-    public final ApiDisplayProperties displayProperties;
-    public final Optional<String> ctaButtonText;
-
+@AutoValue
+abstract class ApiCompanionAd extends ApiBaseAdVisual {
     @JsonCreator
-    public ApiCompanionAd(@JsonProperty("urn") Urn urn,
-                          @JsonProperty("image_url") String imageUrl,
-                          @JsonProperty("clickthrough_url") String clickthroughUrl,
-                          @JsonProperty("tracking_impression_urls") List<String> trackingImpressionUrls,
-                          @JsonProperty("tracking_click_urls") List<String> trackingClickUrls,
-                          @JsonProperty("cta_button_text") @Nullable String ctaButtonText,
-                          @JsonProperty("display_properties") ApiDisplayProperties displayProperties) {
-        super(urn, imageUrl, clickthroughUrl, trackingImpressionUrls, trackingClickUrls);
-        this.displayProperties = displayProperties;
-        this.ctaButtonText = Optional.fromNullable(ctaButtonText);
+    static ApiCompanionAd create(@JsonProperty("adUrn") Urn urn,
+                                 @JsonProperty("image_url") String imageUrl,
+                                 @JsonProperty("clickthrough_url") String clickthroughUrl,
+                                 @JsonProperty("tracking_impression_urls") List<String> trackingImpressionUrls,
+                                 @JsonProperty("tracking_click_urls") List<String> trackingClickUrls,
+                                 @JsonProperty("cta_button_text") Optional<String> ctaButtonText,
+                                 @JsonProperty("display_properties") ApiDisplayProperties displayProperties) {
+       return new AutoValue_ApiCompanionAd(urn, imageUrl, clickthroughUrl, trackingImpressionUrls,
+                                           trackingClickUrls, displayProperties, ctaButtonText);
     }
 
+    public abstract ApiDisplayProperties displayProperties();
+    public abstract Optional<String> ctaButtonText();
 }
