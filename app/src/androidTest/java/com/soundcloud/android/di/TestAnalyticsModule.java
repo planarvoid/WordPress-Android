fe6d074c599798c7b708b82c455748699273f0fa
Marvin Ramin <marvin.ramin@gmail.com>
2017-05-09 11:57:34 +0200

    fix Override in TestAnalyticsModule to disable Appboy in Tests (#7789)

--- app/src/androidTest/java/com/soundcloud/android/di/TestAnalyticsModule.java
+++ app/src/androidTest/java/com/soundcloud/android/di/TestAnalyticsModule.java
@@ -1,13 +1,15 @@
 package com.soundcloud.android.di;
 
 import com.soundcloud.android.analytics.AnalyticsModule;
+import com.soundcloud.android.analytics.appboy.AppboyInAppMessageListener;
 import com.soundcloud.android.analytics.appboy.AppboyWrapper;
 import com.soundcloud.android.di.testimplementations.NoopAppboyWrapper;
 
 import android.content.Context;
 
 public class TestAnalyticsModule extends AnalyticsModule {
-    public AppboyWrapper provideAppboy(Context context) {
+    @Override
+    public AppboyWrapper provideAppboy(Context context, AppboyInAppMessageListener listener) {
         return new NoopAppboyWrapper();
     }
 }
