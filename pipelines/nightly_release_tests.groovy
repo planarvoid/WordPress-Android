db2f7c94c47ab6d5db9c926be71ee6eb0e990819
Marvin Ramin <marvin.ramin@gmail.com>
2017-05-10 12:43:47 +0200

    add jenkins pipeline for nightly release tests build (#7778)
    * add jenkins pipeline for nightly release tests build
    
    * rename build stage and set BUILD_TYPE env variable
    
    * don't report to slack while testing
    
    * publish test results in any case

---
+++ pipelines/nightly_release_tests.groovy
@@ -0,0 +1,45 @@
+timestamps {
+  ansiColor('xterm') {
+    try {
+      stage('Checkout') {
+        node('chaos-slave') {
+          checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[url: 'git@github.com:soundcloud/android.git']]])
+
+          def gitCommit = sh(returnStdout: true, script: 'git rev-parse --short=7 HEAD').trim()
+          env.PIPELINE_VERSION = BUILD_NUMBER + '-' + gitCommit
+          currentBuild.displayName = env.PIPELINE_VERSION
+          stash name: 'repository'
+        }
+      }
+      stage('Build and Test') {
+        node('chaos-slave') {
+          deleteDir()
+          unstash 'repository'
+          env.BUILD_TYPE = 'preRelease'
+          try {
+            gradle 'buildPreRelease assembleAcceptanceTest runLollipopTestsRelease'
+          } finally {
+            junit 'results/xml/*.xml'
+            publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: 'results/', reportFiles: 'index.html', reportName: 'Test results'])
+            publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: 'results/', reportFiles: 'collection_index.html', reportName: 'Collection Test results'])
+          }
+        }
+      }
+    } finally {
+      stage('Reporting') {
+        node('chaos-slave') {
+          deleteDir()
+          unstash 'repository'
+          echo 'Reporting'
+//          sh './scripts/release_build_of_master_branch_acceptance_tests_report.sh'
+        }
+      }
+    }
+  } // timestamps
+} // color
+
+def gradle(String tasks) {
+  withEnv(['GRADLE_OPTS=-Dorg.gradle.daemon=false']) {
+    sh "./gradlew " + tasks
+  }
+}
