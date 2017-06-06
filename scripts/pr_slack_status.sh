8469a73fda2eba910342f5a3381687b8d5ba4e59
Marvin Ramin <marvin.ramin@gmail.com>
2017-06-06 17:26:32 +0200

    add success/failed indication to slack message as username and icon (#7971)

--- scripts/pr_slack_status.sh
+++ scripts/pr_slack_status.sh
@@ -5,14 +5,10 @@ set -e
 URL=$( cat /home/mobile/installs/webhook.properties | grep -e "url=.*" | cut -d = -f 2)
 
 TRIGGER_AUTHOR=$ghprbTriggerAuthorLogin
-
-USERNAME='ci-slackbot'
-ICON=':rightshark:'
-
 BUILD_STATUS=$1
 
 function payload {
-    echo "{\"channel\":\"$1\", \"username\": \"$USERNAME\", \"text\": \"$2\", \"icon_emoji\": \"$ICON\"}"
+    echo "{\"channel\":\"$1\", \"username\": \"$2\", \"text\": \"$3\", \"icon_emoji\": \"$4\"}"
 }
 
 IFS=$'\n' users=($(<${WORKSPACE}/scripts/.slack))
@@ -21,11 +17,15 @@ do
    IFS=': ' read -ra map <<< "$i"
    if [ "$TRIGGER_AUTHOR" = "${map[0]}" ]; then
       if [ "$BUILD_STATUS" = 'SUCCESS' ]; then
+          ICON=':green_build:'
+          USERNAME="Green PR Build"
           MESSAGE="... can be merged! :rocket:"
       else
+          ICON=':red_build:'
+          USERNAME="Failing PR Build"
           MESSAGE="... is failing on CI! :fire:"
       fi
 
-      curl -X POST --data-urlencode payload="$(payload "@${map[1]}" "*PR$ghprbPullId: $ghprbPullTitle*\n$MESSAGE\n Jenkins: ${JOB_URL}${BUILD_NUMBER} \n GitHub: ${ghprbPullLink}")" $URL
+      curl -X POST --data-urlencode payload="$(payload "@${map[1]}" "${USERNAME}" "*PR$ghprbPullId: $ghprbPullTitle*\n$MESSAGE\n Jenkins: ${JOB_URL}${BUILD_NUMBER} \n GitHub: ${ghprbPullLink}" "${ICON}")" $URL
    fi
 done
