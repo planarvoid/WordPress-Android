76fd9136c2024c559c5fe135f2c0b0c06dd462f5
Júlio Zynger <julioz92@gmail.com>
2017-06-07 11:57:35 +0200

    Do not trigger concurrent streaming pause control while casting (#7979)
    Since the playback control should be on the receiver, if a user is casting
    with device A and then starts playback on device B we should then ignore
    the concurrent streaming control because the playback is centralised in the
    receiver instead of both sender devices.

--- app/src/main/java/com/soundcloud/android/playback/ConcurrentPlaybackOperations.java
+++ app/src/main/java/com/soundcloud/android/playback/ConcurrentPlaybackOperations.java
@@ -1,5 +1,6 @@
 package com.soundcloud.android.playback;
 
+import com.soundcloud.android.cast.CastConnectionHelper;
 import com.soundcloud.android.playback.ui.view.PlaybackFeedbackHelper;
 
 import javax.inject.Inject;
@@ -9,22 +10,24 @@ public class ConcurrentPlaybackOperations {
     private final StopReasonProvider stopReasonProvider;
     private final PlaySessionController playSessionController;
     private final PlaySessionStateProvider playSessionStateProvider;
+    private final CastConnectionHelper castConnectionHelper;
     private final PlaybackFeedbackHelper playbackFeedbackHelper;
 
     @Inject
     ConcurrentPlaybackOperations(StopReasonProvider stopReasonProvider,
                                  PlaySessionController playSessionController,
                                  PlaySessionStateProvider playSessionStateProvider,
+                                 CastConnectionHelper castConnectionHelper,
                                  PlaybackFeedbackHelper playbackFeedbackHelper) {
-
         this.stopReasonProvider = stopReasonProvider;
         this.playSessionController = playSessionController;
         this.playSessionStateProvider = playSessionStateProvider;
+        this.castConnectionHelper = castConnectionHelper;
         this.playbackFeedbackHelper = playbackFeedbackHelper;
     }
 
     public void pauseIfPlaying() {
-        if (playSessionStateProvider.isPlaying()) {
+        if (!castConnectionHelper.isCasting() && playSessionStateProvider.isPlaying()) {
             stopReasonProvider.setPendingConcurrentPause();
             playSessionController.fadeAndPause();
             playbackFeedbackHelper.showConcurrentStreamingStoppedFeedback();
--- app/src/test/java/com/soundcloud/android/playback/ConcurrentPlaybackOperationsTest.java
+++ app/src/test/java/com/soundcloud/android/playback/ConcurrentPlaybackOperationsTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.soundcloud.android.cast.CastConnectionHelper;
 import com.soundcloud.android.playback.ui.view.PlaybackFeedbackHelper;
 import com.soundcloud.android.testsupport.AndroidUnitTest;
 import org.junit.Test;
@@ -15,6 +16,7 @@ public class ConcurrentPlaybackOperationsTest extends AndroidUnitTest {
     @Mock StopReasonProvider stopReasonProvider;
     @Mock PlaySessionStateProvider playSessionStateProvider;
     @Mock PlaySessionController playSessionController;
+    @Mock CastConnectionHelper castConnectionHelper;
     @Mock PlaybackFeedbackHelper playbackFeedbackHelper;
 
     @InjectMocks private ConcurrentPlaybackOperations operations;
@@ -73,4 +75,14 @@ public class ConcurrentPlaybackOperationsTest extends AndroidUnitTest {
         verify(playbackFeedbackHelper, never()).showConcurrentStreamingStoppedFeedback();
     }
 
+    @Test
+    public void pauseIfPlayingDoesNotPauseWhileCastingSinceThePlaybackControlShouldBeOnTheReceiver() {
+        when(castConnectionHelper.isCasting()).thenReturn(true);
+
+        operations.pauseIfPlaying();
+
+        verify(stopReasonProvider, never()).setPendingConcurrentPause();
+        verify(playSessionController, never()).fadeAndPause();
+        verify(playbackFeedbackHelper, never()).showConcurrentStreamingStoppedFeedback();
+    }
 }
