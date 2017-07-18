30f27dffa05bb58123fda5f75cd6628d4d054f0a
Júlio Zynger <julioz92@gmail.com>
2017-07-18 12:50:08 +0200

    Reuse GooglePlayServicesWrapper to detect framework availability (#8269)
    * Reuse GooglePlayServicesWrapper to detect whether the framework is available
    
    * Make field final

--- app/src/main/java/com/soundcloud/android/ads/AdIdWrapper.java
+++ app/src/main/java/com/soundcloud/android/ads/AdIdWrapper.java
@@ -1,10 +1,9 @@
 package com.soundcloud.android.ads;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.soundcloud.android.utils.GooglePlayServicesWrapper;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
@@ -15,17 +14,20 @@ import java.io.IOException;
 class AdIdWrapper {
 
     private final Context context;
+    private final GooglePlayServicesWrapper googlePlayServicesWrapper;
 
     @Inject
-    public AdIdWrapper(Context context) {
+    public AdIdWrapper(Context context, GooglePlayServicesWrapper googlePlayServicesWrapper) {
         this.context = context;
+        this.googlePlayServicesWrapper = googlePlayServicesWrapper;
     }
 
     public boolean isPlayServicesAvailable() {
-        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS;
+        return googlePlayServicesWrapper.isPlayServiceAvailable(context);
     }
 
-    @Nullable public AdvertisingIdClient.Info getAdInfo() throws IOException, GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
+    @Nullable
+    public AdvertisingIdClient.Info getAdInfo() throws IOException, GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
         return AdvertisingIdClient.getAdvertisingIdInfo(context);
     }
 
