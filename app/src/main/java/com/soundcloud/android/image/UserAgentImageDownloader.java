a35d9d85b7e5031754a1e6f1687b6ca4b5f71a4e
Riccardo Ciovati <rciovati@gmail.com>
2017-07-27 15:25:09 +0200

    use okhttp to request images (#8347)

--- app/src/main/java/com/soundcloud/android/image/UserAgentImageDownloader.java
+++ app/src/main/java/com/soundcloud/android/image/UserAgentImageDownloader.java
@@ -5,27 +5,36 @@ import com.google.auto.factory.Provided;
 import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
 import com.soundcloud.android.utils.DeviceHelper;
 import com.soundcloud.java.net.HttpHeaders;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 import android.content.Context;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
 
 @AutoFactory(allowSubclasses = true)
 public class UserAgentImageDownloader extends BaseImageDownloader {
 
+    private final OkHttpClient okHttpClient;
     private final String userAgent;
 
     public UserAgentImageDownloader(Context context,
+                                    @Provided OkHttpClient okHttpClient,
                                     @Provided DeviceHelper deviceHelper) {
         super(context);
+        this.okHttpClient = okHttpClient;
         this.userAgent = deviceHelper.getUserAgent();
     }
 
     @Override
-    protected HttpURLConnection createConnection(String url, Object extra) throws IOException {
-        HttpURLConnection conn = super.createConnection(url, extra);
-        conn.setRequestProperty(HttpHeaders.USER_AGENT, userAgent);
-        return conn;
+    protected InputStream getStreamFromNetwork(String imageUri, Object extra) throws IOException {
+
+        final Request request = new Request.Builder()
+                .url(imageUri)
+                .addHeader(HttpHeaders.USER_AGENT, userAgent)
+                .build();
+
+        return okHttpClient.newCall(request).execute().body().byteStream();
     }
 }
