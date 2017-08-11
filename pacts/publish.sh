95a262c610610cdf3db46291d1d8821cff5c6b58
Vivek Jain <vivekjain10@gmail.com>
2017-08-11 11:50:43 +0200

    Ensure curl returns failure code

--- pacts/publish.sh
+++ pacts/publish.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname "$0")
 PACT_BROKER=pact-broker.dev.s-cloud.net
 
-curl -v -X PUT -H "Content-Type: application/json" -d @$BASEDIR/android-api-mobile.json http://$PACT_BROKER/pacts/provider/api-mobile/consumer/android/version/1.0.0
+curl -f -v -X PUT -H "Content-Type: application/json" -d @$BASEDIR/android-api-mobile.json http://$PACT_BROKER/pacts/provider/api-mobile/consumer/android/version/1.0.0
