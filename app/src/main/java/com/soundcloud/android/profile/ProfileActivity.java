396102f707290afdf92b7efeb09ebf890aedd999
Riccardo Ciovati <rciovati@gmail.com>
2017-09-27 10:10:59 +0200

    Add bottom nav in profile layout (#8683)

--- app/src/main/java/com/soundcloud/android/profile/ProfileActivity.java
+++ app/src/main/java/com/soundcloud/android/profile/ProfileActivity.java
@@ -3,9 +3,11 @@ package com.soundcloud.android.profile;
 
 import com.soundcloud.android.R;
 import com.soundcloud.android.SoundCloudApplication;
+import com.soundcloud.android.configuration.experiments.AppNavigationExperiment;
 import com.soundcloud.android.main.FullscreenablePlayerActivity;
 import com.soundcloud.android.main.Screen;
 import com.soundcloud.android.model.Urn;
+import com.soundcloud.android.navigation.BottomNavigationViewPresenter;
 import com.soundcloud.android.utils.UriUtils;
 import com.soundcloud.android.utils.Urns;
 import com.soundcloud.android.view.screen.BaseLayoutHelper;
@@ -21,8 +23,11 @@ public class ProfileActivity extends FullscreenablePlayerActivity {
     public static final String EXTRA_SEARCH_QUERY_SOURCE_INFO = "searchQuerySourceInfo";
 
     @Inject @LightCycle ProfilePresenter profilePresenter;
+    @Inject @LightCycle BottomNavigationViewPresenter bottomNavigationViewPresenter;
+
     @Inject ProfileConfig profileConfig;
     @Inject BaseLayoutHelper baseLayoutHelper;
+    @Inject AppNavigationExperiment appNavigationExperiment;
 
     public ProfileActivity() {
         SoundCloudApplication.getObjectGraph().inject(this);
@@ -37,11 +42,18 @@ public class ProfileActivity extends FullscreenablePlayerActivity {
     @Override
     protected void setActivityContentView() {
         super.setActivityContentView();
-        if (profileConfig.showProfileBanner()) {
-            baseLayoutHelper.createActionBarLayout(this, R.layout.profile);
+
+        final boolean bottomNavEnabled = appNavigationExperiment.isBottomNavigationEnabled();
+        final boolean showProfileBanner = profileConfig.showProfileBanner();
+
+        final int targetLayout;
+        if (showProfileBanner) {
+            targetLayout = bottomNavEnabled ? R.layout.profile_with_bottom_view : R.layout.profile;
         } else {
-            baseLayoutHelper.createActionBarLayout(this, R.layout.profile_no_banner);
+            targetLayout = bottomNavEnabled ? R.layout.profile_no_banner_with_bottom_view : R.layout.profile_no_banner;
         }
+
+        baseLayoutHelper.createActionBarLayout(this, targetLayout);
     }
 
     @Override
