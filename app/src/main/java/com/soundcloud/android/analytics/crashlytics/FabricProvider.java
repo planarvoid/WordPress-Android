b99bf22bf9d31a5fb3e9ca6548699c88ccd8c2a1
nicholasdower <nicholasdower@gmail.com>
2017-10-02 16:44:09 +0200

    Only include CrashlyticsNdk in beta. (#8738)

--- app/src/main/java/com/soundcloud/android/analytics/crashlytics/FabricProvider.java
+++ app/src/main/java/com/soundcloud/android/analytics/crashlytics/FabricProvider.java
@@ -3,13 +3,17 @@ package com.soundcloud.android.analytics.crashlytics;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.core.CrashlyticsCore;
-import com.crashlytics.android.ndk.CrashlyticsNdk;
+import com.soundcloud.android.properties.ApplicationProperties;
+import com.soundcloud.android.utils.ErrorUtils;
+import com.soundcloud.java.collections.Lists;
 import io.fabric.sdk.android.Fabric;
+import io.fabric.sdk.android.Kit;
 
 import android.content.Context;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 import java.util.concurrent.Executor;
 
 @Singleton
@@ -25,20 +29,24 @@ public class FabricProvider {
         executor = Fabric.with(context).getExecutorService();
     }
 
-    public static void initialize(Context context) {
-        Fabric.with(context, new Crashlytics(), new CrashlyticsNdk(), new Answers());
+    public static void initialize(Context context, ApplicationProperties applicationProperties) {
+        List<Kit> kits = Lists.newArrayList(new Crashlytics(), new Answers());
+        if (applicationProperties.shouldReportNativeCrashes()) {
+            try {
+                kits.add((Kit) Class.forName("com.crashlytics.android.ndk.CrashlyticsNdk").newInstance());
+            } catch (Exception e) {
+                ErrorUtils.handleSilentException("Failed to load CrashlyticsNdk", e);
+            }
+        }
+        Fabric.with(context, kits.toArray(new Kit[kits.size()]));
     }
 
-    public boolean isInitialized() {
+    boolean isInitialized() {
         return Fabric.isInitialized();
     }
 
     Executor getExecutor() {
         return executor;
-    }
-
-    Answers getAnswers() {
-        return Fabric.getKit(Answers.class);
     }
 
     CrashlyticsCore getCrashlyticsCore() {
